3d31c83d4edf0e15d09c8af1226a6f2ac19c3195
hamed shirbandi <hamed.shirbandi@gmail.com>
2020-10-16 18:54:18 +0330

    add test layers

---
+++ UnitTests.Base/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Base
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

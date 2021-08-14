06436feeb9551a698d937b12dcc4d8ff6875930e
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-08-14 20:30:13 +0430

    add all board's commands and queries to application service interface

--- Application/Boards/Services/IBoardService.cs
+++ Application/Boards/Services/IBoardService.cs
@@ -4,11 +4,15 @@ using TaskoMask.Application.Core.ViewMoldes;
 using TaskoMask.Application.BaseEntities.Services;
 using TaskoMask.Application.Core.Dtos.Boards;
 using System.Collections.Generic;
+using TaskoMask.Application.Core.Commands;
 
 namespace TaskoMask.Application.Boards.Services
 {
     public interface IBoardService:IBaseEntityService
     {
+        Task<Result<CommandResult>> CreateAsync(BoardInputDto input);
+        Task<Result<CommandResult>> UpdateAsync(BoardInputDto input);
+
         Task<Result<BoardDetailViewModel>> GetDetailAsync(string id);
         Task<Result<BoardBasicInfoDto>> GetAsync(string id);
         Task<Result<BoardReportDto>> GetReportAsync(string id);

0892d4c3153d706652e18f04bbe9dca3a4269f7c
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-10 22:17:23 +0330

    add role mapers

---
+++ Infrastructure.CrossCutting/Mapper/Profiles/RoleMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TaskoMask.Application.Core.Dtos.Boards;
+using TaskoMask.Application.Core.Dtos.Roles;
+using TaskoMask.Domain.Administration.Entities;
+
+namespace TaskoMask.Application.Mapper.Profiles
+{
+    public class RoleMappingProfile : Profile
+    {
+        public RoleMappingProfile()
+        {
+            CreateMap<Role, RoleBasicInfoDto>();
+            CreateMap<Role, RoleInputDto>();
+            CreateMap<RoleBasicInfoDto, RoleInputDto>();
+        }
+    }
+}

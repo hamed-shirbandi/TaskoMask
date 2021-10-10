e4ea966c89bfe453a2b93db8c4e4b97943912b7c
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-10 21:34:21 +0330

    add role repository

---
+++ Domain/Administration/Data/IRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using TaskoMask.Domain.Core.Data;
+using TaskoMask.Domain.Administration.Entities;
+
+namespace TaskoMask.Domain.Administration.Data
+{
+    public interface IRoleRepository : IBaseRepository<Role>
+    {
+        Task<bool> ExistByNameAsync(string id, string name);
+    }
+}
---
+++ Infrastructure.Data/Repositories/RoleRepository.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskoMask.Domain.Administration.Data;
+using TaskoMask.Domain.Administration.Entities;
+using TaskoMask.Infrastructure.Data.DbContext;
+
+namespace TaskoMask.Infrastructure.Data.Repositories
+{
+    public class RoleRepository : BaseRepository<Role>, IRoleRepository
+    {
+        #region Fields
+
+        private readonly IMongoCollection<Role> _roles;
+
+        #endregion
+
+        #region Ctors
+
+        public RoleRepository(IMongoDbContext dbContext) : base(dbContext)
+        {
+            _roles = dbContext.GetCollection<Role>(); 
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<bool> ExistByNameAsync(string id, string name)
+        {
+            var role = await _roles.Find(e => e.Name == name).FirstOrDefaultAsync();
+            return role != null && role.Id != id;
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+    }
+}

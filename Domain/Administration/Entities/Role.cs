71de179777d5505faba1346e0dd69ec0169bc4b4
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-10 21:27:43 +0330

    add role entity

---
+++ Domain/Administration/Entities/Role.cs
@@ -0,0 +1,58 @@
+﻿using TaskoMask.Domain.Core.Models;
+
+
+namespace TaskoMask.Domain.Administration.Entities
+{
+    /// <summary>
+    /// operator's role
+    /// </summary>
+   public class Role : BaseEntity
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Ctors
+
+        public Role(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+
+
+        #endregion
+
+        #region Properties
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Update(string name, string description)
+        {
+            Description = description;
+            Name = name;
+            base.Update();
+        }
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}

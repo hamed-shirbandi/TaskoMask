333895b715c350d01ce4520dbc92a87c38a39ea0
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-11 23:59:15 +0330

    add operators controller to admin web  layer

---
+++ Web.Admin/Areas/Administration/Controllers/OperatorsController.cs
@@ -0,0 +1,52 @@
+﻿using TaskoMask.Application.Administration.Operators.Commands.Models;
+using TaskoMask.Application.Administration.Operators.Queries.Models;
+using TaskoMask.Application.Administration.Operators.Services;
+using TaskoMask.Application.Administration.Roles.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using TaskoMask.Domain.Administration.Entities;
+using TaskoMask.Web.Common.Controllers;
+
+namespace TaskoMask.Web.Admin.Areas.Administration.Controllers
+{
+
+    [Authorize]
+    [Area("Administration")]
+    public class OperatorsController : BaseMvcController
+    {
+        #region Fields
+
+        private readonly IOperatorService _operatorService;
+        private readonly IRoleService _roleService;
+
+        #endregion
+
+        #region Ctor
+
+        public OperatorsController(IOperatorService operatorService, IRoleService roleService, IMapper mapper) : base(mapper)
+        {
+            _operatorService = operatorService;
+            _roleService = roleService;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+
+    }
+}

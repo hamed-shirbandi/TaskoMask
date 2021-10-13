08eaed0615aaff939365930407793f4a160b1533
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-14 00:17:37 +0330

    add role actions

--- Web.Admin/Areas/Administration/Controllers/RolesController.cs
+++ Web.Admin/Areas/Administration/Controllers/RolesController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
 using TaskoMask.Web.Common.Controllers;
+using System.Threading.Tasks;
+using TaskoMask.Application.Core.Dtos.Roles;
+using TaskoMask.Web.Common.Helpers;
 
 namespace TaskoMask.Web.Admin.Areas.Administration.Controllers
 {
@@ -27,6 +30,74 @@ namespace TaskoMask.Web.Admin.Areas.Administration.Controllers
         #endregion
 
         #region Public Methods
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var rolesListQueryResult = await _roleService.GetListAsync();
+            return View(rolesListQueryResult);
+        }
+
+
+
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RoleInputDto input)
+        {
+            var cmdResult = await _roleService.CreateAsync(input);
+            return View(cmdResult, input);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Update(string id)
+        {
+            var role = await _roleService.GetDetailsAsync(id);
+            return View(role);
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(RoleInputDto input)
+        {
+            var cmdResult = await _roleService.UpdateAsync(input);
+            return View(cmdResult, input);
+        }
+
+
 
 
 

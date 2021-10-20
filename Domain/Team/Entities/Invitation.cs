b6a7b37bf6d261bcfccf9c06397d93e753c376db
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-21 00:40:36 +0330

    add invitation entity to domain

---
+++ Domain/Team/Entities/Invitation.cs
@@ -0,0 +1,48 @@
+﻿using TaskoMask.Domain.Core.Models;
+using TaskoMask.Domain.Core.Services;
+using TaskoMask.Domain.Team.Events;
+
+namespace TaskoMask.Domain.Team.Entities
+{
+    /// <summary>
+    /// To determine invited members to another member's account
+    /// </summary>
+    public class Invitation : BaseEntity
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Ctors
+
+        public Invitation(string displayName, string email,  string password)
+        {
+
+        }
+
+
+
+        #endregion
+
+        #region Properties
+
+        public string InviterMemberId { get; private set; }
+        public string InvitedMemberId { get; private set; }
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}

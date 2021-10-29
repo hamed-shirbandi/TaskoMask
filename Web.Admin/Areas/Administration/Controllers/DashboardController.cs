2fc01645c53cbd643c48c83ebdbd876eb6d83d1d
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-29 23:27:46 +0330

    add dashboard to admin panel

---
+++ Web.Admin/Areas/Administration/Controllers/DashboardController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskoMask.Application.Team.Organizations.Services;
+using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
+using TaskoMask.Web.Common.Controllers;
+using TaskoMask.Domain.Core.Services;
+
+namespace TaskoMask.Web.Admin.Areas.Administration.Controllers
+{
+    [Authorize]
+    [Area("administration")]
+    public class DashboardController : BaseMvcController
+    {
+        #region Fields
+
+        private readonly IOrganizationService _organizationService;
+
+
+        #endregion
+
+        #region Ctors
+
+        public DashboardController(IOrganizationService organizationService, IMapper mapper, IAuthenticatedUserService authenticatedUserService) : base(mapper, authenticatedUserService)
+        {
+            _organizationService = organizationService;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<IActionResult> Index()
+        {
+            
+            return View();
+        }
+
+
+
+
+
+        #endregion
+
+    }
+}
---
+++ Web.Admin/Areas/Administration/Models/DashboardIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using TaskoMask.Application.Core.ViewModels;
+
+namespace TaskoMask.Web.Admin.Area.Admin.Models
+{
+    public class DashboardIndexViewModel
+    {
+    }
+}

87d2734c05ee5a818810c32eecaaf6976c0cd58d
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-29 23:42:41 +0330

    add admin panel public controllers

---
+++ Web.Admin/Controllers/ErrorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskoMask.Web.Common.Controllers;
+
+namespace Web.Admin.Controllers
+{
+    public class ErrorController : BaseMvcController
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Ctor
+
+        public ErrorController( )
+        {
+           
+        }
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IActionResult Unknown()
+        {
+            return View("Error");
+        }
+
+
+
+        
+        #endregion
+
+    }
+}

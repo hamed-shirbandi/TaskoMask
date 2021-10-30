5bd1a03e9e0c20d3faba642823703f7576c609e6
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-10-30 04:10:18 +0330

    add setUserActive to admin panel

---
+++ Web.Admin/Models/SetUserActiveViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace TaskoMask.web.Admin.Models
+{
+    public class SetUserActiveViewModel
+    {
+        public SetUserActiveViewModel(string id,bool isActive)
+        {
+            Id = id;
+            IsActive = isActive;
+        }
+        public string Id { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

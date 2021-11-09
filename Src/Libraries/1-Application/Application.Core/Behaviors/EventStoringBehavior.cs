6e53760222a272b5b11ebf5d2e36bcdf786003f8
Connell Watkins <connell.watkins@oakbrookfinance.com>
2021-11-09 18:42:26 +0000

    Move IEventStore abstraction to Domain.Core and remove reference to infrastructure from application layer

--- Src/Libraries/1-Application/Application.Core/Behaviors/EventStoringBehavior.cs
+++ Src/Libraries/1-Application/Application.Core/Behaviors/EventStoringBehavior.cs
@@ -8,7 +8,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using TaskoMask.Application.Core.Commands;
 using TaskoMask.Domain.Core.Events;
-using TaskoMask.Infrastructure.Data.EventSourcing;
 
 namespace TaskoMask.Application.Core.Behaviors
 {
---
+++ Src/Libraries/3-Domain/Domain.Core/Events/IEventStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaskoMask.Domain.Core.Events
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IEventStore
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void Save<T>(T @event) where T : IDomainEvent;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Task SaveAsync<T>(T @event) where T : IDomainEvent;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Task<List<T>> GetListAsync<T>(string entityId, string entityType) where T : IDomainEvent;
+    }
+}

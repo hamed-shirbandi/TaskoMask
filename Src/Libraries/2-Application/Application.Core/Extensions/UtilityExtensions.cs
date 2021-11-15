5441b2d5063b76b2900724ece5f7967e9bc2d4c4
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-15 22:16:48 +0330

    add an enam extension

--- Src/Libraries/2-Application/Application.Core/Extensions/UtilityExtensions.cs
+++ Src/Libraries/2-Application/Application.Core/Extensions/UtilityExtensions.cs
@@ -1,4 +1,9 @@
 ﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
 namespace TaskoMask.Application.Core.Extensions
 {
 
@@ -14,6 +19,21 @@ namespace TaskoMask.Application.Core.Extensions
         {
             return str.Substring(0, 1).ToLower() + str.Substring(1);
         }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            .GetName();
+        }
+
 
     }
 }

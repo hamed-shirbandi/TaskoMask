42809f47df5853d1c030427099810f7c59c31329
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-15 22:14:51 +0330

    add board view and controller in admin panel

---
+++ Src/Presentation/Web.Admin/Areas/Workspace/Controllers/BoardsController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using TaskoMask.Web.Common.Controllers;
+using System.Threading.Tasks;
+using TaskoMask.Web.Common.Filters;
+using TaskoMask.Web.Common.Helpers;
+using TaskoMask.Application.Workspace.Boards.Services;
+
+namespace TaskoMask.Web.Admin.Areas.Workspace.Controllers
+{
+    [Authorize]
+    [Area("Workspace")]
+    public class BoardsController : BaseMvcController
+    {
+        #region Fields
+
+        private readonly IBoardService _boardService;
+
+
+        #endregion
+
+        #region Ctor
+
+        public BoardsController(IBoardService boardService, IMapper mapper) : base(mapper)
+        {
+            _boardService = boardService;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var boardQueryResult = await _boardService.SearchAsync(page: 1, recordsPerPage: recordsPerPage, term: "");
+
+            #region ViewBags
+
+            ViewBag.PageSize = boardQueryResult.Value.PageNumber;
+            ViewBag.CurrentPage = 1;
+            ViewBag.TotalItemCount = boardQueryResult.Value.TotalCount;
+
+            #endregion
+
+            return View(boardQueryResult);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet]
+        [AjaxOnly]
+        public async Task<IActionResult> Search(int page = 1, string term = "")
+        {
+            var boardQueryResult = await _boardService.SearchAsync(page: page, recordsPerPage: recordsPerPage, term: term);
+
+            if (!boardQueryResult.IsSuccess)
+                return RedirectToErrorPage(boardQueryResult);
+
+            #region ViewBags
+
+            ViewBag.PageSize = boardQueryResult.Value.PageNumber;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalItemCount = boardQueryResult.Value.TotalCount;
+
+            #endregion
+
+            return PartialView("_ItemList", boardQueryResult.Value.Items);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Details(string id)
+        {
+            var boardQueryResult = await _boardService.GetDetailsAsync(id);
+
+            return View(boardQueryResult);
+
+        }
+
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+    }
+}

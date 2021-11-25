30a7144addb6cb25e520a448b38c5a3bffd4018a
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-25 15:56:27 +0330

    move user panel layout from old mvc project to blazor one

---
+++ Src/Presentation/Web.Panel/Models/DashboardIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TaskoMask.Application.Core.ViewModels;
+
+namespace TaskoMask.Web.Panel.Models
+{
+    public class DashboardIndexViewModel
+    {
+        public DashboardIndexViewModel()
+        {
+            OrganizationsDetailsList = new List<OrganizationDetailsViewModel>();
+        }
+
+        public IEnumerable<OrganizationDetailsViewModel> OrganizationsDetailsList { get; set; }
+
+    }
+}

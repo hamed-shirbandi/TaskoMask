86257275e9f67d5690efe17d5ce366aff35f1f09
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-27 01:39:25 +0330

    remove admin area from website

--- Src/Presentation/Web/Startup.cs
+++ Src/Presentation/Web/Startup.cs
@@ -38,10 +38,6 @@ namespace TaskoMask.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "areas",
-                    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
 

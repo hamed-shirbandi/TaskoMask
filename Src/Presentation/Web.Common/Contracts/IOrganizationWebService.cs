23611bf16e1f9aa209cb7cdd729b2f1401069560
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-29 23:59:51 +0330

    implement organization webservice methods

--- Src/Presentation/Web.Common/Contracts/IOrganizationWebService.cs
+++ Src/Presentation/Web.Common/Contracts/IOrganizationWebService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Team.Organizations;
+using TaskoMask.Application.Core.Helpers;
+using TaskoMask.Application.Core.ViewModels;
 
 namespace TaskoMask.Web.Common.Contracts
 {
     public interface IOrganizationWebService
     {
+        Task<Result<CommandResult>> Create(OrganizationUpsertDto input);
+        Task<Result<OrganizationDetailsViewModel>> Get(string id);
+        Task<Result<CommandResult>> Update(OrganizationUpsertDto input);
     }
 }
--- Src/Presentation/Web.Panel/Data/OrganizationWebService.cs
+++ Src/Presentation/Web.Panel/Data/OrganizationWebService.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Team.Organizations;
+using TaskoMask.Application.Core.Helpers;
+using TaskoMask.Application.Core.ViewModels;
 using TaskoMask.Web.Common.Contracts;
 
 namespace TaskoMask.Web.Panel.Data
 {
     public class OrganizationWebService : IOrganizationWebService
     {
+        public Task<Result<CommandResult>> Create(OrganizationUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<OrganizationDetailsViewModel>> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CommandResult>> Update(OrganizationUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

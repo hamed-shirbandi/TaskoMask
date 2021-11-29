82ea6f742d5f04171fb863f658a70eda94a69d1e
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-29 23:59:26 +0330

    implement card webservice methods

--- Src/Presentation/Web.Common/Contracts/ICardWebService.cs
+++ Src/Presentation/Web.Common/Contracts/ICardWebService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Workspace.Cards;
+using TaskoMask.Application.Core.Helpers;
+using TaskoMask.Application.Core.ViewModels;
 
 namespace TaskoMask.Web.Common.Contracts
 {
     public interface ICardWebService
     {
+        Task<Result<CommandResult>> Create(CardUpsertDto input);
+        Task<Result<CardDetailsViewModel>> Get(string id);
+        Task<Result<CommandResult>> Update(CardUpsertDto input);
     }
 }
--- Src/Presentation/Web.Panel/Data/CardWebService.cs
+++ Src/Presentation/Web.Panel/Data/CardWebService.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Workspace.Cards;
+using TaskoMask.Application.Core.Helpers;
+using TaskoMask.Application.Core.ViewModels;
 using TaskoMask.Web.Common.Contracts;
 
 namespace TaskoMask.Web.Panel.Data
 {
     public class CardWebService : ICardWebService
     {
+        public Task<Result<CommandResult>> Create(CardUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CardDetailsViewModel>> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CommandResult>> Update(CardUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

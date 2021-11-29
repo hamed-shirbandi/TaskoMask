06fe446b5effba0b3b7bb0bb4288f30b3b36ba48
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-29 23:59:07 +0330

    implement board webservice methods

--- Src/Presentation/Web.Common/Contracts/IBoardWebService.cs
+++ Src/Presentation/Web.Common/Contracts/IBoardWebService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Workspace.Boards;
+using TaskoMask.Application.Core.Helpers;
+using TaskoMask.Application.Core.ViewModels;
 
 namespace TaskoMask.Web.Common.Contracts
 {
     public interface IBoardWebService
     {
+        Task<Result<CommandResult>> Create(BoardUpsertDto input);
+        Task<Result<BoardDetailsViewModel>> Get(string id);
+        Task<Result<CommandResult>> Update(BoardUpsertDto input);
     }
 }
--- Src/Presentation/Web.Panel/Data/BoardWebService.cs
+++ Src/Presentation/Web.Panel/Data/BoardWebService.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Workspace.Boards;
+using TaskoMask.Application.Core.Helpers;
+using TaskoMask.Application.Core.ViewModels;
 using TaskoMask.Web.Common.Contracts;
 
 namespace TaskoMask.Web.Panel.Data
 {
     public class BoardWebService : IBoardWebService
     {
+        public Task<Result<CommandResult>> Create(BoardUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<BoardDetailsViewModel>> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CommandResult>> Update(BoardUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

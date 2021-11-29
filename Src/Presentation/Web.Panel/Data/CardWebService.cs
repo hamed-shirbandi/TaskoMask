1dd5f8a8ebc60df68bc76216a2eca0e9eabeebab
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-29 23:48:31 +0330

    add webservices implemention to panle

---
+++ Src/Presentation/Web.Panel/Data/CardWebService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Web.Common.Contracts;
+
+namespace TaskoMask.Web.Panel.Data
+{
+    public class CardWebService : ICardWebService
+    {
+    }
+}

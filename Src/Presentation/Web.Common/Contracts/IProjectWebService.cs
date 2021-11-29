2504e1fed537ace5a53e128c94e4b32f0f7f15f4
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-30 00:00:06 +0330

    implement project webservice methods

--- Src/Presentation/Web.Common/Contracts/IProjectWebService.cs
+++ Src/Presentation/Web.Common/Contracts/IProjectWebService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Team.Projects;
+using TaskoMask.Application.Core.Helpers;
+using TaskoMask.Application.Core.ViewModels;
 
 namespace TaskoMask.Web.Common.Contracts
 {
     public interface IProjectWebService
     {
+        Task<Result<CommandResult>> Create(ProjectUpsertDto input);
+        Task<Result<ProjectDetailsViewModel>> Get(string id);
+        Task<Result<CommandResult>> Update(ProjectUpsertDto input);
     }
 }
--- Src/Presentation/Web.Panel/Data/ProjectWebService.cs
+++ Src/Presentation/Web.Panel/Data/ProjectWebService.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Team.Projects;
+using TaskoMask.Application.Core.Helpers;
+using TaskoMask.Application.Core.ViewModels;
 using TaskoMask.Web.Common.Contracts;
 
 namespace TaskoMask.Web.Panel.Data
 {
     public class ProjectWebService : IProjectWebService
     {
+        public Task<Result<CommandResult>> Create(ProjectUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<ProjectDetailsViewModel>> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CommandResult>> Update(ProjectUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

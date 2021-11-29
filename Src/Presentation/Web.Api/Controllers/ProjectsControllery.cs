592092d7248512fbff44fbc95bfb1dbdb33919ca
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-29 23:37:36 +0330

    add webservice contract for projects

--- Src/Presentation/Web.Api/Controllers/ProjectsControllery.cs
+++ Src/Presentation/Web.Api/Controllers/ProjectsControllery.cs
@@ -9,11 +9,12 @@ using TaskoMask.Application.Core.Commands;
 using TaskoMask.Application.Core.Helpers;
 using TaskoMask.Application.Core.ViewModels;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using TaskoMask.Web.Common.Contracts;
 
 namespace TaskoMask.Web.Api.Controllers
 {
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class ProjectsController : BaseApiController
+    public class ProjectsController : BaseApiController, IProjectWebService
     {
         #region Fields
 

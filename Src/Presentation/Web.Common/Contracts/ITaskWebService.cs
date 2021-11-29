54249a4dcc70dde7bbf24d34f540ccc21c18e276
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-30 00:00:13 +0330

    implement task webservice methods

--- Src/Presentation/Web.Common/Contracts/ITaskWebService.cs
+++ Src/Presentation/Web.Common/Contracts/ITaskWebService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Workspace.Tasks;
+using TaskoMask.Application.Core.Helpers;
 
 namespace TaskoMask.Web.Common.Contracts
 {
     public interface ITaskWebService
     {
+        Task<Result<CommandResult>> Create(TaskUpsertDto input);
+        Task<Result<CommandResult>> SetCardId(string taskId, string cardId);
+        Task<Result<CommandResult>> Update(TaskUpsertDto input);
     }
 }
--- Src/Presentation/Web.Panel/Data/TaskWebService.cs
+++ Src/Presentation/Web.Panel/Data/TaskWebService.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Workspace.Tasks;
+using TaskoMask.Application.Core.Helpers;
 using TaskoMask.Web.Common.Contracts;
 
 namespace TaskoMask.Web.Panel.Data
 {
-    public class TaskWebService: ITaskWebService
+    public class TaskWebService : ITaskWebService
     {
+        public Task<Result<CommandResult>> Create(TaskUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CommandResult>> SetCardId(string taskId, string cardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CommandResult>> Update(TaskUpsertDto input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

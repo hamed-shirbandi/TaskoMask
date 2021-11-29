b4198dc8f89fae851936277c50627f3f68d21375
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-29 23:59:39 +0330

    implement member webservice methods

--- Src/Presentation/Web.Common/Contracts/IMemberWebService.cs
+++ Src/Presentation/Web.Common/Contracts/IMemberWebService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Team.Members;
+using TaskoMask.Application.Core.Helpers;
 
 namespace TaskoMask.Web.Common.Contracts
 {
     public interface IMemberWebService
     {
+        Task<Result<CommandResult>> Add(string email);
+        Task<Result<CommandResult>> Delete(string id);
+        Task<Result<MemberBasicInfoDto>> Get(string id);
+        Task<Result<CommandResult>> Update();
     }
 }
--- Src/Presentation/Web.Panel/Data/MemberWebService.cs
+++ Src/Presentation/Web.Panel/Data/MemberWebService.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Application.Core.Dtos.Team.Members;
+using TaskoMask.Application.Core.Helpers;
 using TaskoMask.Web.Common.Contracts;
 
 namespace TaskoMask.Web.Panel.Data
 {
     public class MemberWebService : IMemberWebService
     {
+        public Task<Result<CommandResult>> Add(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CommandResult>> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<MemberBasicInfoDto>> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<CommandResult>> Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

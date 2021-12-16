e5c5a4c23c050e540580d747a8c3631bead04cfb
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-12-16 18:48:45 +0330

    implement card client services

--- Src/Presentation/3-UI/UserPanel/Data/CardClientService.cs
+++ Src/Presentation/3-UI/UserPanel/Data/CardClientService.cs
@@ -2,32 +2,72 @@
 using TaskoMask.Application.Share.Helpers;
 using TaskoMask.Application.Share.ViewModels;
 using TaskoMask.Presentation.Framework.Share.Contracts;
+using TaskoMask.Presentation.Framework.Share.Helpers;
 using TaskoMask.Presentation.Framework.Share.Services.Http;
 
 namespace TaskoMask.Presentation.UI.UserPanel.Data
 {
     public class CardClientService : ICardClientService
     {
+        #region Fields
+
         private readonly IHttpClientServices _httpClientServices;
+
+        #endregion
+
+        #region Ctor
 
         public CardClientService(IHttpClientServices httpClientServices)
         {
             _httpClientServices = httpClientServices;
         }
 
-        public Task<Result<CommandResult>> Create(CardUpsertDto input)
+        #endregion
+
+        #region Public Methods
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CardDetailsViewModel>> Get(string id)
         {
-            throw new NotImplementedException();
+            var uri = new ClientUriBuilder(new Uri(_httpClientServices.GetBaseAddress(), $"/cards"))
+                .AddParameter("id", id)
+                .Uri;
+
+            return await _httpClientServices.GetAsync<CardDetailsViewModel>(uri);
         }
 
-        public Task<Result<CardDetailsViewModel>> Get(string id)
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CommandResult>> Create(CardUpsertDto input)
         {
-            throw new NotImplementedException();
+            var uri = new ClientUriBuilder(new Uri(_httpClientServices.GetBaseAddress(), $"/cards")).Uri;
+            return await _httpClientServices.PostAsync<CommandResult>(uri, input);
         }
 
-        public Task<Result<CommandResult>> Update(CardUpsertDto input)
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CommandResult>> Update(CardUpsertDto input)
         {
-            throw new NotImplementedException();
+            var uri = new ClientUriBuilder(new Uri(_httpClientServices.GetBaseAddress(), $"/cards")).Uri;
+            return await _httpClientServices.PutAsync<CommandResult>(uri, input);
         }
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
     }
 }

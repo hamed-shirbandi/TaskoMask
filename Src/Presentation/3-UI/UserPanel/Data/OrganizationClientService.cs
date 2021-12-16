362d7b266671f3686792c8f6e939647a8e185598
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-12-16 18:50:02 +0330

    implement organization client services

--- Src/Presentation/3-UI/UserPanel/Data/OrganizationClientService.cs
+++ Src/Presentation/3-UI/UserPanel/Data/OrganizationClientService.cs
@@ -2,32 +2,72 @@
 using TaskoMask.Application.Share.Helpers;
 using TaskoMask.Application.Share.ViewModels;
 using TaskoMask.Presentation.Framework.Share.Contracts;
+using TaskoMask.Presentation.Framework.Share.Helpers;
 using TaskoMask.Presentation.Framework.Share.Services.Http;
 
 namespace TaskoMask.Presentation.UI.UserPanel.Data
 {
     public class OrganizationClientService : IOrganizationClientService
     {
+        #region Fields
+
         private readonly IHttpClientServices _httpClientServices;
+
+        #endregion
+
+        #region Ctor
 
         public OrganizationClientService(IHttpClientServices httpClientServices)
         {
             _httpClientServices = httpClientServices;
         }
 
-        public Task<Result<CommandResult>> Create(OrganizationUpsertDto input)
+        #endregion
+
+        #region Public Methods
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<OrganizationDetailsViewModel>> Get(string id)
         {
-            throw new NotImplementedException();
+            var uri = new ClientUriBuilder(new Uri(_httpClientServices.GetBaseAddress(), $"/organizations"))
+                .AddParameter("id", id)
+                .Uri;
+
+            return await _httpClientServices.GetAsync<OrganizationDetailsViewModel>(uri);
         }
 
-        public Task<Result<OrganizationDetailsViewModel>> Get(string id)
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CommandResult>> Create(OrganizationUpsertDto input)
         {
-            throw new NotImplementedException();
+            var uri = new ClientUriBuilder(new Uri(_httpClientServices.GetBaseAddress(), $"/organizations")).Uri;
+            return await _httpClientServices.PostAsync<CommandResult>(uri, input);
         }
 
-        public Task<Result<CommandResult>> Update(OrganizationUpsertDto input)
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CommandResult>> Update(OrganizationUpsertDto input)
         {
-            throw new NotImplementedException();
+            var uri = new ClientUriBuilder(new Uri(_httpClientServices.GetBaseAddress(), $"/organizations")).Uri;
+            return await _httpClientServices.PutAsync<CommandResult>(uri, input);
         }
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
     }
 }

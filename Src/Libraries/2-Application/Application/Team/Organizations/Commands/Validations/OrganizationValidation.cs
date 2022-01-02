3d1592e39283ffc1f1d9e6dc5637efac8e98d036
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-01-02 21:07:38 +0330

    remove extra usings

--- Src/Libraries/2-Application/Application/Team/Organizations/Commands/Validations/OrganizationValidation.cs
+++ Src/Libraries/2-Application/Application/Team/Organizations/Commands/Validations/OrganizationValidation.cs
@@ -1,9 +1,4 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaskoMask.Application.Team.Organizations.Commands.Models;
 using TaskoMask.Application.Share.Resources;
 
--- Src/Libraries/2-Application/Application/Team/Organizations/Commands/Validations/UpdateOrganizationCommandValidation.cs
+++ Src/Libraries/2-Application/Application/Team/Organizations/Commands/Validations/UpdateOrganizationCommandValidation.cs
@@ -1,11 +1,4 @@
-﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TaskoMask.Application.Team.Organizations.Commands.Models;
-using TaskoMask.Application.Share.Resources;
+﻿using TaskoMask.Application.Team.Organizations.Commands.Models;
 
 namespace TaskoMask.Application.Team.Organizations.Commands.Validations
 {

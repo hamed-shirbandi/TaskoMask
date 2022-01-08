ea61a81fac2cfa0e7daa819c06e164d3a0b16c32
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-01-09 01:36:10 +0330

    add a builder for project entity

---
+++ Src/Libraries/3-Domain/Domain/Team/Entities/Projects/OrganizationBuilder.cs
@@ -0,0 +1,85 @@
+﻿using TaskoMask.Domain.Team.Entities.Projects.ValueObjects;
+
+namespace TaskoMask.Domain.Team.Entities.Projects
+{
+    public class ProjectBuilder
+    {
+        #region Properties
+
+        public ProjectName Name { get; private set; }
+        public ProjectDescription Description { get; private set; }
+        public ProjectOrganizationId OrganizationId { get; private set; }
+
+        #endregion
+
+        #region Ctors
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private ProjectBuilder()
+        {
+        }
+
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static ProjectBuilder Init()
+        {
+            return new ProjectBuilder();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ProjectBuilder WithName(string name)
+        {
+            Name = ProjectName.Create(name);
+            return this;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ProjectBuilder WithDescription(string description)
+        {
+            Description = ProjectDescription.Create(description);
+            return this;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ProjectBuilder WithOrganizationId(string organizationId)
+        {
+            OrganizationId = ProjectOrganizationId.Create(organizationId);
+            return this;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Project Build() => Project.Create(Name, Description, OrganizationId);
+
+
+        #endregion
+    }
+}

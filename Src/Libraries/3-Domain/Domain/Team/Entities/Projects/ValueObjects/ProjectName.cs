ffed01cc825dee1c3f360a5c57ea8d5aad7abbd2
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-01-09 01:35:50 +0330

    add value objects for project entity

---
+++ Src/Libraries/3-Domain/Domain/Team/Entities/Projects/ValueObjects/ProjectName.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskoMask.Domain.Core.Exceptions;
+using TaskoMask.Domain.Core.Models;
+using TaskoMask.Domain.Share.Resources;
+
+namespace TaskoMask.Domain.Team.Entities.Projects.ValueObjects
+{
+    public class ProjectName : BaseValueObject
+    {
+        #region Properties
+
+        public string Value { get; private set; }
+
+
+        #endregion
+
+        #region Ctors
+
+        public ProjectName(string value)
+        {
+            Value = value;
+
+            CheckPolicies();
+        }
+
+        #endregion
+
+        #region  Methods
+
+
+
+        /// <summary>
+        /// Factory method for creating new object
+        /// </summary>
+        public static ProjectName Create(string value)
+        {
+            return new ProjectName(value);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void CheckPolicies()
+        {
+            if (string.IsNullOrEmpty(Value))
+                throw new DomainException(string.Format(DomainMessages.Required, nameof(ProjectName)));
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+
+
+        #endregion
+
+    }
+}

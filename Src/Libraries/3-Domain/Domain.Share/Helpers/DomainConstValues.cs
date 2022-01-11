ff9be58a193c1a80e4696793ba6a997fb7864cc5
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-01-11 18:53:00 +0330

    add a class to hold are const values for domain validations

---
+++ Src/Libraries/3-Domain/Domain.Share/Helpers/DomainConstValues.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace TaskoMask.Domain.Share.Helpers
+{
+    public static class DomainConstValues
+    {
+        public const int Organization_Name_Min_Length = 2;
+        public const int Organization_Name_Max_Length = 50;
+
+        public const int Organization_Description_Min_Length = 2;
+        public const int Organization_Description_Max_Length = 50;
+
+
+        public const int Project_Name_Min_Length = 2;
+        public const int Project_Name_Max_Length = 50;
+
+        public const int Project_Description_Min_Length = 2;
+        public const int Project_Description_Max_Length = 50;
+
+
+        public const int Board_Name_Min_Length = 2;
+        public const int Board_Name_Max_Length = 50;
+
+        public const int Board_Description_Min_Length = 2;
+        public const int Board_Description_Max_Length = 50;
+
+
+        public const int Card_Name_Min_Length = 2;
+        public const int Card_Name_Max_Length = 50;
+
+        public const int Card_Description_Min_Length = 2;
+        public const int Card_Description_Max_Length = 50;
+
+
+        public const int Task_Title_Min_Length = 2;
+        public const int Task_Title_Max_Length = 50;
+
+        public const int Task_Description_Min_Length = 2;
+        public const int Task_Description_Max_Length = 250;
+    }
+}

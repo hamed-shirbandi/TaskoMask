53ef56f57b097c6ea4063e97e5363c1ab76163b2
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-01-18 00:51:23 +0330

    add new entities to task

---
+++ Src/Libraries/3-Domain/Domain/Workspace/Tasks/Entities/Activity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Domain.Core.Models;
+
+namespace TaskoMask.Domain.Workspace.Tasks.Entities
+{
+    /// <summary>
+    /// All task activities like moving between cards, delete and ...
+    /// </summary>
+    public class Activity:BaseEntity
+    {
+    }
+}
---
+++ Src/Libraries/3-Domain/Domain/Workspace/Tasks/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Domain.Core.Models;
+
+namespace TaskoMask.Domain.Workspace.Tasks.Entities
+{
+    /// <summary>
+    /// Every board member can leave comment on tasks
+    /// </summary>
+    public class Comment: BaseEntity
+    {
+    }
+}

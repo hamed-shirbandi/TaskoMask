dfe5374fc5ac6cc595ef8d1fd6dc89afa20bf1d7
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-01-22 02:43:25 +0330

    add custom Id to Owner read data model

--- Src/Libraries/3-Domain/Domain/ReadModel/Entities/Owner.cs
+++ Src/Libraries/3-Domain/Domain/ReadModel/Entities/Owner.cs
@@ -1,4 +1,6 @@
-﻿using TaskoMask.Domain.Core.Models;
+﻿using TaskoMask.Domain.Core.Exceptions;
+using TaskoMask.Domain.Core.Models;
+using TaskoMask.Domain.Share.Resources;
 
 namespace TaskoMask.Domain.ReadModel.Entities
 {
@@ -7,6 +9,13 @@ namespace TaskoMask.Domain.ReadModel.Entities
     /// </summary>
     public class Owner : BaseEntity
     {
+        public Owner(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new DomainException(string.Format(DomainMessages.Null_Reference_Error, nameof(id)));
+
+            base.SetId(id);
+        }
         public string UserName { get; set; }
         public bool IsActive { get; set; }
         public string DisplayName { get; set; }

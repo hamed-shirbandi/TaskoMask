76f2136df49939e6c210286fee9f270a520182b5
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-01-22 17:21:17 +0330

    add owner domain event handlers

---
+++ Src/Libraries/2-Application/Application/Workspace/Owners/EventHandlers/OwnerEventHandles.cs
@@ -0,0 +1,104 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskoMask.Domain.ReadModel.Data;
+using TaskoMask.Domain.ReadModel.Entities;
+using TaskoMask.Domain.WriteModel.Authorization.Data;
+using TaskoMask.Domain.WriteModel.Workspace.Owners.Events.Owners;
+
+namespace TaskoMask.Application.Workspace.Owners.EventHandlers
+{
+    public class OwnerEventHandles : 
+        INotificationHandler<OwnerCreatedEvent>,
+        INotificationHandler<OwnerUpdatedEvent>,
+        INotificationHandler<OwnerDeletedEvent>,
+        INotificationHandler<OwnerRecycledEvent>
+    {
+        #region Fields
+
+        private readonly IOwnerRepository _ownerRepository;
+        private readonly IUserRepository _userRepository;
+
+        #endregion
+
+        #region Ctors
+
+        public OwnerEventHandles(IOwnerRepository ownerRepository, IUserRepository userRepository)
+        {
+            _ownerRepository = ownerRepository;
+            _userRepository = userRepository;
+        }
+
+        #endregion
+
+        #region Handlers
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task Handle(OwnerCreatedEvent createdOwner, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(createdOwner.Id);
+
+            var owner = new Owner(createdOwner.Id)
+            {
+                DisplayName= createdOwner.DisplayName,
+                Email= createdOwner.Email,
+                UserName= user.UserName,
+                IsActive= user.IsActive,
+            };
+           await _ownerRepository.CreateAsync(owner);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task Handle(OwnerUpdatedEvent updatedOwner, CancellationToken cancellationToken)
+        {
+            var owner = await _ownerRepository.GetByIdAsync(updatedOwner.Id);
+            var user = await _userRepository.GetByIdAsync(updatedOwner.Id);
+
+            owner.DisplayName = updatedOwner.DisplayName;
+            owner.Email = updatedOwner.Email;
+            owner.UserName = user.UserName;
+
+            await _ownerRepository.UpdateAsync(owner);
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task Handle(OwnerDeletedEvent deletedOwner, CancellationToken cancellationToken)
+        {
+            var owner = await _ownerRepository.GetByIdAsync(deletedOwner.Id);
+            owner.Delete();
+            await _ownerRepository.UpdateAsync(owner);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task Handle(OwnerRecycledEvent recycledOwner, CancellationToken cancellationToken)
+        {
+            var owner = await _ownerRepository.GetByIdAsync(recycledOwner.Id);
+            owner.Recycle();
+            await _ownerRepository.UpdateAsync(owner);
+        }
+
+        #endregion
+
+
+
+
+
+
+    }
+}

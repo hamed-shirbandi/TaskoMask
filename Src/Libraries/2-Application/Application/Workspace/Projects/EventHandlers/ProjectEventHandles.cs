f1888adcfd047c16d7a930b902c60797f0d6560b
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-01-24 01:00:35 +0330

    implement task project event handlers

---
+++ Src/Libraries/2-Application/Application/Workspace/Projects/EventHandlers/ProjectEventHandles.cs
@@ -0,0 +1,97 @@
+﻿using MediatR;
+using System.Threading;
+using TaskoMask.Domain.ReadModel.Data;
+using TaskoMask.Domain.ReadModel.Entities;
+using TaskoMask.Domain.WriteModel.Workspace.Owners.Events.Projects;
+
+namespace TaskoMask.Application.Workspace.Projects.EventHandlers
+{
+    public class ProjectEventHandles : 
+        INotificationHandler<ProjectCreatedEvent>,
+        INotificationHandler<ProjectUpdatedEvent>,
+        INotificationHandler<ProjectDeletedEvent>,
+        INotificationHandler<ProjectRecycledEvent>
+    {
+        #region Fields
+
+        private readonly IProjectRepository _projectRepository;
+
+        #endregion
+
+        #region Ctors
+
+        public ProjectEventHandles(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        #endregion
+
+        #region Handlers
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task Handle(ProjectCreatedEvent createdProject, CancellationToken cancellationToken)
+        {
+            var project = new Project(createdProject.Id)
+            {
+                Name= createdProject.Name,
+                Description= createdProject.Description,
+                OrganizationId= createdProject.OrganizationId,
+            };
+           await _projectRepository.CreateAsync(project);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task Handle(ProjectUpdatedEvent updatedProject, CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository.GetByIdAsync(updatedProject.Id);
+
+            project.Name = updatedProject.Name;
+            project.Description = updatedProject.Description;
+
+            project.SetAsUpdated();
+
+            await _projectRepository.UpdateAsync(project);
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task Handle(ProjectDeletedEvent deletedProject, CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository.GetByIdAsync(deletedProject.Id);
+            project.SetAsDeleteed();
+            await _projectRepository.UpdateAsync(project);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task Handle(ProjectRecycledEvent recycledProject, CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository.GetByIdAsync(recycledProject.Id);
+            project.SetAsRecycled();
+            await _projectRepository.UpdateAsync(project);
+        }
+
+        #endregion
+
+
+
+
+
+
+    }
+}

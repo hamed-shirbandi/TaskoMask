f13b9adfd75cda81f524ec03feb1a64998c15cbd
Amir Adelipour <amir.adelipour@gmail.com>
2022-04-08 18:52:23 +0430

    Added UseSerilog to operate correctly

--- Src/Presentation/3-UI/AdminPanel/Program.cs
+++ Src/Presentation/3-UI/AdminPanel/Program.cs
@@ -9,6 +9,7 @@ Log.Information("Starting up");
 try
 {
     var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog(((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration)));
     builder.Services.AddMvcProjectConfigureServices(builder.Configuration, builder.Environment);
     var app = builder.Build();
     app.UseSerilogRequestLogging();

8463769e3788738a93b8545420caf1682ab1213f
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-23 00:24:02 +0430

    first test for board

--- src/Tests/Unit/Domain.Tests.Unit/BoardUnitTests.cs
+++ src/Tests/Unit/Domain.Tests.Unit/BoardUnitTests.cs
@@ -1,16 +1,43 @@
-﻿using System;
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskoMask.Domain.WriteModel.Workspace.Boards.Entities;
+using TaskoMask.Domain.WriteModel.Workspace.Boards.Services;
 using Xunit;
 
 namespace TaskoMask.Domain.Tests.Unit
 {
     public class BoardUnitTests
     {
-      
+        public BoardUnitTests()
+        {
+
+        }
+
+
+        [Fact]
+        public void Create_Board()
+        {
+            //Arrange
+            var name = "Test Name";
+            var description = "Test Description";
+            var projectId = "No_Id";
+            var boardValidatorService = Substitute.For<IBoardValidatorService>();
+            boardValidatorService.BoardHasUniqueName(Arg.Any<string>(), projectId, name).Returns(true);
+
+            //Act
+            var bord =  Board.CreateBoard(name,description,projectId, boardValidatorService);
+
+
+            //Assert
+            bord.Name.Value.Should().NotBeNull().And.Be(name);
+            bord.Description.Value.Should().NotBeNull().And.Be(description);
+            bord.ProjectId.Value.Should().NotBeNull().And.Be(projectId);
+        }
 
     }
 }

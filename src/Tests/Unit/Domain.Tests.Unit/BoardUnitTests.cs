3a7effcbde71b64a22e328840c52d8ded61438dc
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-22 23:58:26 +0430

    add a test class for Board

---
+++ src/Tests/Unit/Domain.Tests.Unit/BoardUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Domain.WriteModel.Workspace.Boards.Entities;
+using Xunit;
+
+namespace TaskoMask.Domain.Tests.Unit
+{
+    public class BoardUnitTests
+    {
+      
+
+    }
+}

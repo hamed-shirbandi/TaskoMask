159606a48675a60bed5521f76cd47092d51e0e15
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-23 15:23:50 +0430

    add a test class for owner aggregate

---
+++ Src/Tests/Unit/Domain.Tests.Unit/OwnerAggregateUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace TaskoMask.Domain.Tests.Unit
+{
+    public class OwnerAggregateUnitTests
+    {
+        public OwnerAggregateUnitTests()
+        {
+
+        }
+
+
+    }
+}

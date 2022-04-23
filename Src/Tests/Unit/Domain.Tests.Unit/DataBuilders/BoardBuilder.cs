26d43a1e34d70dff08d72f48542351c7da0ea9b1
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-24 01:50:52 +0430

    add a test data builder for board

---
+++ Src/Tests/Unit/Domain.Tests.Unit/DataBuilders/BoardBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Domain.WriteModel.Workspace.Boards.Entities;
+using TaskoMask.Domain.WriteModel.Workspace.Boards.Services;
+using TaskoMask.Domain.WriteModel.Workspace.Owners.Entities;
+
+namespace TaskoMask.Domain.Tests.Unit.DataBuilders
+{
+    internal class BoardBuilder
+    {
+        private  IBoardValidatorService _boardValidatorService;
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string ProjectId { get; private set; }
+
+
+        private BoardBuilder(IBoardValidatorService boardValidatorService)
+        {
+            _boardValidatorService = boardValidatorService;
+        }
+
+
+        public static BoardBuilder Init(IBoardValidatorService boardValidatorService)
+        {
+            return new BoardBuilder(boardValidatorService);
+        }
+
+
+        public BoardBuilder WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+
+
+        public BoardBuilder WithDescription(string description)
+        {
+            Description = description;
+            return this;
+        }
+
+
+        public BoardBuilder WithProjectId(string projectId)
+        {
+            ProjectId = projectId;
+            return this;
+        }
+
+
+
+        public Board Build()
+        {
+            return Board.CreateBoard(Name, ProjectId, Description,_boardValidatorService);
+        }
+
+
+    }
+}

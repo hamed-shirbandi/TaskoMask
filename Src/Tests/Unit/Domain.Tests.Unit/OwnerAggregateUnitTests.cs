e801b2d1cdd6644bf2fa357cfe456b50733d54ed
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-23 15:42:02 +0430

    add a test to ensure owner aggregate is constructed

--- Src/Tests/Unit/Domain.Tests.Unit/OwnerAggregateUnitTests.cs
+++ Src/Tests/Unit/Domain.Tests.Unit/OwnerAggregateUnitTests.cs
@@ -1,5 +1,7 @@
 ﻿using FluentAssertions;
+using MongoDB.Bson;
 using NSubstitute;
+using TaskoMask.Domain.WriteModel.Workspace.Owners.Entities;
 using Xunit;
 
 namespace TaskoMask.Domain.Tests.Unit
@@ -11,6 +13,26 @@ namespace TaskoMask.Domain.Tests.Unit
 
         }
 
+
+        [Fact]
+        public void Owner_Is_Constructed_Properly()
+        {
+            //Arrange
+            var userId = ObjectId.GenerateNewId().ToString();
+            var email = "Test@email.com";
+            var displayName = "Test Name";
+
+            //Act
+            var owner = Owner.CreateOwner(userId, displayName, email);
+
+
+            //Assert
+            owner.Id.Should().NotBeNullOrEmpty().And.Be(userId);
+            owner.Email.Value.Should().NotBeNull().And.Be(email);
+            owner.DisplayName.Value.Should().NotBeNull().And.Be(displayName);
+            owner.Organizations.Should().HaveCount(0);
+
+        }
 
     }
 }

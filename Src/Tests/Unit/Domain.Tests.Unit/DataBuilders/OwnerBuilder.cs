d41ca64cce4110063094ef09d4d1b9f79db21b81
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-24 01:45:15 +0430

    add a test data builder for owner

---
+++ Src/Tests/Unit/Domain.Tests.Unit/DataBuilders/OwnerBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Domain.WriteModel.Workspace.Owners.Entities;
+
+namespace TaskoMask.Domain.Tests.Unit.DataBuilders
+{
+    internal class OwnerBuilder
+    {
+        public string Id { get; private set; }
+        public string Email { get; private set; }
+        public string DisplayName { get; private set; }
+
+
+        private OwnerBuilder()
+        {
+
+        }
+
+
+        public static OwnerBuilder Init()
+        {
+            return new OwnerBuilder();
+        }
+
+
+        public OwnerBuilder WithId(string id)
+        {
+            Id = id;
+            return this;
+        }
+
+
+
+        public OwnerBuilder WithEmail(string email)
+        {
+            Email = email;
+            return this;
+        }
+
+
+        public OwnerBuilder WithDisplayName(string displayName)
+        {
+            DisplayName = displayName;
+            return this;
+        }
+
+
+
+        public Owner Build()
+        {
+            return Owner.CreateOwner(Id, DisplayName, Email);
+        }
+
+
+    }
+}

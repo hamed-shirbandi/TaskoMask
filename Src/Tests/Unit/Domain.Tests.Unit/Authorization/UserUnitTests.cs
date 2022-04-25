e52aeeb4a4bc77e1c927e1660eb69f93fd4dc4dd
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-26 02:03:34 +0430

    add a test class for user domain model

---
+++ Src/Tests/Unit/Domain.Tests.Unit/Authorization/UserUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using MongoDB.Bson;
+using NSubstitute;
+using System;
+using System.Linq;
+using TaskoMask.Domain.Core.Exceptions;
+using TaskoMask.Domain.Share.Resources;
+using TaskoMask.Domain.Tests.Unit.DataBuilders;
+using Xunit;
+
+namespace TaskoMask.Domain.Tests.Unit.Authorization
+{
+    public class UserUnitTests
+    {
+        public UserUnitTests()
+        {
+
+        }
+
+
+
+    }
+}

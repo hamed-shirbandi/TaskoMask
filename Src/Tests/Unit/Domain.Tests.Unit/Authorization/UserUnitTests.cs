ed1c67a799bf917ad5844ed1fd537d09e12fe216
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-26 02:38:15 +0430

    add a test to ensure user is deleted properly

--- Src/Tests/Unit/Domain.Tests.Unit/Authorization/UserUnitTests.cs
+++ Src/Tests/Unit/Domain.Tests.Unit/Authorization/UserUnitTests.cs
@@ -1,11 +1,8 @@
 ﻿using FluentAssertions;
 using MongoDB.Bson;
-using NSubstitute;
 using System;
-using System.Linq;
-using TaskoMask.Domain.Core.Exceptions;
-using TaskoMask.Domain.Share.Resources;
 using TaskoMask.Domain.Tests.Unit.DataBuilders;
+using TaskoMask.Domain.WriteModel.Authorization.Entities;
 using Xunit;
 
 namespace TaskoMask.Domain.Tests.Unit.Authorization
@@ -16,6 +13,75 @@ namespace TaskoMask.Domain.Tests.Unit.Authorization
         {
 
         }
+
+
+        [Fact]
+        public void User_Is_Constructed_Properly()
+        {
+            //Arrange
+            var userName = "TestUserName";
+
+
+            //Act
+            var user = new User
+            {
+                UserName = userName,
+                IsActive = true,
+            };
+
+
+            //Assert
+            user.Id.Should().NotBeNullOrEmpty();
+            user.UserName.Should().Be(userName);
+            user.IsActive.Should().Be(true);
+
+        }
+
+
+        [Fact]
+        public void User_Is_Updated_Properly()
+        {
+            //Arrange
+            var userName = "TestUserName";
+
+
+            //Act
+            var user = new User
+            {
+                UserName = userName,
+            };
+
+            var specifiedModifiedDateTime = user.CreationTime.ModifiedDateTime;
+
+            user.SetAsUpdated();
+
+            //Assert
+            user.CreationTime.ModifiedDateTime.Should().BeAfter(specifiedModifiedDateTime);
+
+        }
+
+
+
+        [Fact]
+        public void User_Is_Deleted_Properly()
+        {
+            //Arrange
+            var userName = "TestUserName";
+
+
+            //Act
+            var user = new User
+            {
+                UserName = userName,
+            };
+
+            user.SetAsDeleted();
+
+            //Assert
+            user.IsDeleted.Should().Be(true);
+
+        }
+
 
 
 

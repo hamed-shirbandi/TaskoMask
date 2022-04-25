132958f41ffc9b44eb81c7d0dc01744d81544ccc
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-26 02:56:02 +0430

    add a test to ensure user is created by UserService

--- Src/Tests/Unit/Application.Tests.Unit/Authorization/UserServiceUnitTests.cs
+++ Src/Tests/Unit/Application.Tests.Unit/Authorization/UserServiceUnitTests.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskoMask.Application.Authorization.Users.Services;
+using TaskoMask.Application.Core.Bus;
+using TaskoMask.Application.Core.Notifications;
+using TaskoMask.Domain.Core.Services;
+using TaskoMask.Domain.WriteModel.Authorization.Data;
+using TaskoMask.Domain.WriteModel.Authorization.Entities;
 using Xunit;
 
 namespace TaskoMask.Application.Tests.Unit.Authorization
@@ -15,6 +23,27 @@ namespace TaskoMask.Application.Tests.Unit.Authorization
 
         }
 
-       
+        [Fact]
+        public async void User_Is_Created_Properly()
+        {
+            //Arrange
+            var dummyInMemoryBus = Substitute.For<IInMemoryBus>();
+            var dummyIMapper = Substitute.For<IMapper>();
+            var dummyDomainNotificationHandler = Substitute.For<IDomainNotificationHandler>();
+            var dummyEncryptionService = Substitute.For<IEncryptionService>();
+            var userRepositoryStub = Substitute.For<IUserRepository>();
+            userRepositoryStub.CreateAsync(Arg.Any<User>()).Returns(Task.CompletedTask);
+            var userService = new UserService(dummyInMemoryBus, dummyIMapper, dummyDomainNotificationHandler, userRepositoryStub, dummyEncryptionService);
+
+
+            //Act
+            var result = await userService.CreateAsync("TestUserName", "TestPassword");
+
+
+            //Asserrt
+            result.IsSuccess.Should().Be(true);
+            result.Value.EntityId.Should().NotBeNullOrEmpty();
+        }
+
     }
 }

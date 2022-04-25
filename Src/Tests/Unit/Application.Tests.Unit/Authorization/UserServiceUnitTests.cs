7fb3311456797bbb56feaade8fd5fd8f9ca18656
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-26 02:43:01 +0430

    add a test class for UserService

---
+++ Src/Tests/Unit/Application.Tests.Unit/Authorization/UserServiceUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Application.Authorization.Users.Services;
+using Xunit;
+
+namespace TaskoMask.Application.Tests.Unit.Authorization
+{
+    public class UserServiceUnitTests
+    {
+        public UserServiceUnitTests()
+        {
+
+        }
+
+       
+    }
+}

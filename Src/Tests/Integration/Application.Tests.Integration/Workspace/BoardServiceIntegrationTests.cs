30ff81e8a74f686436a6c0a15366a6618f49182d
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-26 16:17:37 +0430

    move the test class to related folder

--- Src/Tests/Integration/Application.Tests.Integration/Workspace/BoardServiceIntegrationTests.cs
+++ Src/Tests/Integration/Application.Tests.Integration/Workspace/BoardServiceIntegrationTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Tests.Integration
+namespace TaskoMask.Application.Tests.Integration.Workspace
 {
     public class BoardServiceIntegrationTests
     {

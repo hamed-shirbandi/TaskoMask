72ad14a1b0a349e288cdc3da2fc3763353b31a7e
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-26 16:18:06 +0430

    add a test class for UserServuce integration tests

---
+++ Src/Tests/Integration/Application.Tests.Integration/Authorization/UserServiceIntegrationTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TaskoMask.Application.Tests.Integration.Workspace
+{
+    public class UserServiceIntegrationTests
+    {
+        public UserServiceIntegrationTests()
+        {
+
+        }
+
+      
+
+    }
+}

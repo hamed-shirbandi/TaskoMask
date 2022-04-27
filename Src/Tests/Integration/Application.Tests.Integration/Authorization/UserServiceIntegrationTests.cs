c1c284a57cbaf195f00261cd70805ebb574f4c96
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-28 01:23:18 +0430

    add TestsBase for Application.Tests.Integration

--- Src/Tests/Integration/Application.Tests.Integration/Authorization/UserServiceIntegrationTests.cs
+++ Src/Tests/Integration/Application.Tests.Integration/Authorization/UserServiceIntegrationTests.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TaskoMask.Application.Tests.Integration.TestData;
 using Xunit;
 
 namespace TaskoMask.Application.Tests.Integration.Workspace
 {
-    public class UserServiceIntegrationTests
+    public class UserServiceIntegrationTests : TestsBase
     {
         public UserServiceIntegrationTests()
         {
 
         }
-
-
 
     }
 }
--- Src/Tests/Integration/Application.Tests.Integration/Workspace/BoardServiceIntegrationTests.cs
+++ Src/Tests/Integration/Application.Tests.Integration/Workspace/BoardServiceIntegrationTests.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Application.Tests.Integration.TestData;
 
 namespace TaskoMask.Application.Tests.Integration.Workspace
 {
-    public class BoardServiceIntegrationTests
+    public class BoardServiceIntegrationTests : TestsBase
     {
     }
 }

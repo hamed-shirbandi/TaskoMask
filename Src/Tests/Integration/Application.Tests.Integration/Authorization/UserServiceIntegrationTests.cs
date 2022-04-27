aff0ebc2c98d599cd18a63db9fad27f6d583b45a
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-28 02:41:23 +0430

    add User_Is_Created_Properly integration test method

--- Src/Tests/Integration/Application.Tests.Integration/Authorization/UserServiceIntegrationTests.cs
+++ Src/Tests/Integration/Application.Tests.Integration/Authorization/UserServiceIntegrationTests.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TaskoMask.Application.Authorization.Users.Services;
 using TaskoMask.Application.Tests.Integration.TestData;
 using Xunit;
 
@@ -9,10 +12,23 @@ namespace TaskoMask.Application.Tests.Integration.Workspace
 {
     public class UserServiceIntegrationTests : TestsBase
     {
+        private readonly IUserService _userService;
+        
         public UserServiceIntegrationTests()
         {
-
+            _userService = ServiceProvider.GetRequiredService<IUserService>();
         }
 
+
+        [Fact]
+        public async Task User_Is_Created_Properly()
+        {
+    
+            var result = await _userService.CreateAsync("TestUserName", "TestPass");
+            result.IsSuccess.Should().BeTrue();
+            result.Value.EntityId.Should().NotBeNull();
+        }
+
+    
     }
 }

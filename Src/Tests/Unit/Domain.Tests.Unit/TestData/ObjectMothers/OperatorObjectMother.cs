9d017cb0f95d437b847fcb39920281f02e473560
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-27 22:11:42 +0430

    add OperatorObjectMother

---
+++ Src/Tests/Unit/Domain.Tests.Unit/TestData/ObjectMothers/OperatorObjectMother.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Domain.WriteModel.Membership.Entities;
+
+namespace TaskoMask.Domain.Tests.Unit.TestData.ObjectMothers
+{
+    internal static class OperatorObjectMother
+    {
+
+
+        public static Operator CreateNewOperator()
+        {
+           return new Operator(ObjectId.GenerateNewId().ToString())
+            {
+                DisplayName = "TestOperatorName",
+               Email = "Test@mail.com",
+                
+            };
+        }
+
+
+
+        public static Operator CreateNewOperatorWithEmail(string email)
+        {
+            return new Operator(ObjectId.GenerateNewId().ToString())
+            {
+                DisplayName = "TestOperatorName",
+                Email = email,
+            };
+        }
+
+
+    }
+}

c13ed101fee7d0ce5e4facf8fa3de8b7c4877dec
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-28 01:56:35 +0430

    implement TestsBase for integration tests

--- Src/Tests/Integration/Application.Tests.Integration/TestData/TestsBase.cs
+++ Src/Tests/Integration/Application.Tests.Integration/TestData/TestsBase.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿using Infrastructure.CrossCutting.IoC;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskoMask.Infrastructure.Data.ReadModel.DataProviders;
+using TaskoMask.Infrastructure.Data.WriteModel.DataProviders;
+using TaskoMask.Presentation.Framework.Web.Configuration.Startup;
 
 namespace TaskoMask.Application.Tests.Integration.TestData
 {
     public class TestsBase
     {
+        protected IServiceProvider ServiceProvider { get; private set; }
+
+        public TestsBase()
+        {
+            ServiceProvider = GetServiceProvider();
+        }
+
+
+
+        private static IServiceProvider GetServiceProvider()
+        {
+            var services = new ServiceCollection();
+
+            var configuration = new ConfigurationBuilder()
+                                //Copy from AdminPanel project during the build event
+                                .AddJsonFile("appsettings.json", reloadOnChange: true, optional: false)
+                                .AddJsonFile("appsettings.Development.json", reloadOnChange: true, optional: false)
+                                .Build();
+
+            services.AddCommonConfigureServices(configuration);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            WriteDbInitialization.Initial(serviceProvider);
+            ReadDbInitialization.Initial(serviceProvider);
+            WriteDbSeedData.SeedEssentialData(serviceProvider);
+
+            return serviceProvider;
+        }
+
+
+
+        /// <summary>
+        /// Creates an IServiceScope which contains an IServiceProvider used to resolve dependencies from a newly created scope and then runs an associated callback.
+        /// </summary>
+        protected void RunScopedService<T, S>(IServiceProvider serviceProvider, Action<S, T> callback)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<S>();
+
+                callback(context, serviceScope.ServiceProvider.GetRequiredService<T>());
+                if (context is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Creates an IServiceScope which contains an IServiceProvider used to resolve dependencies from a newly created scope and then runs an associated callback.
+        /// </summary>
+        protected void RunScopedService<S>(IServiceProvider serviceProvider, Action<S> callback)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<S>();
+                callback(context);
+                if (context is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Creates an IServiceScope which contains an IServiceProvider used to resolve dependencies from a newly created scope and then runs an associated callback.
+        /// </summary>
+        protected T RunScopedService<T, S>(IServiceProvider serviceProvider, Func<S, T> callback)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<S>();
+                return callback(context);
+            }
+        }
+
+
     }
 }

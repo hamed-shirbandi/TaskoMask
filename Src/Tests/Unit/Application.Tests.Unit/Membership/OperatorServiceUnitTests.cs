106dbd24eeade679c692fe6d318f73d1b8caf06f
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-27 22:29:15 +0430

    add a test class for OperatorService

---
+++ Src/Tests/Unit/Application.Tests.Unit/Membership/OperatorServiceUnitTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskoMask.Application.Tests.Unit.Membership
+{
+    public class OperatorServiceUnitTests
+    {
+    }
+}

baaaa0818a15cc8e10cd347e8702bb9848863585
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-29 02:38:03 +0430

    add OwnerServiceIntegrationTests

---
+++ Src/Tests/Integration/Application.Tests.Integration/Workspace/OwnerServiceIntegrationTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Application.Tests.Integration.TestData;
+
+namespace TaskoMask.Application.Tests.Integration.Workspace
+{
+    public class OwnerServiceIntegrationTests:TestsBase
+    {
+    }
+}

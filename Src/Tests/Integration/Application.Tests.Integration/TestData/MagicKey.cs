031cec8716b577cc75d7d662f1d223b104d63632
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-29 19:22:48 +0430

    add MagicKey to hold const values

---
+++ Src/Tests/Integration/Application.Tests.Integration/TestData/MagicKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskoMask.Application.Tests.Integration.TestData
+{
+    public static class MagicKey
+    {
+        public static class User
+        {
+            public const string Created_User_Id = "Created_User_Id";
+        }
+    }
+}

18d194935629995c7849d7965523874ae26827fb
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-29 20:37:44 +0430

    configure shared fixture for owner and organization and project test classes

--- Src/Tests/Integration/Application.Tests.Integration/Workspace/OrganizationServiceIntegrationTests.cs
+++ Src/Tests/Integration/Application.Tests.Integration/Workspace/OrganizationServiceIntegrationTests.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using FluentAssertions;
 using System.Threading.Tasks;
+using TaskoMask.Application.Tests.Integration.TestData;
+using TaskoMask.Application.Workspace.Organizations.Services;
+using Xunit;
+using Xunit.Priority;
 
 namespace TaskoMask.Application.Tests.Integration.Workspace
 {
-    internal class OrganizationServiceIntegrationTests
+    [TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
+    [Collection("TestsBaseFixture collection")]
+    public class OrganizationServiceIntegrationTests
     {
+        #region Fields
+
+        private readonly IOrganizationService _organizationService;
+        private readonly TestsBaseFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public OrganizationServiceIntegrationTests(TestsBaseFixture fixture)
+        {
+            _fixture = fixture;
+            _organizationService = _fixture.GetRequiredService<IOrganizationService>();
+        }
+
+        #endregion
+
+        #region Test Mthods
+
+
+
+
+
+
+        #endregion
+
     }
 }
--- Src/Tests/Integration/Application.Tests.Integration/Workspace/ProjectServiceIntegrationTests.cs
+++ Src/Tests/Integration/Application.Tests.Integration/Workspace/ProjectServiceIntegrationTests.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using FluentAssertions;
 using System.Threading.Tasks;
+using TaskoMask.Application.Tests.Integration.TestData;
+using TaskoMask.Application.Workspace.Projects.Services;
+using Xunit;
+using Xunit.Priority;
 
 namespace TaskoMask.Application.Tests.Integration.Workspace
 {
-    internal class ProjectServiceIntegrationTests
+    [TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
+    [Collection("TestsBaseFixture collection")]
+    public class ProjectServiceIntegrationTests
     {
+        #region Fields
+
+        private readonly IProjectService _projectService;
+        private readonly TestsBaseFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public ProjectServiceIntegrationTests(TestsBaseFixture fixture)
+        {
+            _fixture = fixture;
+            _projectService = _fixture.GetRequiredService<IProjectService>();
+        }
+
+        #endregion
+
+        #region Test Mthods
+
+
+
+
+
+
+        #endregion
+
     }
 }

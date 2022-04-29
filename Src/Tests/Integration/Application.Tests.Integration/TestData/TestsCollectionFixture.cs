fd19052bb055aa8c26487fa1296f0660ee67559f
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-29 18:12:41 +0430

    add TestsCollectionFixture

---
+++ Src/Tests/Integration/Application.Tests.Integration/TestData/TestsCollectionFixture.cs
@@ -0,0 +1,13 @@
+﻿
+using Xunit;
+
+namespace TaskoMask.Application.Tests.Integration.TestData
+{
+    [CollectionDefinition("TestsBaseFixture collection")]
+    public class TestsCollectionFixture : ICollectionFixture<TestsBaseFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}

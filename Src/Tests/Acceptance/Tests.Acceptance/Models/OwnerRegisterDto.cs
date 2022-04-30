7aa214b4bb190ce4ab8522e85a1c5d01473557f2
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-01 01:19:17 +0430

    add a model to map data table from scenarios

---
+++ Src/Tests/Acceptance/Tests.Acceptance/Models/OwnerRegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskoMask.Tests.Acceptance.Models
+{
+    public class OwnerRegisterDto
+    {
+        public string DisplayName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}

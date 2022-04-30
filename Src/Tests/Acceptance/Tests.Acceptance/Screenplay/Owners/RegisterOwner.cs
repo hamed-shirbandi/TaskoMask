baaa2cadd41695c52413041dfe1fdc81cb6490fa
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-01 01:19:48 +0430

    add a task model for owner

---
+++ Src/Tests/Acceptance/Tests.Acceptance/Screenplay/Owners/RegisterOwner.cs
@@ -0,0 +1,23 @@
+﻿using Suzianna.Core.Screenplay;
+using Suzianna.Core.Screenplay.Actors;
+using Suzianna.Rest.Screenplay.Interactions;
+using TaskoMask.Tests.Acceptance.Models;
+
+namespace TaskoMask.Tests.Acceptance.Screenplay.Owners
+{
+    public class RegisterOwner : ITask
+    {
+        private readonly OwnerRegisterDto _ownerRegisterDto;
+
+        public RegisterOwner(OwnerRegisterDto ownerRegisterDto)
+        {
+            _ownerRegisterDto = ownerRegisterDto;
+        }
+
+
+        public void PerformAs<T>(T actor) where T : Actor
+        {
+            actor.AttemptsTo(Post.DataAsJson(_ownerRegisterDto).To("account/login"));
+        }
+    }
+}

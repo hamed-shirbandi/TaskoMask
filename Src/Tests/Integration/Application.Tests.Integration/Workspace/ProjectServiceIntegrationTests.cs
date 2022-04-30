66c8e82c3c72b7f47d44d7879a3eda2ae8b8da33
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-04-30 19:15:03 +0430

    add Project_Is_Created integration test

--- Src/Tests/Integration/Application.Tests.Integration/Workspace/ProjectServiceIntegrationTests.cs
+++ Src/Tests/Integration/Application.Tests.Integration/Workspace/ProjectServiceIntegrationTests.cs
@@ -1,5 +1,7 @@
 ﻿using FluentAssertions;
+using System.Linq;
 using System.Threading.Tasks;
+using TaskoMask.Application.Share.Dtos.Workspace.Projects;
 using TaskoMask.Application.Tests.Integration.TestData;
 using TaskoMask.Application.Tests.Integration.TestData.Fixtures;
 using TaskoMask.Application.Workspace.Projects.Services;
@@ -29,6 +31,29 @@ namespace TaskoMask.Application.Tests.Integration.Workspace
 
         #region Test Methods
 
+
+
+
+        [Fact]
+        public async Task Project_Is_Created()
+        {
+            //Arrange
+            var organization = await _fixture.GetSampleOrganizationAsync();
+            var dto = new ProjectUpsertDto
+            {
+                Name = "Test Project Name",
+                Description = "Test Project Description",
+                OrganizationId = organization.Id,
+            };
+
+            //Act
+            var result = await _fixture.ProjectService.CreateAsync(dto);
+
+            //Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.EntityId.Should().NotBeNull();
+
+        }
 
 
 

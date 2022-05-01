9a45b728c4ca2fd8e36e560399b6c84c06e320cb
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-02 01:00:36 +0430

    implement RegisterOwnerTask

--- Src/Tests/Acceptance/Tests.Acceptance.Share/Tasks/RegisterOwnerTask.cs
+++ Src/Tests/Acceptance/Tests.Acceptance.Share/Tasks/RegisterOwnerTask.cs
@@ -5,21 +5,24 @@ using TaskoMask.Tests.Acceptance.Share.Models;
 
 namespace TaskoMask.Tests.Acceptance.Share.Tasks
 {
-    public class RegisterOwnerTask : ITask
+    public abstract class RegisterOwnerTask : ITask
     {
-        private readonly OwnerRegisterDto _ownerRegisterDto;
+        protected readonly OwnerRegisterDto OwnerRegisterDto;
 
         public RegisterOwnerTask(OwnerRegisterDto ownerRegisterDto)
         {
-            _ownerRegisterDto = ownerRegisterDto;
+            OwnerRegisterDto = ownerRegisterDto;
         }
 
 
         public void PerformAs<T>(T actor) where T : Actor
         {
-            //actor.AttemptsTo(Post.DataAsJson(_ownerRegisterDto).To("account/register"));
-            //var result = actor.AsksFor(LastResponse.Content<Result<UserJwtTokenDto>>());
-            //actor.Remember(MagicKey.Owner.Regiser_Result, result);
+            var result = DoRegister(actor);
+
+            actor.Remember(MagicKey.Owner.Regiser_Result, result);
         }
+
+
+        protected abstract bool DoRegister<T>(T actor) where T : Actor;
     }
 }

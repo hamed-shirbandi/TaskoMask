7d92efa97b4965d6060ed85c88f553be2e698c52
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-02 00:29:51 +0430

    add ui level tasks

---
+++ Src/Tests/Acceptance/Tests.Acceptance.UI/Tasks/LoginOwnerUiTask.cs
@@ -0,0 +1,20 @@
+﻿using Suzianna.Core.Screenplay;
+using Suzianna.Core.Screenplay.Actors;
+
+
+namespace TaskoMask.Tests.Acceptance.UI.Tasks
+{
+    public class LoginOwnerUiTask : ITask
+    {
+
+        public LoginOwnerUiTask( )
+        {
+        }
+
+
+        public void PerformAs<T>(T actor) where T : Actor
+        {
+  
+        }
+    }
+}
---
+++ Src/Tests/Acceptance/Tests.Acceptance.UI/Tasks/RegisterOwnerUiTask.cs
@@ -0,0 +1,20 @@
+﻿using Suzianna.Core.Screenplay;
+using Suzianna.Core.Screenplay.Actors;
+using TaskoMask.Tests.Acceptance.Share.Models;
+
+namespace TaskoMask.Tests.Acceptance.UI.Tasks
+{
+    public class RegisterOwnerUiTask : ITask
+    {
+
+        public RegisterOwnerUiTask( )
+        {
+        }
+
+
+        public void PerformAs<T>(T actor) where T : Actor
+        {
+
+        }
+    }
+}

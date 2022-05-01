ce7c362c3e9f354f551be3bda9325b3af59f1a5e
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-02 01:04:15 +0430

    add PerformTask

---
+++ Src/Tests/Acceptance/Tests.Acceptance/Screenplay/PerformTask.cs
@@ -0,0 +1,24 @@
+﻿using Suzianna.Core.Screenplay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.Tests.Acceptance.Share.Models;
+
+namespace TaskoMask.Tests.Acceptance.Screenplay
+{
+    public static class PerformTask
+    {
+        public static ITask LoginOwnerTask(OwnerLoginDto ownerLoginDto)
+        {
+            return null;
+        }
+
+
+        public static ITask RegisterOwnerTask(OwnerRegisterDto ownerRegisterDto)
+        {
+            return null;
+        }
+    }
+}

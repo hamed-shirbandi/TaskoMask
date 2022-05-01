220764aa54dd07922df322cd1324cc158af488af
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-02 01:00:15 +0430

    implement LoginOwnerApiTask

--- Src/Tests/Acceptance/Tests.Acceptance.API/Tasks/LoginOwnerApiTask.cs
+++ Src/Tests/Acceptance/Tests.Acceptance.API/Tasks/LoginOwnerApiTask.cs
@@ -1,18 +1,28 @@
 ﻿using Suzianna.Core.Screenplay;
 using Suzianna.Core.Screenplay.Actors;
+using Suzianna.Rest.Screenplay.Interactions;
+using Suzianna.Rest.Screenplay.Questions;
+using TaskoMask.Tests.Acceptance.Share.Helpers;
+using TaskoMask.Tests.Acceptance.Share.Models;
+using TaskoMask.Tests.Acceptance.Share.Tasks;
 
 namespace TaskoMask.Tests.Acceptance.API.Tasks
 {
-    public class LoginOwnerAPITask : ITask
+    public class LoginOwnerAPITask : LoginOwnerTask
     {
-
-        public LoginOwnerAPITask()
+        public LoginOwnerAPITask(OwnerLoginDto ownerLoginDto) : base(ownerLoginDto)
         {
 
         }
 
-        public void PerformAs<T>(T actor) where T : Actor
+
+        protected override bool DoLogin<T>(T actor)
         {
+            actor.AttemptsTo(Post.DataAsJson(OwnerLoginDto).To("account/login"));
+            var result = actor.AsksFor(LastResponse.Content<Result<UserJwtTokenDto>>());
+            return result.IsSuccess;
         }
+
+
     }
 }

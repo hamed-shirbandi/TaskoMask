a34e9174579647c634873499f4df2438a03b1dae
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-02 00:29:42 +0430

    add api level tasks

---
+++ Src/Tests/Acceptance/Tests.Acceptance.API/Tasks/LoginOwnerApiTask.cs
@@ -0,0 +1,18 @@
+﻿using Suzianna.Core.Screenplay;
+using Suzianna.Core.Screenplay.Actors;
+
+namespace TaskoMask.Tests.Acceptance.API.Tasks
+{
+    public class LoginOwnerAPITask : ITask
+    {
+
+        public LoginOwnerAPITask()
+        {
+
+        }
+
+        public void PerformAs<T>(T actor) where T : Actor
+        {
+        }
+    }
+}
---
+++ Src/Tests/Acceptance/Tests.Acceptance.API/Tasks/RegisterOwnerApiTask.cs
@@ -0,0 +1,20 @@
+﻿using Suzianna.Core.Screenplay;
+using Suzianna.Core.Screenplay.Actors;
+using TaskoMask.Tests.Acceptance.Share.Models;
+
+namespace TaskoMask.Tests.Acceptance.API.Tasks
+{
+    public class RegisterOwnerApiTask : ITask
+    {
+
+        public RegisterOwnerApiTask()
+        {
+
+        }
+
+
+        public void PerformAs<T>(T actor) where T : Actor
+        {
+        }
+    }
+}

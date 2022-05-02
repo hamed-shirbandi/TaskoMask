e23cb9dc5cad341152a754f72b85c16a00fdafe5
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-02 18:39:46 +0430

    remove config from magickey

--- Src/Tests/Acceptance/Tests.Acceptance.Share/Helpers/MagicKey.cs
+++ Src/Tests/Acceptance/Tests.Acceptance.Share/Helpers/MagicKey.cs
@@ -4,11 +4,6 @@ namespace TaskoMask.Tests.Acceptance.Share.Helpers
     public static class MagicKey
     {
 
-        public static class Configuration
-        {
-            public const string API_Base_Url = "https://localhost:44314/";
-        }
-
         public static class Owner
         {
             public const string Regiser_Result = "Owner_Regiser_Result";

edf16731c10f208097631415870baf5a6ecd0096
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-02 19:57:45 +0430

    refactor Factory class

---
+++ Src/Tests/Acceptance/Tests.Acceptance.Share/Helpers/Reflector.cs
@@ -0,0 +1,39 @@
+﻿using Suzianna.Core.Screenplay;
+using Suzianna.Core.Screenplay.Questions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskoMask.Tests.Acceptance.Share.Helpers
+{
+    public static class Reflector
+    {
+        public static IDictionary<string, Type> GetScreenplayTypes(this Assembly assembly)
+        {
+            return assembly.GetExportedTypes()
+                .Where(t => t.IsTask() || t.IsQuestion())
+                .ToDictionary(t => t.BaseType.Name, t => t);
+        }
+
+        public static T GetInstanceOf<T>(this IDictionary<string, Type> types, object[] parameters)
+        {
+            var type = types[(typeof(T).Name)];
+            return (T)Activator.CreateInstance(type, parameters);
+        }
+
+
+        public static bool IsTask(this Type type)
+        {
+            return type.BaseType.GetInterfaces().Contains(typeof(ITask));
+        }
+
+
+        public static bool IsQuestion(this Type type)
+        {
+            return type.BaseType.GetInterfaces().Contains(typeof(IQuestion<>));
+        }
+    }
+}
--- Src/Tests/Acceptance/Tests.Acceptance/Screenplay/Factory.cs
+++ Src/Tests/Acceptance/Tests.Acceptance/Screenplay/Factory.cs
@@ -1,53 +1,29 @@
 ﻿using Suzianna.Core.Screenplay;
-using Suzianna.Core.Screenplay.Questions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using TaskoMask.Tests.Acceptance.Share.Helpers;
 
 namespace TaskoMask.Tests.Acceptance.Screenplay
 {
     public static class Factory
     {
-        private static IDictionary<string,Type> _types;
+        private static IDictionary<string,Type> _screenplayTypes;
 
         static Factory()
         {
-            _types = Config.TestLevelAssembly
-                .GetExportedTypes()
-                .Where(t=>t.IsTask()||t.IsQuestion())
-                .ToDictionary(t=>t.BaseType.Name,t=>t);
+            _screenplayTypes = Config.TestLevelAssembly.GetScreenplayTypes();
         }
 
 
         public static T CreateTask<T>(params object[] parameters) where T:ITask
         {
-            return GetInstanceOf<T>(parameters);
+            return _screenplayTypes.GetInstanceOf<T>(parameters);
         }
 
 
         public static T CreateQuestion<T>(params object[] parameters)
         {
-            return GetInstanceOf<T>(parameters);
-        }
-
-
-        private static T GetInstanceOf<T>(object[] parameters)
-        {
-            var type = _types[(typeof(T).Name)];
-            return (T)Activator.CreateInstance(type, parameters);
-        }
-
-
-        private static bool IsTask(this Type type)
-        {
-            return type.BaseType.GetInterfaces().Contains(typeof(ITask));
-        }
-
-
-        private static bool IsQuestion(this Type type)
-        {
-            return type.BaseType.GetInterfaces().Contains(typeof(IQuestion<>));
+            return _screenplayTypes.GetInstanceOf<T>(parameters);
         }
     }
 }

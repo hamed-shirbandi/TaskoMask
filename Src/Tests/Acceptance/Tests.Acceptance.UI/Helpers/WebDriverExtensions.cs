607fbe3da717c73188725e7743cab38fcb15c895
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-04 03:35:55 +0430

    add an extension for IWebDriver

---
+++ Src/Tests/Acceptance/Tests.Acceptance.UI/Helpers/WebDriverExtensions.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using Selenium.WebDriver.WaitExtensions;
+
+
+namespace TaskoMask.Tests.Acceptance.UI.Helpers
+{
+    internal static class WebDriverExtensions
+    {
+        public static bool WaitForElementToExist(this IWebDriver driver, By by, int millisecond=5000)
+        {
+            try
+            {
+                driver.Wait(millisecond).ForElement(by).ToExist();
+                return true;
+            }
+            catch (WebDriverTimeoutException ex)
+            {
+                if (ex.InnerException?.GetType() == typeof(NoSuchElementException))
+                    return false;
+                else
+                    throw;
+            }
+        }
+    }
+}

bc11f01359ed568d66f2edb278bfa33bb7b17651
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-05-04 04:55:26 +0430

    implement LoginOwnerUiTask to check if user can login or not

--- Src/Tests/Acceptance/Tests.Acceptance.UI/Tasks/LoginOwnerUiTask.cs
+++ Src/Tests/Acceptance/Tests.Acceptance.UI/Tasks/LoginOwnerUiTask.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using TaskoMask.Tests.Acceptance.Core.Models;
 using TaskoMask.Tests.Acceptance.Core.Screenplay.Tasks;
+using TaskoMask.Tests.Acceptance.UI.Helpers;
+using TaskoMask.Tests.Acceptance.UI.Suzianna;
 
 namespace TaskoMask.Tests.Acceptance.UI.Tasks
 {
     public class LoginOwnerUiTask : LoginOwnerTask
     {
+        private string loginUrl;
+
         public LoginOwnerUiTask(OwnerLoginDto ownerLoginDto):base(ownerLoginDto)
         {
 
@@ -14,7 +19,17 @@ namespace TaskoMask.Tests.Acceptance.UI.Tasks
 
         protected override bool DoLogin<T>(T actor)
         {
-            throw new NotImplementedException();
+            var ability = actor.FindAbility<BrowseWebPage>();
+
+            loginUrl = $"{ability.BaseUrl}/login";
+            ability.Driver.Navigate().GoToUrl(loginUrl);
+
+            ability.Driver.FindElement(By.Id("Input_UserName")).SendKeys(OwnerLoginDto.UserName);
+            ability.Driver.FindElement(By.Id("Input_Password")).SendKeys(OwnerLoginDto.Password);
+
+            ability.Driver.FindElement(By.Id("loginBtn")).Click();
+
+            return ability.Driver.WaitForElementToExist(By.Id("dashboard_page"));
         }
     }
 }

6e28a6649aa4f55d1a489a0582d9d4d82b90f68d
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-07 23:41:28 +0430

    fix metadata for project modles

--- Src/Libraries/2-Application/Application.Share/Dtos/Workspace/Projects/ProjectBaseDto.cs
+++ Src/Libraries/2-Application/Application.Share/Dtos/Workspace/Projects/ProjectBaseDto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using TaskoMask.Application.Share.Resources;
+using TaskoMask.Domain.Share.Helpers;
 using TaskoMask.Domain.Share.Resources;
 
 namespace TaskoMask.Application.Share.Dtos.Workspace.Projects
@@ -7,16 +8,18 @@ namespace TaskoMask.Application.Share.Dtos.Workspace.Projects
     public abstract class ProjectBaseDto
     {
         public string Id { get; set; }
-       
-        
+
+
         [Display(Name = nameof(ApplicationMetadata.Name), ResourceType = typeof(ApplicationMetadata))]
+        [StringLength(DomainConstValues.Project_Name_Max_Length, MinimumLength = DomainConstValues.Project_Name_Min_Length, ErrorMessageResourceName = nameof(DomainMessages.Length_Error), ErrorMessageResourceType = typeof(DomainMessages))]
         [Required(ErrorMessageResourceName = nameof(DomainMessages.Required), ErrorMessageResourceType = typeof(DomainMessages))]
-        public string Name { get; set; }
+        public string Name { get; }
 
 
         [Display(Name = nameof(ApplicationMetadata.Description), ResourceType = typeof(ApplicationMetadata))]
+        [StringLength(DomainConstValues.Project_Name_Max_Length, MinimumLength = DomainConstValues.Project_Name_Min_Length, ErrorMessageResourceName = nameof(DomainMessages.Length_Error), ErrorMessageResourceType = typeof(DomainMessages))]
         [Required(ErrorMessageResourceName = nameof(DomainMessages.Required), ErrorMessageResourceType = typeof(DomainMessages))]
-        public string Description { get; set; }
+        public string Description { get; }
 
 
         [Display(Name = nameof(ApplicationMetadata.OrganizationId), ResourceType = typeof(ApplicationMetadata))]
--- Src/Libraries/2-Application/Application/Workspace/Projects/Commands/Models/ProjectBaseCommand.cs
+++ Src/Libraries/2-Application/Application/Workspace/Projects/Commands/Models/ProjectBaseCommand.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using TaskoMask.Domain.Share.Helpers;
 using TaskoMask.Domain.Share.Resources;
+using TaskoMask.Application.Share.Resources;
 
 namespace TaskoMask.Application.Workspace.Projects.Commands.Models
 {
@@ -14,14 +15,18 @@ namespace TaskoMask.Application.Workspace.Projects.Commands.Models
         }
 
 
+
+        [Display(Name = nameof(ApplicationMetadata.Name), ResourceType = typeof(ApplicationMetadata))]
         [StringLength(DomainConstValues.Project_Name_Max_Length, MinimumLength = DomainConstValues.Project_Name_Min_Length, ErrorMessageResourceName = nameof(DomainMessages.Length_Error), ErrorMessageResourceType = typeof(DomainMessages))]
         [Required(ErrorMessageResourceName = nameof(DomainMessages.Required), ErrorMessageResourceType = typeof(DomainMessages))]
-        public string Name { get;  }
+        public string Name { get; }
 
 
+        [Display(Name = nameof(ApplicationMetadata.Description), ResourceType = typeof(ApplicationMetadata))]
         [StringLength(DomainConstValues.Project_Name_Max_Length, MinimumLength = DomainConstValues.Project_Name_Min_Length, ErrorMessageResourceName = nameof(DomainMessages.Length_Error), ErrorMessageResourceType = typeof(DomainMessages))]
         [Required(ErrorMessageResourceName = nameof(DomainMessages.Required), ErrorMessageResourceType = typeof(DomainMessages))]
-        public string Description { get;  }
+        public string Description { get; }
+
 
 
     }

036ada3e0d481445129179b53d3d2c192ec3d11a
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-14 00:59:34 +0430

    update program.cs

--- Src/Presentation/3-UI/UserPanel/Program.cs
+++ Src/Presentation/3-UI/UserPanel/Program.cs
@@ -1,12 +1,11 @@
-using TaskoMask.Presentation.UI.UserPanel.Configuration;
+using TaskoMask.Presentation.UI.UserPanel;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddProjectConfigureServices(builder.Configuration, builder.Environment);
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-app.UseProjectConfigure(builder.Environment);
-
-app.Run();
+await builder.Build().RunAsync();

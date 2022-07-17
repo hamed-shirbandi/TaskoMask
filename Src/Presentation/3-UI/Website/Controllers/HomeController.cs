8b3ca83d3f26ed0d46f0480b0c5231009b8641d1
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-18 04:07:21 +0430

    refactor landing page UI and delete unused model from index view

--- Src/Presentation/3-UI/Website/Controllers/HomeController.cs
+++ Src/Presentation/3-UI/Website/Controllers/HomeController.cs
@@ -1,12 +1,5 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using TaskoMask.Application.Workspace.Organizations.Services;
-using TaskoMask.Presentation.UI.Website.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using TaskoMask.Presentation.Framework.Web.Controllers;
-using TaskoMask.Application.Workspace.Projects.Services;
-using TaskoMask.Application.Workspace.Boards.Services;
-using TaskoMask.Application.Workspace.Owners.Services;
-using TaskoMask.Application.Workspace.Tasks.Services;
 
 namespace TaskoMask.Presentation.UI.Website.Controllers
 {
@@ -14,23 +7,14 @@ namespace TaskoMask.Presentation.UI.Website.Controllers
     {
         #region Fields
 
-        private readonly IOrganizationService _organizationService;
-        private readonly IProjectService _projectService;
-        private readonly ITaskService _taskService;
-        private readonly IBoardService _boardService;
-        private readonly IOwnerService _ownerService;
 
         #endregion
 
         #region Ctors
 
-        public HomeController(IOrganizationService organizationService, ITaskService taskService, IBoardService boardService, IOwnerService ownerService, IProjectService projectService)
+        public HomeController()
         {
-            _organizationService = organizationService;
-            _taskService = taskService;
-            _boardService = boardService;
-            _ownerService = ownerService;
-            _projectService = projectService;
+
         }
 
         #endregion
@@ -45,17 +29,7 @@ namespace TaskoMask.Presentation.UI.Website.Controllers
         /// </summary>
         public async Task<IActionResult> Index()
         {
-            //TODO cache this queries
-            var model = new HomeIndexViewModel
-            {
-                OrganizationsCount = (await _organizationService.CountAsync()).Value,
-                ProjectsCount = (await _projectService.CountAsync()).Value,
-                BoardsCount = (await _boardService.CountAsync()).Value,
-                TasksCount = (await _taskService.CountAsync()).Value,
-                OwnersCount= (await _ownerService.CountAsync()).Value,
-            };
-
-            return View(model);
+            return View();
         }
 
 

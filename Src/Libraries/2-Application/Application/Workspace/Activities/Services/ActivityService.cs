44e968920e51d9f5516ca67247f9b609ecf4d228
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-18 00:37:51 +0430

    add activity application services

---
+++ Src/Libraries/2-Application/Application/Workspace/Activities/Services/ActivityService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using TaskoMask.Application.Share.Helpers;
+using System.Collections.Generic;
+using TaskoMask.Application.Core.Notifications;
+using TaskoMask.Application.Core.Bus;
+using TaskoMask.Application.Core.Services;
+using TaskoMask.Application.Share.Dtos.Workspace.Activities;
+using System.Threading.Tasks;
+using TaskoMask.Application.Workspace.Activities.Queries.Models;
+
+namespace TaskoMask.Application.Workspace.Activities.Services
+{
+    public class TaskService : ApplicationService, IActivityService
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Ctors
+
+        public TaskService(IInMemoryBus inMemoryBus, IMapper mapper, IDomainNotificationHandler notifications) : base(inMemoryBus, mapper, notifications)
+        { }
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<IEnumerable<ActivityBasicInfoDto>>> GetListByTaskIdAsync(string taskId)
+        {
+            return await SendQueryAsync(new GetActivitiesByTaskIdQuery(taskId));
+        }
+
+
+
+
+        #endregion
+    }
+}

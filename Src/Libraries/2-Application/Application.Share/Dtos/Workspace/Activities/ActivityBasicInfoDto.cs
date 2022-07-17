f2f1f38804ad6f4276e663f84952fb29bdd15a87
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-18 00:34:55 +0430

    add activity dtos

---
+++ Src/Libraries/2-Application/Application.Share/Dtos/Workspace/Activities/ActivityBasicInfoDto.cs
@@ -0,0 +1,10 @@
+﻿using TaskoMask.Application.Share.Dtos.Common;
+
+namespace TaskoMask.Application.Share.Dtos.Workspace.Activities
+{
+    public class ActivityBasicInfoDto: ActivityBaseDto
+    {
+        public CreationTimeDto CreationTime { get; set; }
+
+    }
+}

0248c69c13a9a920de46fc38df2155391c9274a5
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-21 19:30:03 +0430

    add TempCards component

--- Src/Libraries/2-Application/Application.Share/ViewModels/BoardDetailsViewModel.cs
+++ Src/Libraries/2-Application/Application.Share/ViewModels/BoardDetailsViewModel.cs
@@ -4,6 +4,10 @@ namespace TaskoMask.Application.Share.ViewModels
 {
    public class BoardDetailsViewModel
     {
+        public BoardDetailsViewModel()
+        {
+            Cards = new List<CardDetailsViewModel>();
+        }
         public BoardOutputDto Board { get; set; }
         public IEnumerable<CardDetailsViewModel> Cards { get; set; }
     }

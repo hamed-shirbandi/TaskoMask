dbfffd38779ccea584da083acac61bf8dd34388d
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-23 01:05:55 +0430

    add DeleteCommentCommand model

---
+++ Src/Libraries/2-Application/Application/Workspace/Comments/Commands/Models/DeleteCommentCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskoMask.Application.Core.Commands;
+using TaskoMask.Domain.Share.Resources;
+
+namespace TaskoMask.Application.Workspace.Comments.Commands.Models
+{
+    public class DeleteCommentCommand : BaseCommand
+    {
+        public DeleteCommentCommand(string id)
+        {
+            Id = id;
+        }
+
+        [Required(ErrorMessageResourceName = nameof(DomainMessages.Required), ErrorMessageResourceType = typeof(DomainMessages))]
+        public string Id { get; }
+
+    }
+}

6858d2cf3624b1f3402c59f6533c21fa11004c6c
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-23 19:16:11 +0430

    implement comment client data services

---
+++ Src/Presentation/3-UI/UserPanel/Services/Data/CommentClientService.cs
@@ -0,0 +1,80 @@
+﻿using TaskoMask.Application.Share.Dtos.Workspace.Comments;
+using TaskoMask.Application.Share.Helpers;
+using TaskoMask.Presentation.Framework.Share.Contracts;
+using TaskoMask.Presentation.Framework.Share.Services.Http;
+
+namespace TaskoMask.Presentation.UI.UserPanel.Services.Data
+{
+    public class CommentClientService : ICommentClientService
+    {
+        #region Fields
+
+        private readonly IHttpClientService _httpClientService;
+
+        #endregion
+
+        #region Ctor
+
+        public CommentClientService(IHttpClientService httpClientService)
+        {
+            _httpClientService = httpClientService;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CommentBasicInfoDto>> Get(string id)
+        {
+            var url = $"/comments/{id}";
+            return await _httpClientService.GetAsync<CommentBasicInfoDto>(url);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CommandResult>> Create(CommentUpsertDto input)
+        {
+            var url = $"/comments";
+            return await _httpClientService.PostAsync<CommandResult>(url, input);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CommandResult>> Update(string id,CommentUpsertDto input)
+        {
+            var url = $"/comments/{id}";
+            return await _httpClientService.PutAsync<CommandResult>(url, input);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Result<CommandResult>> Delete(string id)
+        {
+            var url = $"/comments/{id}";
+            return await _httpClientService.DeleteAsync<CommandResult>(url);
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+    }
+}

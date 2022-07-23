3454704b8f4f1fbd9d9083a711e9c97b6604e193
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-23 23:37:40 +0430

    add CommandResultHandler

---
+++ Src/Presentation/3-UI/UserPanel/Helpers/CommandResultHandler.cs
@@ -0,0 +1,104 @@
+﻿using Blazored.Modal;
+using Blazored.Toast.Services;
+using TaskoMask.Application.Share.Helpers;
+using TaskoMask.Presentation.UI.UserPanel.Services.Message;
+
+namespace TaskoMask.Presentation.UI.UserPanel.Helpers
+{
+    public class CommandResultHandler
+    {
+        private Result<CommandResult> _result;
+        private MessageType _messageType;
+
+
+        private CommandResultHandler()
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CommandResultHandler Init()
+        {
+            return new CommandResultHandler();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CommandResultHandler WithResult(Result<CommandResult> result)
+        {
+            _result = result;
+            return this;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CommandResultHandler WithMessageType(MessageType messageType)
+        {
+            _messageType = messageType;
+            return this;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CommandResultHandler ShowToast(IToastService toastService)
+        {
+            if (_result.IsSuccess)
+                toastService.ShowSuccess(_result.Value.Message, _result.Message);
+            else
+                toastService.ShowError(_result.Errors.ParseToFragment(), _result.Message);
+            return this;
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CommandResultHandler ShowErrorToast(IToastService toastService)
+        {
+            if (!_result.IsSuccess)
+                toastService.ShowError(_result.Errors.ParseToFragment(), _result.Message);
+            return this;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CommandResultHandler PublishMessage(IMessageService messageService)
+        {
+            if (_result.IsSuccess)
+                messageService.SendMessage(_messageType);
+            return this;
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CommandResultHandler CloseModal(BlazoredModalInstance? modalInstance)
+        {
+            modalInstance.CloseAsync();
+            return this;
+
+        }
+
+    }
+}

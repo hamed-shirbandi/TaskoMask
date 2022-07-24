c848d68fc32b0623704107eea778d1f2286233d1
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-07-24 19:19:50 +0430

    add ReloadDataHelper

---
+++ Src/Presentation/3-UI/UserPanel/Helpers/ReloadDataHelper.cs
@@ -0,0 +1,22 @@
+﻿using TaskoMask.Presentation.UI.UserPanel.Services.ComponentMessage;
+
+namespace TaskoMask.Presentation.UI.UserPanel.Helpers
+{
+    public static class ReloadDataHelper
+    {
+
+        public static bool Task_Details_Need_Reload(MessageType messageType)
+        {
+            if (messageType == MessageType.Task_Updated
+                || messageType == MessageType.Task_Moved
+                || messageType == MessageType.Comment_Created
+                || messageType == MessageType.Comment_Updated
+                || messageType == MessageType.Comment_Deleted)
+                return true;
+
+            return false;
+        }
+
+
+    }
+}

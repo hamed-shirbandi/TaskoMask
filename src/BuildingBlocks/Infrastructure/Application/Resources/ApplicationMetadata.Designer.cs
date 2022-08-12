81e404771862202dbaf458501e05507522b10025
hamed shirbandi <hamed.shirbandi@hotmail.com>
2022-08-13 03:21:30 +0430

    chore: fix Infrastructure AssemblyName and RootNamespace

--- src/BuildingBlocks/Infrastructure/Application/Resources/ApplicationMetadata.Designer.cs
+++ src/BuildingBlocks/Infrastructure/Application/Resources/ApplicationMetadata.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Infrastructure.Application.Resources {
+namespace TaskoMask.BuildingBlocks.Infrastructure.Application.Resources {
     using System;
     
     
@@ -39,7 +39,8 @@ namespace Infrastructure.Application.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Infrastructure.Application.Resources.ApplicationMetadata", typeof(ApplicationMetadata).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TaskoMask.BuildingBlocks.Infrastructure.Application.Resources.ApplicationMetadata" +
+                            "", typeof(ApplicationMetadata).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

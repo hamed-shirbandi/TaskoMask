2c2c60beb01282cc4b76cefa8b51d815bb3256f0
hamed shirbandi <hamed.shirbandi@hotmail.com>
2022-08-14 16:25:35 +0430

    chore: add Monolith.Api

--- src/2-Services/Monolith/Presentation/1-Framework/Framework.Web/Services/Authentication/JwtAuthentication/IJwtAuthenticationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TaskoMask.Services.Monolith.Domain.Share.Models;
-
-namespace TaskoMask.Services.Monolith.Presentation.Framework.Web.Services.Authentication.JwtAuthentication
-{
-    public interface IJwtAuthenticationService
-    {
-        string GenerateJwtToken(AuthenticatedUser user);
-    }
-}
--- src/2-Services/Monolith/Presentation/2-API/README.md
+++
@@ -1,5 +0,0 @@
-
-# About TaskoMask APIs
-
-In this part of application we provide some APIs to be used by client side applications.
-Every developers aroun the world can use our APIs to make their own application.

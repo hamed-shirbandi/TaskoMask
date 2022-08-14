be586500b90b2b77262e4d804535b5472dd06f6d
hamed shirbandi <hamed.shirbandi@hotmail.com>
2022-08-14 23:56:37 +0430

    chore: some changes on services architecture

--- src/2-Services/Monolith/Libraries/Infrastructure/Infrastructure.CrossCutting/IoC/IoCConfig.cs
+++ src/2-Services/Monolith/Libraries/Infrastructure/Infrastructure.CrossCutting/IoC/IoCConfig.cs
@@ -3,7 +3,6 @@ using TaskoMask.Services.Monolith.Application.Workspace.Projects.Services;
 using TaskoMask.Services.Monolith.Infrastructure.Data.Write.DbContext;
 using TaskoMask.BuildingBlocks.Infrastructure.EventSourcing;
 using TaskoMask.BuildingBlocks.Infrastructure.Bus;
-using TaskoMask.BuildingBlocks.Infrastructure.Bus;
 using TaskoMask.BuildingBlocks.Domain.Events;
 using TaskoMask.Services.Monolith.Application.Membership.Roles.Services;
 using TaskoMask.Services.Monolith.Application.Membership.Operators.Services;

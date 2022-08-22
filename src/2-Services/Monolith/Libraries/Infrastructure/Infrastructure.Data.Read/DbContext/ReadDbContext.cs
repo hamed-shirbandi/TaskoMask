35da0811f068c590f7bfe1d1f7d6c3ed75816465
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-08-22 04:53:16 +0430

    refactor: add properties to ReadDbContext

--- src/2-Services/Monolith/Libraries/Infrastructure/Infrastructure.Data.Read/DbContext/ReadDbContext.cs
+++ src/2-Services/Monolith/Libraries/Infrastructure/Infrastructure.Data.Read/DbContext/ReadDbContext.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using System.Linq;
 using TaskoMask.BuildingBlocks.Infrastructure.MongoDB;
 using Microsoft.Extensions.Options;
+using TaskoMask.Services.Monolith.Domain.DataModel.Entities;
 
 namespace TaskoMask.Services.Monolith.Infrastructure.Data.Read.DbContext
 {
@@ -23,17 +24,31 @@ namespace TaskoMask.Services.Monolith.Infrastructure.Data.Read.DbContext
 
 
         public ReadDbContext(IOptions<ReadDbOptions> mongoDbOptions) : base(mongoDbOptions)
-
         {
-
+            Owners = GetCollection<Owner>();
+            Organizations = GetCollection<Organization>();
+            Projects = GetCollection<Project>();
+            Boards = GetCollection<Board>();
+            Cards = GetCollection<Card>();
+            Tasks = GetCollection<Task>();
+            Comments = GetCollection<Comment>();
+            Activities = GetCollection<Activity>(nameof(Activities));
         }
 
 
 
         #endregion
 
-        #region Public Methods
+        #region Properties
 
+        public IMongoCollection<Owner> Owners { get; }
+        public IMongoCollection<Organization> Organizations { get; }
+        public IMongoCollection<Project> Projects { get; }
+        public IMongoCollection<Board> Boards { get; }
+        public IMongoCollection<Card> Cards { get; }
+        public IMongoCollection<Task> Tasks { get; }
+        public IMongoCollection<Comment> Comments { get; }
+        public IMongoCollection<Activity> Activities { get; }
 
 
         #endregion

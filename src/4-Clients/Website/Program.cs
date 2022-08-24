d1202631fff00d49c5e9c09836b5b1dbca672cab
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-08-24 18:27:51 +0430

    refactor: remove extra dependencies from website project

--- src/4-Clients/Website/Program.cs
+++ src/4-Clients/Website/Program.cs
@@ -1,13 +1,10 @@
 using Serilog;
 using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Serilog;
 using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Startup;
-using TaskoMask.Services.Monolith.Infrastructure.CrossCutting.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddCustomSerilog();
-
-builder.Services.AddProjectServices(builder.Configuration);
 
 builder.Services.AddMvcPreConfigured(builder.Configuration, builder.Environment);
 
@@ -16,8 +13,6 @@ var app = builder.Build();
 app.UseSerilogRequestLogging();
 
 app.UseMvcPreConfigured(app.Services, builder.Environment);
-
-app.Services.InitialAdnSeedDatabases();
 
 app.UseEndpoints(endpoints =>
 {

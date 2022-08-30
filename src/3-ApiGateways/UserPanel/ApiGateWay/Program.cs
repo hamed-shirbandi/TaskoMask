06199f43cecc1e1d58e0086de3502ca1002d96eb
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-08-30 13:20:48 +0430

    feat: add oceleot basic configuration

--- src/3-ApiGateways/UserPanel/ApiGateWay/Program.cs
+++ src/3-ApiGateways/UserPanel/ApiGateWay/Program.cs
@@ -1,6 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", false, true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseOcelot().Wait();
 
 app.Run();

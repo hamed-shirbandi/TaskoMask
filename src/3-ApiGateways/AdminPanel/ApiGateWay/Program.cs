ae67767dfa56612b46f6a8e9d18a2484bd3bef10
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-08-31 01:42:53 +0430

    feat: config ocelot for AdminPanel gateway

--- src/3-ApiGateways/AdminPanel/ApiGateWay/Program.cs
+++ src/3-ApiGateways/AdminPanel/ApiGateWay/Program.cs
@@ -1,6 +1,35 @@
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Serilog;
+using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddCustomSerilog();
+
+builder.Configuration.AddOcelotWithSwaggerSupport((o) =>
+{
+    o.Folder = "Configuration";
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddOcelot();
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSerilogRequestLogging();
+
+app.UseStaticFiles();
+
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
+
+app.UseOcelot().Wait();
 
 app.Run();

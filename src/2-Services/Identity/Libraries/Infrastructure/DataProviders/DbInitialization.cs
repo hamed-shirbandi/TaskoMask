771b482c1ebf952ff297ce0f936146c2122eca0b
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-06 15:38:53 +0430

    chore: move DataProviders to identity service

---
+++ src/2-Services/Identity/Libraries/Infrastructure/DataProviders/DbInitialization.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using TaskoMask.Services.Identity.Domain.Entities;
+using TaskoMask.Services.Identity.Infrastructure.DbContext;
+
+namespace TaskoMask.Services.Identity.Infrastructure.DataProviders
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DbInitialization
+    {
+
+        /// <summary>
+        /// Create collections and set indexes
+        /// </summary>
+        public static void Initial( IServiceProvider serviceProvider)
+        {
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetService<IIdentityDbContext>();
+
+                CreateIndexes(dbContext);
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// Create index for collections
+        /// </summary>
+        private static void CreateIndexes(IIdentityDbContext dbContext)
+        {
+            #region User Indexs
+
+            dbContext.GetCollection<User>().Indexes.CreateOneAsync(new CreateIndexModel<User>(Builders<User>.IndexKeys.Ascending(x => x.Id), new CreateIndexOptions() { Name = nameof(User.Id), Unique = true }));
+            dbContext.GetCollection<User>().Indexes.CreateOneAsync(new CreateIndexModel<User>(Builders<User>.IndexKeys.Ascending(x => x.UserName), new CreateIndexOptions() { Name = nameof(User.UserName), Unique = true }));
+
+
+            #endregion
+        }
+
+
+
+        /// <summary>
+        /// Drop database
+        /// </summary>
+        public static void DropDatabase(IServiceProvider serviceProvider)
+        {
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetService<IIdentityDbContext>();
+
+                dbContext.DropDatabase();
+            }
+        }
+
+
+    }
+}

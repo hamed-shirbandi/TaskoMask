94485dd6483daa7a42bbc97f3e5c7f374464f4ce
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-06 16:54:37 +0430

    fix: add project service dependencies to fixture

--- src/2-Services/Monolith/Tests/Integration/Application.Tests.Integration/Fixtures/TestsBaseFixture.cs
+++ src/2-Services/Monolith/Tests/Integration/Application.Tests.Integration/Fixtures/TestsBaseFixture.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using TaskoMask.Services.Monolith.Infrastructure.Data.Generator;
 using TaskoMask.Services.Monolith.Infrastructure.Data.Read.DataProviders;
 using TaskoMask.Services.Monolith.Infrastructure.Data.Write.DataProviders;
-using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Startup;
+using TaskoMask.Services.Monolith.Infrastructure.CrossCutting.IoC;
 
 namespace TaskoMask.Services.Monolith.Application.Tests.Integration.Fixtures
 {
@@ -108,7 +108,7 @@ namespace TaskoMask.Services.Monolith.Application.Tests.Integration.Fixtures
 
             services.AddSingleton<IConfiguration>(provider => { return configuration; });
 
-            services.AddCommonServices(configuration);
+            services.AddProjectServices(configuration);
 
             var serviceProvider = services.BuildServiceProvider();
 

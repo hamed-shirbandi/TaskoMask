cf9f0452fb55a557515eb1cf5263db7017ceb80b
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-06 16:02:59 +0430

    chore: add ioc to infrastracture

---
+++ src/2-Services/Identity/Libraries/Infrastructure/CrossCutting/IoC/ApplicationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TaskoMask.Services.Identity.Application.Users.Services;
+
+namespace TaskoMask.Services.Identity.Infrastructure.CrossCutting.IoC
+{
+    public static class ApplicationExtensions
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+        }
+
+
+    }
+}
---
+++ src/2-Services/Identity/Libraries/Infrastructure/CrossCutting/IoC/InfrastructureExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskoMask.BuildingBlocks.Infrastructure.MongoDB;
+using TaskoMask.Services.Identity.Domain.Data;
+using TaskoMask.Services.Identity.Infrastructure.Data.DbContext;
+using TaskoMask.Services.Identity.Infrastructure.Data.Repositories;
+
+namespace TaskoMask.Services.Identity.Infrastructure.CrossCutting.IoC
+{
+    public static class InfrastructureExtensions
+    {
+
+        #region Public Methods
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext(configuration);
+            services.AddRepositories();
+        }
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var options = configuration.GetSection("Mongo");
+
+            services.AddScoped<IIdentityDbContext, IdentityDbContext>().AddOptions<MongoDbOptions>().Bind(options);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+
+
+        #endregion
+    }
+}

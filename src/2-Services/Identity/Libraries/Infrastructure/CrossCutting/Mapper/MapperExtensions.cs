2443c3670d112449784b28779fd017a76092d381
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-06 16:02:46 +0430

    chore: add mapper to infrastracture

---
+++ src/2-Services/Identity/Libraries/Infrastructure/CrossCutting/Mapper/MapperExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskoMask.Services.Identity.Infrastructure.CrossCutting.Mapper
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MapperExtensions
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddAutoMapperSetup(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddAutoMapper(typeof(UserMappingProfile));
+        }
+    }
+}
---
+++ src/2-Services/Identity/Libraries/Infrastructure/CrossCutting/Mapper/UserMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Authorization.Users;
+using TaskoMask.Services.Identity.Domain.Entities;
+
+namespace TaskoMask.Services.Identity.Infrastructure.CrossCutting.Mapper
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<User, UserBasicInfoDto>();
+        }
+    }
+}

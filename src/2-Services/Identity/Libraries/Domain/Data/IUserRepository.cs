cea8754d41540d268fd4fc5f0072161cd357e534
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-06 15:24:35 +0430

    chore: move Authorization BC to identity service

---
+++ src/2-Services/Identity/Libraries/Domain/Data/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using TaskoMask.BuildingBlocks.Domain.Data;
+using TaskoMask.Services.Identity.Domain.Entities;
+
+namespace TaskoMask.Services.Identity.Domain.Data
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<bool> ExistByUserNameAsync(string userName);
+        Task<User> GetByUserNameAsync(string userName);
+    }
+}

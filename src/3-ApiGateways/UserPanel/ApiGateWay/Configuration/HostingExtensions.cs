66ec63fac8243c3048c113470bc44782543e2d43
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 01:29:25 +0430

    refactor: refactor UserPanel.ApiGateWay with HostingExtensions

---
+++ src/3-ApiGateways/UserPanel/ApiGateWay/Configuration/HostingExtensions.cs
@@ -0,0 +1,56 @@
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Serilog;
+using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Serilog;
+
+namespace TaskoMask.ApiGateways.UserPanel.ApiGateWay.Configuration
+{
+    internal static class HostingExtensions
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+
+            builder.AddCustomSerilog();
+
+            builder.Configuration.AddOcelotWithSwaggerSupport((o) =>
+            {
+                o.Folder = "Configuration/Ocelot";
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddOcelot();
+
+            builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+            return builder.Build();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseSerilogRequestLogging();
+
+            app.UseStaticFiles();
+
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
+
+            app.UseOcelot().Wait();
+
+            return app;
+        }
+    }
+}

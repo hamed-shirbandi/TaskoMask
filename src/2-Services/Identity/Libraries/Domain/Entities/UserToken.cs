d97f04141a61a0cd72924c20759ae05657002a63
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-14 23:36:28 +0430

    chore: add IdentityDbContext

--- src/2-Services/Identity/Libraries/Domain/Entities/UserToken.cs
+++ src/2-Services/Identity/Libraries/Domain/Entities/UserToken.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace Identity.Api.Domain
+namespace TaskoMask.Services.Identity.Domain.Entities
 {
     public class UserToken : IdentityUserToken<string>
     {
---
+++ src/2-Services/Identity/Libraries/Infrastructure/Data/DbContext/IdentityDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using TaskoMask.Services.Identity.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace TaskoMask.Services.Identity.Infrastructure.Data.DbContext
+{
+    public class IdentityDbContext : IdentityDbContext<User, IdentityRole<string>, string, IdentityUserClaim<string>, IdentityUserRole<string>, UserLogin, IdentityRoleClaim<string>, UserToken>
+    {
+        private readonly IConfiguration _configuration;
+
+        public IdentityDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("SqlConnection"));
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>().ToTable("Users");
+            builder.Entity<IdentityRole<long>>().ToTable("Roles");
+            builder.Entity<IdentityUserToken<long>>().ToTable("UserTokens");
+            builder.Entity<IdentityUserRole<long>>().ToTable("UserRoles");
+            builder.Entity<IdentityRoleClaim<long>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserClaim<long>>().ToTable("UserClaims");
+            builder.Entity<UserLogin>().ToTable("UserLogins");
+        }
+    }
+}

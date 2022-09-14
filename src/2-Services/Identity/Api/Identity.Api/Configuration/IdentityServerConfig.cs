d5f28fe2005800a35475e374ac3def074c3d1b8a
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 01:20:07 +0430

    refactor: refactor IdentityServerConfig

--- src/2-Services/Identity/Api/Identity.Api/Configuration/IdentityServerConfig.cs
+++ src/2-Services/Identity/Api/Identity.Api/Configuration/IdentityServerConfig.cs
@@ -5,45 +5,69 @@ namespace TaskoMask.Services.Identity.Api.Configuration
 {
     internal static class IdentityServerConfig
     {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
         public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
         {
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
         };
 
+
+
+        /// <summary>
+        /// 
+        /// </summary>
         public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
         {
-            new ApiScope(name:"Owner.Api",displayName:"Owner Api")
+            new ApiScope(name:"Monolith.Api",displayName:"Monolith Api")
         };
 
+
+
+        /// <summary>
+        /// 
+        /// </summary>
         public static IEnumerable<Client> Clients => new Client[]
         {
-            new Client
-            {
-                ClientId="UserPanel",
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets =
-                {
-                    new Secret("secret".Sha256())
-                },
-                AllowedScopes = { "Owner.Api" }
-            },
+            ClientsConfig.UserPanel,
+            ClientsConfig.AdminPanel,
+        };
+    }
 
-            new Client
+
+    static class ClientsConfig
+    {
+
+        public static Client UserPanel => new()
+        {
+            ClientId = "UserPanel",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets =
             {
-                ClientId = "AdminPanel",
-                ClientSecrets = { new Secret("secret2".Sha256()) },
-                AllowedGrantTypes = GrantTypes.Code,
-                RedirectUris = { "https://localhost:5013/signin-oidc" },
-                PostLogoutRedirectUris = { "https://localhost:5013/signout-callback-oidc" },
-                AllowOfflineAccess = true,
-                UpdateAccessTokenClaimsOnRefresh = true,
-                AllowedScopes = new List<string>
-                {
-                    IdentityServerConstants.StandardScopes.OpenId,
-                    IdentityServerConstants.StandardScopes.Profile,
-                    "Owner.Api"
-                }
+                new Secret("secret".Sha256())
+            },
+            AllowedScopes = { "Monolith.Api" }
+        };
+
+
+        public static Client AdminPanel => new()
+        {
+            ClientId = "AdminPanel",
+            ClientSecrets = { new Secret("secret2".Sha256()) },
+            AllowedGrantTypes = GrantTypes.Code,
+            RedirectUris = { "https://localhost:5013/signin-oidc" },
+            PostLogoutRedirectUris = { "https://localhost:5013/signout-callback-oidc" },
+            AllowOfflineAccess = true,
+            UpdateAccessTokenClaimsOnRefresh = true,
+            AllowedScopes = new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                "Monolith.Api"
             }
         };
     }

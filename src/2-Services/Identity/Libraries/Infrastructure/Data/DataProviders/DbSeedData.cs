626271fec8b231b8590c84fe4a4fc43afef187a3
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 00:01:18 +0430

    chore: add DbSeedData for identity service

---
+++ src/2-Services/Identity/Libraries/Infrastructure/Data/DataProviders/DbSeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskoMask.Services.Identity.Domain.Entities;
+
+namespace TaskoMask.Services.Identity.Infrastructure.Data.DataProviders
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DbSeedData
+    {
+
+
+        /// <summary>
+        /// Seed the necessary data that system needs
+        /// </summary>
+        public static async Task SeedEssentialData(IServiceProvider serviceProvider)
+        {
+            using var serviceScope = serviceProvider.CreateScope();
+            var configuration = serviceScope.ServiceProvider.GetService<IConfiguration>();
+            var userManager = serviceScope.ServiceProvider.GetService<UserManager<User>>();
+
+            await SeedSuperUser(userManager, configuration);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private async static Task SeedSuperUser(UserManager<User> userManager, IConfiguration configuration)
+        {
+            var superUserEmail = configuration["Identity:SuperUser:Email"];
+            var superUserName = configuration["Identity:SuperUser:UserName"];
+
+            if (await userManager.FindByEmailAsync(superUserEmail) == null)
+            {
+                var superUser = new User
+                {
+                    UserName = superUserName,
+                    Email = superUserEmail,
+                    IsActive = true,
+                };
+
+                var password = configuration["Identity:SuperUser:Password"];
+                await userManager.CreateAsync(superUser, password);
+            }
+        }
+    }
+}

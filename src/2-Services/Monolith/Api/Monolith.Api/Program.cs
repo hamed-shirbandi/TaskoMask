477813819bee49bc2d3a94cd74918b035fbb7a21
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 01:25:38 +0430

    refactor: refactor Monolith.Api with HostingExtensions

--- src/2-Services/Monolith/Api/Monolith.Api/Program.cs
+++ src/2-Services/Monolith/Api/Monolith.Api/Program.cs
@@ -1,27 +1,7 @@
-using Serilog;
-using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Startup;
-using TaskoMask.Services.Monolith.Infrastructure.CrossCutting.IoC;
-using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Serilog;
+using TaskoMask.Services.Monolith.Api.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddCustomSerilog();
-
-builder.Services.AddProjectServices(builder.Configuration);
-
-builder.Services.AddWebApiPreConfigured(builder.Configuration);
-
-var app = builder.Build();
-
-app.UseSerilogRequestLogging();
-
-app.UseWebApiPreConfigured(app.Services, builder.Environment);
-
-app.Services.InitialDatabasesAndSeedEssentialData();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+var app = builder.ConfigureServices().ConfigurePipeline();
 
 app.Run();

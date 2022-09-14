18f0f64ab69157e5613aea98274ab23f89680164
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 01:06:57 +0430

    refactor: refactor startup configurations

---
+++ src/2-Services/Identity/Api/Identity.Api/Configuration/HostingExtensions.cs
@@ -0,0 +1,54 @@
+using Serilog;
+using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Serilog;
+using TaskoMask.Services.Identity.Infrastructure.CrossCutting.IoC;
+using TaskoMask.Services.Identity.Infrastructure.Data.DataProviders;
+
+namespace TaskoMask.Services.Identity.Api.Configuration
+{
+    internal static class HostingExtensions
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.AddCustomSerilog();
+
+            builder.Services.AddProjectServices(builder.Configuration);
+
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddAspNetIdentity(builder.Configuration);
+
+            builder.Services.AddIdentityServer();
+
+            return builder.Build();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseSerilogRequestLogging();
+
+            if (app.Environment.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.MapRazorPages().RequireAuthorization();
+
+            DbSeedData.SeedEssentialData(app.Services);
+
+            return app;
+        }
+    }
+}
--- src/2-Services/Identity/Api/Identity.Api/Program.cs
+++ src/2-Services/Identity/Api/Identity.Api/Program.cs
@@ -1,28 +1,7 @@
-using Serilog;
-using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Startup;
-using TaskoMask.BuildingBlocks.Web.MVC.Configuration.Serilog;
-using TaskoMask.Services.Identity.Infrastructure.CrossCutting.IoC;
-using TaskoMask.Services.Identity.Infrastructure.Data.DataProviders;
+using TaskoMask.Services.Identity.Api.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddCustomSerilog();
-
-builder.Services.AddProjectServices(builder.Configuration);
-
-builder.Services.AddWebApiPreConfigured(builder.Configuration);
-
-var app = builder.Build();
-
-app.UseSerilogRequestLogging();
-
-app.UseWebApiPreConfigured(app.Services, builder.Environment);
-
-DbSeedData.SeedEssentialData(app.Services);
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+var app = builder.ConfigureServices().ConfigurePipeline();
 
 app.Run();

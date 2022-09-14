11dd989555d26f571d44e2a7b0ffd9955eccebff
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 01:06:10 +0430

    feat: add Asp Identity configuration

---
+++ src/2-Services/Identity/Api/Identity.Api/Configuration/AspNetIdentityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TaskoMask.Services.Identity.Domain.Entities;
+using TaskoMask.Services.Identity.Infrastructure.Data.DbContext;
+
+namespace TaskoMask.Services.Identity.Api.Configuration
+{
+    internal static class AspNetIdentityExtensions
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddAspNetIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                configuration.GetSection("Identity:Options").Bind(options);
+            });
+        }
+    }
+}

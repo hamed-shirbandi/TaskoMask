41208e885f158b636f30dfe9fb4004b75436e6e4
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 01:06:27 +0430

    feat: add IdentityServer configuration

---
+++ src/2-Services/Identity/Api/Identity.Api/Configuration/IdentityServerConfig.cs
@@ -0,0 +1,50 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace TaskoMask.Services.Identity.Api.Configuration
+{
+    internal static class IdentityServerConfig
+    {
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope(name:"Owner.Api",displayName:"Owner Api")
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            new Client
+            {
+                ClientId="UserPanel",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = { "Owner.Api" }
+            },
+
+            new Client
+            {
+                ClientId = "AdminPanel",
+                ClientSecrets = { new Secret("secret2".Sha256()) },
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = { "https://localhost:5013/signin-oidc" },
+                PostLogoutRedirectUris = { "https://localhost:5013/signout-callback-oidc" },
+                AllowOfflineAccess = true,
+                UpdateAccessTokenClaimsOnRefresh = true,
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "Owner.Api"
+                }
+            }
+        };
+    }
+}
---
+++ src/2-Services/Identity/Api/Identity.Api/Configuration/IdentityServerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using TaskoMask.Services.Identity.Domain.Entities;
+
+namespace TaskoMask.Services.Identity.Api.Configuration
+{
+    internal static class IdentityServerExtensions
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddIdentityServer(this IServiceCollection services )
+        {
+            services.AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+
+                    // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+                    options.EmitStaticAudienceClaim = true;
+                })
+                .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
+                .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
+                .AddInMemoryClients(IdentityServerConfig.Clients)
+                .AddAspNetIdentity<User>();
+        }
+    }
+}

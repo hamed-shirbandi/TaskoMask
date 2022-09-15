0dff42c0fc917c2587eceee7e4c119d5d46c6c1e
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 14:06:49 +0430

    chore: remove logout page

--- src/2-Services/Identity/Api/Identity.Api/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,9 +0,0 @@
-
-namespace TaskoMask.Services.Identity.Api.Pages.Logout
-{
-    public class LogoutOptions
-    {
-        public static bool ShowLogoutPrompt = false;
-        public static bool AutomaticRedirectAfterSignOut = true;
-    }
-}

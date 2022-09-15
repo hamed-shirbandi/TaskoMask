570970a1a60397733dc48da932abb7c4848306fe
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 15:06:27 +0430

    chore: remove models from login page

--- src/2-Services/Identity/Api/Identity.Api/Pages/Account/Login/LoginOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace TaskoMask.Services.Identity.Api.Pages.Login
-{
-    public class LoginOptions
-    {
-        public static bool AllowRememberLogin = true;
-        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
-    }
-}

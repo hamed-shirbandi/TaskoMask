fef88a23ce190bc4ad5e872ed62bea44a09e2f8e
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-15 20:27:14 +0430

    chore: add validation metadata to UserLoginRequest

--- src/2-Services/Identity/Libraries/Application/UseCases/UserLogin/UserLoginRequest.cs
+++ src/2-Services/Identity/Libraries/Application/UseCases/UserLogin/UserLoginRequest.cs
@@ -1,15 +1,16 @@
 ﻿using MediatR;
 using System.ComponentModel.DataAnnotations;
 using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
 
 namespace TaskoMask.Services.Identity.Application.UseCases.UserLogin
 {
     public class UserLoginRequest: IRequest<Result>
     {
-        [Required]
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
         public string UserName { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
         public string Password { get; set; }
 
         public bool RememberLogin { get; set; }

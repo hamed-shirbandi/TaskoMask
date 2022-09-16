598cca11601ecb4574602039e0960351a66cf2e7
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-17 03:30:26 +0430

    chore: remove admin panel api gateway

--- src/3-ApiGateways/AdminPanel/ApiGateWay/Program.cs
+++
@@ -1,7 +0,0 @@
-using TaskoMask.ApiGateways.AdminPanel.ApiGateWay.Configuration;
-
-var builder = WebApplication.CreateBuilder(args);
-
-var app = builder.ConfigureServices().ConfigurePipeline();
-
-app.Run();

db1d4e19dd1359fc7597b7cd2eca8e132de866e5
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-17 02:24:13 +0430

    feat: add integration test class for UserLoginUseCase

---
+++ src/2-Services/Identity/Tests/IntegrationTests/UseCases/UserLoginUseCaseTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Contracts.Enums;
+using TaskoMask.Services.Identity.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace TaskoMask.Services.Identity.IntegrationTests.UseCases
+{
+    public class UserLoginUseCaseTest : IClassFixture<UserClassFixture>
+    {
+
+        #region Fields
+
+        private readonly UserClassFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public UserLoginUseCaseTest(UserClassFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #endregion
+
+
+        #region Test Methods
+
+
+
+
+
+        #endregion
+    }
+}

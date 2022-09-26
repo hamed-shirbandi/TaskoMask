f6d40668184b2c638dee990a5e68c899a63a647d
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-26 22:23:25 +0330

    test: add a test for RegisterNewUserUseCase

---
+++ src/2-Services/Identity/Tests/IntegrationTests/UseCases/RegisterNewUserUseCaseTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using TaskoMask.Services.Identity.Application.UseCases.RegisterNewUser;
+using TaskoMask.Services.Identity.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace TaskoMask.Services.Identity.IntegrationTests.UseCases
+{
+    public class RegisterNewUserUseCaseTest : IClassFixture<UserClassFixture>
+    {
+
+        #region Fields
+
+        private readonly UserClassFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public RegisterNewUserUseCaseTest(UserClassFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+        [Fact]
+        public async Task User_Is_Registered()
+        {
+            //Arrange
+            var useCase = new RegisterNewUserUseCase(_fixture.UserManager);
+            var request = new RegisterNewUserRequest
+            {
+                Email = "test@taskomask.ir",
+                Password = "TestPass",
+            };
+
+            //Act
+            var result = await useCase.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.IsSuccess.Should().BeTrue();
+        }
+
+
+        #endregion
+    }
+}

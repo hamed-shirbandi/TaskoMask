276e6d711137def12357ba97b14e114e6eb7ca7d
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-26 22:24:28 +0330

    feat: add UserBuilder for unit tests

---
+++ src/2-Services/Identity/Tests/UnitTests/TestData/UserBuilder.cs
@@ -0,0 +1,59 @@
+﻿using TaskoMask.Services.Identity.Domain.Entities;
+
+namespace TaskoMask.Services.Identity.UnitTests.TestData
+{
+    internal class UserBuilder
+    {
+        public string Email { get; private set; }
+        public string UserName { get; private set; }
+        public bool IsActive { get; private set; }
+
+
+        private UserBuilder()
+        {
+
+        }
+
+
+        public static UserBuilder Init()
+        {
+            return new UserBuilder();
+        }
+
+
+
+        public UserBuilder WithEmail(string email)
+        {
+            Email = email;
+            return this;
+        }
+
+
+        public UserBuilder WithUserName(string userName)
+        {
+            UserName = userName;
+            return this;
+        }
+
+
+        public UserBuilder WithIsActive(bool isActive)
+        {
+            IsActive = isActive;
+            return this;
+        }
+
+
+
+        public User Build()
+        {
+           return  new User
+            {
+                UserName = UserName,
+                Email = Email,
+                IsActive = IsActive,
+            };
+        }
+
+
+    }
+}

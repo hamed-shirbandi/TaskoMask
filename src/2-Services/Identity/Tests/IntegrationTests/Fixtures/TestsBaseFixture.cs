7165873f4c85f71a3089bc6c4acad30f6416848d
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-26 20:01:45 +0330

    chore: some changes for TestsBaseFixture comments

--- src/2-Services/Identity/Tests/IntegrationTests/Fixtures/TestsBaseFixture.cs
+++ src/2-Services/Identity/Tests/IntegrationTests/Fixtures/TestsBaseFixture.cs
@@ -8,28 +8,24 @@ namespace TaskoMask.Services.Identity.IntegrationTests.Fixtures
 {
     /// <summary>
     /// Each test class must have its own fixture and each fixture initialize and dispose a unique database
-    /// So, we have control over parallel test run and lower cost for creating database for tests
+    /// So, we have control over parallel tests run and lower cost for creating database for tests
     /// ------------------* But *-----------------------------------------------
     /// If you want the fixture to be initialized and disposed for each test method in a class
     /// You just need to make a new fixture class by Inheriting from TestsBaseFixture (public class MyFixture:TestsBaseFixture)
     /// And use that new fixture class as a base for the test class (public class MyTestsClass:MyFixture)
     /// So the fixture initialize before each test method and then dispose after that test run
     /// ------------------* But *-----------------------------------------------
-    /// If you want to share TestsBaseFixture for all test methods in a Test Class
-    /// You just need to Inherit from IClassFixture<TestsBaseFixture> for that class
-    /// And get TestsBaseFixture as a parameter in test class constructor
+    /// If you want to share a fixture for all test methods in a Test Class
+    /// You just need to Inherit from IClassFixture<TestsBaseFixture> for that test class
+    /// And get TestsBaseFixture as a parameter in the constructor
     /// So the TestsBaseFixture initialize before all test methods in that test class and then dispose after all tests run
     /// ------------------* But *-----------------------------------------------
-    /// If you want TestsBaseFixture to be initialized and disposed for each test method in the class
-    /// You just need to  Inherit from TestsBaseFixture for that class
-    /// So the TestsBaseFixture initialize before each test method and then dispose after that test run
-    /// ------------------* But *-----------------------------------------------
-    /// If you want to share TestsBaseFixture for all test methods in some Test Classes
-    /// You just need to make a new class inherited from ICollectionFixture<OwnerCollectionFixture>
+    /// If you want to share a fixture for all test methods in some Test Classes
+    /// You just need to make a new class inherited from ICollectionFixture<TestsBaseFixture>
     /// Then apply [CollectionDefinition("my Collection Fixture")] attribute for that new class
-    /// And then apply [Collection("my Collection Fixture")] attribute for those test classes you want to share the fixture between
-    /// And get TestsBaseFixture as a parameter in each test class constructor
-    /// So the TestsBaseFixture initialize before all test methods in all test classes and then dispose after all tests run
+    /// And then apply [Collection("my Collection Fixture")] attribute for those test classes you want to share the fixture between them
+    /// And get the fixture as a parameter in each test class constructor
+    /// So the fixture initialize before all test methods in all test classes and then dispose after all tests run
     /// </summary>
     public abstract class TestsBaseFixture : IDisposable
     {

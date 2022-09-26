a0b3fda192facb47bbbe210a394860b11801e985
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-26 22:25:03 +0330

    chore: add UserLoginUseCaseTest class for unit tests

---
+++ src/2-Services/Identity/Tests/UnitTests/UseCases/UserLoginUseCaseTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using TaskoMask.Services.Identity.UnitTests.TestData;
+using Xunit;
+
+namespace TaskoMask.Services.Identity.UnitTests.UseCases
+{
+    public class UserLoginUseCaseTest 
+    {
+
+
+
+        
+    }
+}

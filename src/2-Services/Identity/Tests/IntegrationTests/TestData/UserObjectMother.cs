85064c9d7a7d23b9d88e73aef96f076cd3f60028
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-26 22:22:25 +0330

    feat: add UserObjectMother

---
+++ src/2-Services/Identity/Tests/IntegrationTests/TestData/UserObjectMother.cs
@@ -0,0 +1,18 @@
+﻿using TaskoMask.Services.Identity.Domain.Entities;
+
+namespace TaskoMask.Services.Identity.IntegrationTests.TestData
+{
+    internal static class UserObjectMother
+    {
+        public static User GetActiveUserWithEmail(string email)
+        {
+            return new User
+            {
+                UserName = email,
+                Email = email,
+                IsActive = true,
+            };
+        }
+
+    }
+}

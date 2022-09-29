c22bd2147255243f8aa69b1affcc8e83c3bbcdb2
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-29 14:13:44 +0330

    refactor: extract TestUserManager

---
+++ src/2-Services/Identity/Tests/UnitTests/Helpers/TestUserManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using TaskoMask.Services.Identity.Domain.Entities;
+
+namespace TaskoMask.Services.Identity.UnitTests.Helpers
+{
+    public class TestUserManager : UserManager<User>
+    {
+        public TestUserManager() : base(Substitute.For<IUserStore<User>>(), Substitute.For<IOptions<IdentityOptions>>(), Substitute.For<IPasswordHasher<User>>(), Substitute.For<IEnumerable<IUserValidator<User>>>(), Substitute.For<IEnumerable<IPasswordValidator<User>>>(), Substitute.For<ILookupNormalizer>(), Substitute.For<IdentityErrorDescriber>(), Substitute.For<IServiceProvider>(), Substitute.For<ILogger<UserManager<User>>>())
+        {
+        }
+    }
+}

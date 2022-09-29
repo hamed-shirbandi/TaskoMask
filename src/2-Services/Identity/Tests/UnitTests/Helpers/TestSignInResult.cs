e5a74aeb07844f7fa0577c05399ec794b55385c9
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-09-29 14:12:57 +0330

    refactor: extract TestSignInResult

---
+++ src/2-Services/Identity/Tests/UnitTests/Helpers/TestSignInResult.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaskoMask.Services.Identity.UnitTests.Helpers
+{
+    public class TestSignInResult : SignInResult
+    {
+        public TestSignInResult(bool succeeded = false)
+        {
+            Succeeded = succeeded;
+        }
+    }
+}

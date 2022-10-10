706786dae2e4c510a411c79a95a9b8a06eb100fc
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-10 16:41:51 +0330

    feat: add EF base DbContext

---
+++ src/1-BuildingBlocks/Infrastructure/EntityFramework/EFBaseDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace TaskoMask.BuildingBlocks.Infrastructure.EntityFramework
+{
+    public abstract class EFBaseDbContext: DbContext
+    {
+        private readonly EFDatabaseOptions _databaseOptions;
+
+        public EFBaseDbContext(IOptions<EFDatabaseOptions> databaseOptions)
+        {
+            _databaseOptions = databaseOptions.Value;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            switch (_databaseOptions.DatabaseType)
+            {
+                case EFDatabaseType.SqlServer:
+                    optionsBuilder.UseSqlServer(_databaseOptions.Connection.Replace("[DatabaseName]", _databaseOptions.DatabaseName));
+                    break;
+                case EFDatabaseType.Sqlite:
+                    optionsBuilder.UseSqlite(_databaseOptions.Connection.Replace("[DatabaseName]", _databaseOptions.DatabaseName));
+                    break;
+                case EFDatabaseType.PostgreSQL:
+                    optionsBuilder.UseNpgsql(_databaseOptions.Connection.Replace("[DatabaseName]", _databaseOptions.DatabaseName));
+                    break;
+                case EFDatabaseType.InMemoryDatabase:
+                    optionsBuilder.UseInMemoryDatabase(databaseName: _databaseOptions.DatabaseName);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/1-BuildingBlocks/Infrastructure/EntityFramework/EFDatabaseOptions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TaskoMask.BuildingBlocks.Infrastructure.EntityFramework
+{
+    public class EFDatabaseOptions
+    {
+        public string Connection { get; set; }
+        public string DatabaseName { get; set; }
+        public EFDatabaseType DatabaseType { get; set; }
+    }
+
+    public enum EFDatabaseType
+    {
+        SqlServer,
+        Sqlite,
+        PostgreSQL,
+        InMemoryDatabase
+    }
+}

86c6c2bb57068e77887571a0bd0ab56847cf9fa3
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-10 17:19:13 +0330

    feat: add EFExtensions

---
+++ src/1-BuildingBlocks/Infrastructure/EntityFramework/EFExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace TaskoMask.BuildingBlocks.Infrastructure.EntityFramework
+{
+    public static class EFExtensions
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void InitialDatabase<TContext>(this IServiceProvider serviceProvider) where TContext :DbContext
+        {
+            using var serviceScope = serviceProvider.CreateScope();
+
+            var dbContext = serviceScope.ServiceProvider.GetService<TContext>();
+
+            dbContext.Database.EnsureCreated();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void DropDatabase<TContext>(this IServiceProvider serviceProvider) where TContext : DbContext
+        {
+            using var serviceScope = serviceProvider.CreateScope();
+
+            var dbContext = serviceScope.ServiceProvider.GetService<TContext>();
+
+            dbContext.Database.EnsureDeleted();
+        }
+
+    }
+}

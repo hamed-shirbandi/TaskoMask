349e58914093a82d055e864a7cf68956ec324530
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-12 00:06:49 +0330

    refactor: prepare board read service template architecture
    refers to issue #73

---
+++ src/2-Services/Board/Read/Api/Board.Read.Api/Infrastructure/Mapper/OwnerMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace TaskoMask.Services.Owner.Read.Api.Infrastructure.Mapper
+{
+    public class OwnerMappingProfile : Profile
+    {
+        public OwnerMappingProfile()
+        {
+
+          //  CreateMap<Owner, OwnerBasicInfoDto>();
+
+        }
+    }
+}

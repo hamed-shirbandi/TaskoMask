063a404ec4f4d714d85e08c827cd96a66c509370
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-12 14:04:58 +0330

    refactor: Prepare task read service template architecture

---
+++ src/2-Services/Task/Read/Api/Task.Read.Api/Infrastructure/DbContext/DbInitialization.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Driver;
+
+namespace TaskoMask.Services.Task.Read.Api.Infrastructure.DbContext
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DbInitialization
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void InitialDatabasesAndSeedEssentialData(this IServiceProvider serviceProvider)
+        {
+            serviceProvider.SeedEssentialData();
+            serviceProvider.CreateIndexes();
+        }
+
+
+
+        /// <summary>
+        /// Seed the necessary data that system needs
+        /// </summary>
+        public static void SeedEssentialData(this IServiceProvider serviceProvider)
+        {
+            using var serviceScope = serviceProvider.CreateScope();
+            var dbContext = serviceScope.ServiceProvider.GetService<TaskReadDbContext>();
+            var configuration = serviceScope.ServiceProvider.GetService<IConfiguration>();
+
+            // var boards = dbContext.GetCollection<Task>();
+            // seed data ...
+        }
+
+
+
+        /// <summary>
+        /// Create index for collections
+        /// </summary>
+        public static void CreateIndexes(this IServiceProvider serviceProvider)
+        {
+            using var serviceScope = serviceProvider.CreateScope();
+            var dbContext = serviceScope.ServiceProvider.GetService<TaskReadDbContext>();
+
+            //dbContext.GetCollection<Owner>().Indexes.CreateOneAsync(new CreateIndexModel<Owner>(Builders<Owner>.IndexKeys.Ascending(x => x.Id), new CreateIndexOptions() { Name = nameof(Owner.Id), Unique = true }));
+        }
+
+    }
+}
---
+++ src/2-Services/Task/Read/Api/Task.Read.Api/Infrastructure/Mapper/OwnerMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace TaskoMask.Services.Task.Read.Api.Infrastructure.Mapper
+{
+    public class OwnerMappingProfile : Profile
+    {
+        public OwnerMappingProfile()
+        {
+
+          //  CreateMap<Owner, OwnerBasicInfoDto>();
+
+        }
+    }
+}

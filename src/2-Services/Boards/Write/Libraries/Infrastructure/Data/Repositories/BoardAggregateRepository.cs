7f2a2e65189574125704685cf4e1c3a7b656f1fa
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-13 17:28:29 +0330

    refactor: move board repository to board write service

---
+++ src/2-Services/Boards/Write/Libraries/Infrastructure/Data/Repositories/BoardAggregateRepository.cs
@@ -0,0 +1,61 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using TaskoMask.BuildingBlocks.Infrastructure.MongoDB;
+using TaskoMask.Services.Boards.Write.Domain.Data;
+using TaskoMask.Services.Boards.Write.Domain.Entities;
+using TaskoMask.Services.Boards.Write.Infrastructure.Data.DbContext;
+
+namespace TaskoMask.Services.Boards.Write.Infrastructure.Data.Repositories
+{
+    public class BoardAggregateRepository : MongoDbBaseAggregateRepository<Board>, IBoardAggregateRepository
+    {
+        #region Fields
+
+        private readonly IMongoCollection<Board> _boards;
+
+        #endregion
+
+        #region Ctors
+
+        public BoardAggregateRepository(BoardWriteDbContext dbContext) : base(dbContext)
+        {
+            _boards = dbContext.GetCollection<Board>();
+        }
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool ExistBoard(string boardId, string projectId, string boardName)
+        {
+            var board = _boards.Find(e => e.ProjectId.Value == projectId && e.Name.Value == boardName).FirstOrDefault();
+            return board != null && board.Id != boardId;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Board> GetByCardIdAsync(string cardId)
+        {
+            return await _boards.Find(e => e.Cards.Any(c => c.Id == cardId)).FirstOrDefaultAsync();
+        }
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+    }
+}

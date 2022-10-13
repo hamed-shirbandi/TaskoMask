c78fc0d8689e3f9e319d4846fcf2c0990561b9d6
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-13 17:20:55 +0330

    refactor: move domain from monollith to task read service

---
+++ src/2-Services/Tasks/Read/Api/Tasks.Read.Api/Domain/Activity.cs
@@ -0,0 +1,20 @@
+﻿using TaskoMask.BuildingBlocks.Domain.Models;
+using MongoDB.Bson;
+
+namespace TaskoMask.Services.Tasks.Read.Api.Domain
+{
+    public class Activity : BaseEntity
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Activity( )
+        {
+            SetId(ObjectId.GenerateNewId().ToString());
+        }
+
+        public string TaskId { get; set; }
+        public string Description { get; set; }
+
+    }
+}

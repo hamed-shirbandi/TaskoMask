f04a420b7a08cddce3d802c0242bd6de3657be5a
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-15 15:56:11 +0330

    chore: remove unused models from building blocks

--- src/1-BuildingBlocks/Web.MVC/Models/JwtAuthenticationOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaskoMask.BuildingBlocks.Web.MVC.Models
-{
-    public class JwtAuthenticationOptions
-    {
-        public string Key { get; set; }
-        public string Issuer { get; set; }
-        public int ExpireDays { get; set; }
-    }
-}

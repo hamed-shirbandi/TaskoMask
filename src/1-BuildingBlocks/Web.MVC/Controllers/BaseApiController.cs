eca5d81564fcbc4211a2fb75e0df8ca932efd939
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-15 17:37:21 +0330

    refactor: refactor BaseApiController

--- src/1-BuildingBlocks/Web.MVC/Controllers/BaseApiController.cs
+++ src/1-BuildingBlocks/Web.MVC/Controllers/BaseApiController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Application.Commands;
+using TaskoMask.BuildingBlocks.Application.Notifications;
+using TaskoMask.BuildingBlocks.Application.Queries;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
 using TaskoMask.BuildingBlocks.Contracts.Services;
 
 namespace TaskoMask.BuildingBlocks.Web.MVC.Controllers
@@ -9,20 +13,19 @@ namespace TaskoMask.BuildingBlocks.Web.MVC.Controllers
         #region Fields
 
         private readonly IAuthenticatedUserService _authenticatedUserService;
+        private readonly IInMemoryBus _inMemoryBus;
+        private readonly INotificationHandler _notifications;
 
         #endregion
 
         #region Ctors
 
 
-        public BaseApiController( )
-        {
-        }
-
-
-        public BaseApiController(IAuthenticatedUserService authenticatedUserService)
+        public BaseApiController(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus, INotificationHandler notifications)
         {
             _authenticatedUserService = authenticatedUserService;
+            _inMemoryBus = inMemoryBus;
+            _notifications = notifications;
         }
 
 
@@ -30,6 +33,47 @@ namespace TaskoMask.BuildingBlocks.Web.MVC.Controllers
         #endregion
 
         #region Protected Methods
+
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected async Task<Result<CommandResult>> SendCommandAsync<TCommand>(TCommand cmd) where TCommand : BaseCommand
+        {
+            var result = await _inMemoryBus.Send(cmd);
+
+            //get notification errors
+            var errors = _notifications.GetErrors();
+
+            //result is null when throw application or domain exception 
+            if (result == null)
+                return Result.Failure<CommandResult>(errors);
+
+            //if there is any notification error so result is failed
+            if (errors.Count > 0)
+                return Result.Failure<CommandResult>(errors, result.Message);
+
+            return Result.Success(result, result.Message);
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected async Task<Result<TQuery>> SendQueryAsync<TQuery>(BaseQuery<TQuery> query)
+        {
+            var result = await _inMemoryBus.Send(query);
+            if (_notifications.HasAny())
+                return Result.Failure<TQuery>(_notifications.GetErrors());
+
+            return Result.Success(result);
+        }
+
 
 
 

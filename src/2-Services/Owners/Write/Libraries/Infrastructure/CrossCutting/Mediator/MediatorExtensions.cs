bf0c84abe0032d22bb9c66478f5635b3f7c7abe8
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-15 18:50:32 +0330

    chore: add mediator services

--- src/2-Services/Owners/Write/Libraries/Infrastructure/CrossCutting/Mediator/MediatorExtensions.cs
+++ src/2-Services/Owners/Write/Libraries/Infrastructure/CrossCutting/Mediator/MediatorExtensions.cs
@@ -1,8 +1,7 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using FluentValidation;
-using TaskoMask.BuildingBlocks.Application.Exceptions;
-using TaskoMask.BuildingBlocks.Infrastructure.Bus;
+using TaskoMask.Services.Owners.Write.Application.UseCases.Owners.RegiserOwner;
 
 namespace TaskoMask.Services.Owners.Write.Infrastructure.CrossCutting.Mediator
 {
@@ -15,10 +14,10 @@ namespace TaskoMask.Services.Owners.Write.Infrastructure.CrossCutting.Mediator
         public static IServiceCollection AddMediator(this IServiceCollection services)
         {
             //Load all commands and queries ...
-           // services.AddMediatR(typeof(UserLoginUseCase));
+            services.AddMediatR(typeof(RegiserOwnerUseCase));
 
             //Load all fluent validation to use in ValidationBehaviour
-           // services.AddValidatorsFromAssembly(typeof(UserLoginValidation<>).Assembly);
+            services.AddValidatorsFromAssembly(typeof(RegiserOwnerValidation<>).Assembly);
 
             return services;
         }

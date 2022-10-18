e14bf5ce56295b964b91947dd06260752e4665d4
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-18 15:57:53 +0330

    chore: add GetOwnerByIdHandler

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Features/GetOwnerById/GetOwnerByIdHandler.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using MediatR;
+using MongoDB.Driver;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Exceptions;
+using TaskoMask.BuildingBlocks.Application.Queries;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Owners;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+using TaskoMask.Services.Owners.Read.Api.Infrastructure.DbContext;
+
+namespace TaskoMask.Services.Owners.Read.Api.Features.GetOwnerById
+{
+    public class GetOwnerByIdHandler : BaseQueryHandler, IRequestHandler<GetOwnerByIdRequest, OwnerBasicInfoDto>
+    {
+        #region Fields
+
+        private readonly OwnerReadDbContext _ownerReadDbContext;
+
+        #endregion
+
+        #region Ctors
+
+        public GetOwnerByIdHandler(OwnerReadDbContext ownerReadDbContext, IMapper mapper) : base(mapper)
+        {
+            _ownerReadDbContext = ownerReadDbContext;
+        }
+
+        #endregion
+
+        #region Handlers
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<OwnerBasicInfoDto> Handle(GetOwnerByIdRequest request, CancellationToken cancellationToken)
+        {
+            var owner = await _ownerReadDbContext.Owners.Find(e => e.Id == request.Id).FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+            if (owner == null)
+                throw new ApplicationException(ContractsMessages.Data_Not_exist, DomainMetadata.Owner);
+
+            return _mapper.Map<OwnerBasicInfoDto>(owner);
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+
+        #endregion
+    }
+}

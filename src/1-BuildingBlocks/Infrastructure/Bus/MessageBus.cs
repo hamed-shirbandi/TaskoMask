4a794929e75d832b67bde2bfc56869bfcf69e7fa
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-21 17:42:26 +0330

    chore: implement IMessageBus by using MassTransit

---
+++ src/1-BuildingBlocks/Infrastructure/Bus/MessageBus.cs
@@ -0,0 +1,52 @@
+﻿using MassTransit;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Domain.Events;
+
+namespace TaskoMask.BuildingBlocks.Infrastructure.Bus
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MessageBus : IMessageBus
+    {
+        #region Fields
+
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        #endregion
+
+        #region Ctors
+
+        public MessageBus(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task Publish(DomainEvent @event)
+        {
+            await _publishEndpoint.Publish(@event);
+        }
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}
+

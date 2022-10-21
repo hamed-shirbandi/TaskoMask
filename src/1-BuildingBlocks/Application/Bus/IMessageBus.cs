1e5458f1fe7f580709c54f36bbcdb1ce99a685f1
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-21 17:38:10 +0330

    feat: add IMessageBus
    to be used for publishing events between microservices

---
+++ src/1-BuildingBlocks/Application/Bus/IMessageBus.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Domain.Events;
+
+namespace TaskoMask.BuildingBlocks.Application.Bus
+{
+    public interface IMessageBus
+    {
+        Task Publish(DomainEvent @event);
+    }
+}

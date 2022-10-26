c5d865d444db360036441c1200f6e2917c90fb56
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-26 21:52:53 +0330

    feat: addConsumerFaultException

---
+++ src/1-BuildingBlocks/Web.MVC/Exceptions/ConsumerFaultException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TaskoMask.BuildingBlocks.Web.MVC.Exceptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ConsumerFaultException : Exception
+    {
+        public ConsumerFaultException(string message): base(message)
+        {
+        }
+
+    }
+}

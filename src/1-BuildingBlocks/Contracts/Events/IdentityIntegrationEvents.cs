c41c13fec0bf3979f6d53810e96897d86b2a0706
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-28 15:05:53 +0330

    feat: add identity integration events

---
+++ src/1-BuildingBlocks/Contracts/Events/IdentityIntegrationEvents.cs
@@ -0,0 +1,8 @@
+﻿
+using TaskoMask.BuildingBlocks.Contracts.Models;
+
+namespace TaskoMask.BuildingBlocks.Contracts.Events
+{
+    public record NewUserRegistered(string Id, string UserName, string Email) : IntegrationEvent;
+
+}

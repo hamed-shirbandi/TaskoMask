1323b64f333d8ea064b43e43f3dcd876ccd52225
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-28 15:05:47 +0330

    feat: add task integration events

---
+++ src/1-BuildingBlocks/Contracts/Events/TaskIntegrationEvents.cs
@@ -0,0 +1,17 @@
+﻿using TaskoMask.BuildingBlocks.Contracts.Models;
+
+namespace TaskoMask.BuildingBlocks.Contracts.Events
+{
+    public record TaskAdded(string Id, string Title, string Description, string CardId, string BoardId) : IntegrationEvent;
+    public record TaskDeleted(string Id) : IntegrationEvent;
+    public record TaskUpdated(string Id, string Title, string Description) : IntegrationEvent;
+    public record TaskMovedToAnotherCard(string TaskId, string CardId) : IntegrationEvent;
+
+
+
+    public record CommentAdded(string Id, string Content, string TaskId) : IntegrationEvent;
+    public record CommentDeleted(string Id) : IntegrationEvent;
+    public record CommentUpdated(string Id, string Content) : IntegrationEvent;
+
+
+}

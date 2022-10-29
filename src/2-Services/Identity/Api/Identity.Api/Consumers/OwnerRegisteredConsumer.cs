6e04385906aa0ad62b157a4e8550c48fe4a452f1
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-29 16:48:16 +0330

    feat: implement OwnerRegisteredConsumer

--- src/2-Services/Identity/Api/Identity.Api/Consumers/OwnerRegisteredConsumer.cs
+++ src/2-Services/Identity/Api/Identity.Api/Consumers/OwnerRegisteredConsumer.cs
@@ -1,32 +1,31 @@
 ﻿using MassTransit;
 using TaskoMask.BuildingBlocks.Application.Bus;
 using TaskoMask.BuildingBlocks.Application.Notifications;
-using TaskoMask.Services.Identity.Domain.Events;
 using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
 using TaskoMask.BuildingBlocks.Web.MVC.Exceptions;
 using TaskoMask.Services.Identity.Application.UseCases.RegisterNewUser;
+using TaskoMask.BuildingBlocks.Contracts.Events;
 
 namespace TaskoMask.Services.Identity.Api.Consumers
 {
-    //public class OwnerRegisteredConsumer : BaseConsumer<OwnerRegisteredEvent>
-    //{
+    /// <summary>
+    /// After registering an owner, we must register a user for that owner to handle its identity (login,logout,etc)
+    /// </summary>
+    public class OwnerRegisteredConsumer : BaseConsumer<OwnerRegistered>
+    {
 
 
-    //    public OwnerRegisteredConsumer(IInMemoryBus inMemoryBus, INotificationHandler notifications, IMessageBus messageBus) : base(inMemoryBus, notifications, messageBus)
-    //    {
-    //    }
+        public OwnerRegisteredConsumer(IInMemoryBus inMemoryBus, INotificationHandler notifications) : base(inMemoryBus, notifications)
+        {
+        }
 
 
-    //    public override async Task ConsumeMessage(ConsumeContext<OwnerRegisteredEvent> context)
-    //    {
-    //        var registerNewUser = new RegisterNewUserRequest(context.Message.Email, context.Message.Password);
-    //        var result = await SendCommandAsync(registerNewUser);
-    //        if (result.IsSuccess)
-    //            await _messageBus.Publish(new NewUserRegisteredEvent(result.Value.EntityId, context.Message.Email, context.Message.Email));
-    //        else
-    //            // Cause to publish Fault<OwnerRegisteredEvent> message
-    //            throw new ConsumerFaultException(result.Message);
-
-    //    }
-    //}
+        public override async Task ConsumeMessage(ConsumeContext<OwnerRegistered> context)
+        {
+            var registerNewUser = new RegisterNewUserRequest(context.Message.Email, context.Message.Password);
+            var result = await SendCommandAsync(registerNewUser);
+            if (!result.IsSuccess)
+                throw new ConsumerFaultException(result.Message); // Cause to publish Fault<OwnerRegistered> message
+        }
+    }
 }

78282d8ae35f390be1be10e8e2d94a9b73ab86e9
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-30 15:50:56 +0330

    chore: add OwnerUpdatingProfileCompletedConsumer

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Consumers/OwnerUpdatingProfileCompletedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.Services.Owners.Read.Api.Infrastructure.DbContext;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace TaskoMask.Services.Owners.Read.Api.Consumers
+{
+    public class OwnerUpdatingProfileCompletedConsumer : BaseConsumer<OwnerUpdatingProfileCompleted>
+    {
+        private readonly OwnerReadDbContext _ownerReadDbContext;
+
+
+        public OwnerUpdatingProfileCompletedConsumer(IInMemoryBus inMemoryBus, OwnerReadDbContext ownerReadDbContext) : base(inMemoryBus)
+        {
+            _ownerReadDbContext = ownerReadDbContext;
+        }
+
+
+        public override async Task ConsumeMessage(ConsumeContext<OwnerUpdatingProfileCompleted> context)
+        {
+            var owner = await _ownerReadDbContext.Owners.Find(e => e.Id == context.Message.Id).FirstOrDefaultAsync();
+            
+            owner.Email = context.Message.Email;
+            owner.DisplayName = context.Message.DisplayName;
+           
+            await _ownerReadDbContext.Owners.ReplaceOneAsync(p => p.Id == owner.Id, owner, new ReplaceOptions() { IsUpsert = false });
+        }
+    }
+}

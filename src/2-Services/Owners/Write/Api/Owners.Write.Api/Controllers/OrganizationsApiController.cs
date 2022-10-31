793aa13b6ae6e82d42834bf16afca5256847078e
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-31 20:18:35 +0330

    feat: add organization api controller for owner write service

---
+++ src/2-Services/Owners/Write/Api/Owners.Write.Api/Controllers/OrganizationsApiController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Organizations;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Api.Organizations;
+
+namespace TaskoMask.Services.Owners.Write.Api.Controllers
+{
+    public class OrganizationsApiController : BaseApiController, IOrganizationWriteApiService
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Ctors
+
+        public OrganizationsApiController(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        /// <summary>
+        /// Add new organization for current owner
+        /// </summary>
+        [HttpPost]
+        [Route("organizations")]
+        public async Task<Result<CommandResult>> Add([FromBody] AddOrganizationDto input)
+        {
+            //input.OwnerId = GetCurrentUserId();
+            return Result.Failure<CommandResult>();
+        }
+
+
+
+        /// <summary>
+        /// Update an existing organization
+        /// </summary>
+        [HttpPut]
+        [Route("organizations/{id}")]
+        public async Task<Result<CommandResult>> Update(string id,[FromBody] UpdateOrganizationDto input)
+        {
+            return Result.Failure<CommandResult>();
+
+        }
+
+
+
+        /// <summary>
+        /// Delete an organization
+        /// </summary>
+        [HttpDelete]
+        [Route("organizations/{id}")]
+        public async Task<Result<CommandResult>> Delete(string id)
+        {
+            return Result.Failure<CommandResult>();
+        }
+
+
+
+        #endregion
+
+    }
+}

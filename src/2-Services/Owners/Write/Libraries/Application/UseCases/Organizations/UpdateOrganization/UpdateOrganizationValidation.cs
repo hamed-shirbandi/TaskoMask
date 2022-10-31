1ab36c7ddf353888f2602e30482403c83165013a
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-31 20:37:42 +0330

    feat: implement UpdateOrganizationUseCase

---
+++ src/2-Services/Owners/Write/Libraries/Application/UseCases/Organizations/UpdateOrganization/UpdateOrganizationValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+
+namespace TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.UpdateOrganization
+{
+    public abstract class UpdateOrganizationValidation<TRequest> : AbstractValidator<TRequest> where TRequest : UpdateOrganizationRequest
+    {
+        public UpdateOrganizationValidation()
+        {
+            ValidateDescription();
+        }
+
+
+        private void ValidateDescription()
+        {
+            RuleFor(o => o.Description).NotEqual(o => o.Name).WithMessage(DomainMessages.Equal_Name_And_Description_Error);
+        }
+    }
+}

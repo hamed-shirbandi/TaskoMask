1b9e1cf95d483b74f6ad922292e6a553a322d71d
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-10-31 20:39:23 +0330

    feat: implement DeleteOrganizationUseCase

---
+++ src/2-Services/Owners/Write/Libraries/Application/UseCases/Organizations/DeleteOrganization/DeleteOrganizationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskoMask.BuildingBlocks.Application.Commands;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+
+namespace TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.DeleteOrganization
+{
+    public class DeleteOrganizationRequest: BaseCommand
+    {
+        public DeleteOrganizationRequest(string id)
+        {
+            Id = id;
+        }
+
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string Id { get; }
+
+    }
+}
---
+++ src/2-Services/Owners/Write/Libraries/Application/UseCases/Organizations/DeleteOrganization/DeleteOrganizationValidation.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.DeleteOrganization
+{
+    public abstract class DeleteOrganizationValidation<TRequest> : AbstractValidator<TRequest> where TRequest : DeleteOrganizationRequest
+    {
+        public DeleteOrganizationValidation()
+        {
+        }
+
+    }
+}

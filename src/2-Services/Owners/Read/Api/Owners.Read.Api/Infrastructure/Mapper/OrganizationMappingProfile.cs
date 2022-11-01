1c135df025c3fef5b0aa32b60883bfbb48a798ea
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-01 16:11:53 +0330

    fix: add OrganizationMappingProfile

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Infrastructure/Mapper/OrganizationMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Organizations;
+using TaskoMask.Services.Owners.Read.Api.Domain;
+
+namespace TaskoMask.Services.Owners.Read.Api.Infrastructure.Mapper
+{
+    public class OrganizationMappingProfile : Profile
+    {
+        public OrganizationMappingProfile()
+        {
+            CreateMap<Organization, OrganizationBasicInfoDto>();
+        }
+    }
+}

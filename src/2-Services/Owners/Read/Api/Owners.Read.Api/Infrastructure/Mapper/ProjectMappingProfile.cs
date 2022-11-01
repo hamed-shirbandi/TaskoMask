f011b8035f3b1d63441d951f105d60063281be8a
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-01 16:12:10 +0330

    fix: add ProjectMappingProfile

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Infrastructure/Mapper/ProjectMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Projects;
+using TaskoMask.Services.Owners.Read.Api.Domain;
+
+namespace TaskoMask.Services.Owners.Read.Api.Infrastructure.Mapper
+{
+    public class ProjectMappingProfile : Profile
+    {
+        public ProjectMappingProfile()
+        {
+            CreateMap<Project, ProjectBasicInfoDto>();
+        }
+    }
+}

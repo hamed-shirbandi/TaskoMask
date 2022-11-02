b4cbc11a93cff3a2ea72693e1ed16efffba8b54a
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-02 16:57:39 +0330

    feat: add OrganizationUpdatedConsumer

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Consumers/Organizations/OrganizationUpdatedConsumer.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.Services.Owners.Read.Api.Infrastructure.DbContext;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace TaskoMask.Services.Owners.Read.Api.Consumers.Organizations
+{
+    public class OrganizationUpdatedConsumer : BaseConsumer<OrganizationUpdated>
+    {
+        private readonly OwnerReadDbContext _ownerReadDbContext;
+
+
+        public OrganizationUpdatedConsumer(IInMemoryBus inMemoryBus, OwnerReadDbContext ownerReadDbContext) : base(inMemoryBus)
+        {
+            _ownerReadDbContext = ownerReadDbContext;
+        }
+
+
+        public override async Task ConsumeMessage(ConsumeContext<OrganizationUpdated> context)
+        {
+            var organization = await _ownerReadDbContext.Organizations.Find(e => e.Id == context.Message.Id).FirstOrDefaultAsync();
+
+            organization.Name = context.Message.Name;
+            organization.Description = context.Message.Description;
+            organization.SetAsUpdated();
+
+            await _ownerReadDbContext.Organizations.ReplaceOneAsync(p => p.Id == organization.Id, organization, new ReplaceOptions() { IsUpsert = false });
+        }
+    }
+}

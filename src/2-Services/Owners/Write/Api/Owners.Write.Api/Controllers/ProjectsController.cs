39a447208c26068eb63cbdd101a479ab626c2d8a
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-02 14:26:40 +0330

    feat: add project api controller for owner write service

---
+++ src/2-Services/Owners/Write/Api/Owners.Write.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Projects;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Api.OwProjectsners;
+
+namespace TaskoMask.Services.Owners.Write.Api.Controllers
+{
+    public class ProjectsController : BaseApiController, IProjectWriteApiService
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Ctors
+
+        public ProjectsController(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        /// <summary>
+        /// Add new project
+        /// </summary>
+        [HttpPost]
+        [Route("projects")]
+        public async Task<Result<CommandResult>> Add([FromBody] AddProjectDto input)
+        {
+            return Result.Failure<CommandResult>();
+        }
+
+
+
+        /// <summary>
+        /// Update an existing project
+        /// </summary>
+        [HttpPut]
+        [Route("projects/{id}")]
+        public async Task<Result<CommandResult>> Update(string id,[FromBody] UpdateProjectDto input)
+        {
+            return Result.Failure<CommandResult>();
+        }
+
+
+
+        /// <summary>
+        /// Delete a project
+        /// </summary>
+        [HttpDelete]
+        [Route("projects/{id}")]
+        public async Task<Result<CommandResult>> Delete(string id)
+        {
+            return Result.Failure<CommandResult>();
+        }
+
+
+
+        #endregion
+
+    }
+}

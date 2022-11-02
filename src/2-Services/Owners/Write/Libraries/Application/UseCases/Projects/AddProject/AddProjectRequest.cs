b6064cbbcbd3473752a3398a4643f927beaf3b9b
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-02 14:43:05 +0330

    feat: implement AddProjectUseCase

---
+++ src/2-Services/Owners/Write/Libraries/Application/UseCases/Projects/AddProject/AddProjectRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskoMask.BuildingBlocks.Application.Commands;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+
+namespace TaskoMask.Services.Owners.Write.Application.UseCases.Projects.AddProject
+{
+    public class AddProjectRequest: BaseCommand
+    {
+        public AddProjectRequest(string organizationId,string name, string description)
+        {
+            OrganizationId = organizationId;
+            Name = name;
+            Description = description;
+        }
+
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string OrganizationId { get; }
+
+
+        [StringLength(DomainConstValues.Project_Name_Max_Length, MinimumLength = DomainConstValues.Project_Name_Min_Length, ErrorMessageResourceName = nameof(ContractsMetadata.Length_Error), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string Name { get; }
+
+
+        [MaxLength(DomainConstValues.Project_Description_Max_Length, ErrorMessageResourceName = nameof(ContractsMetadata.Max_Length_Error), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string Description { get; }
+
+    }
+}
---
+++ src/2-Services/Owners/Write/Libraries/Application/UseCases/Projects/AddProject/AddProjectValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+
+namespace TaskoMask.Services.Owners.Write.Application.UseCases.Projects.AddProject
+{
+    public abstract class AddProjectValidation<TRequest> : AbstractValidator<TRequest> where TRequest : AddProjectRequest
+    {
+        public AddProjectValidation()
+        {
+            ValidateDescription();
+        }
+
+
+        private void ValidateDescription()
+        {
+            RuleFor(o => o.Description).NotEqual(o => o.Name).WithMessage(DomainMessages.Equal_Name_And_Description_Error);
+        }
+    }
+}

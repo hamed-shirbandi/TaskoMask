6e6f0a0903f4a0d569a64702e1834c60d600bfb3
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-02 16:42:44 +0330

    feat: add OrganizationAddedConsumer

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Consumers/OrganizationAddedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.Services.Owners.Read.Api.Infrastructure.DbContext;
+using System.Threading.Tasks;
+using TaskoMask.Services.Owners.Read.Api.Domain;
+
+namespace TaskoMask.Services.Owners.Read.Api.Consumers
+{
+    public class OrganizationAddedConsumer : BaseConsumer<OrganizationAdded>
+    {
+        private readonly OwnerReadDbContext _ownerReadDbContext;
+
+
+        public OrganizationAddedConsumer(IInMemoryBus inMemoryBus, OwnerReadDbContext ownerReadDbContext) : base(inMemoryBus)
+        {
+            _ownerReadDbContext = ownerReadDbContext;
+        }
+
+
+        public override async Task ConsumeMessage(ConsumeContext<OrganizationAdded> context)
+        {
+            var organization = new Organization(context.Message.Id)
+            {
+                Name = context.Message.Name,
+                Description = context.Message.Description,
+                OwnerId = context.Message.OwnerId,
+            };
+
+            await _ownerReadDbContext.Organizations.InsertOneAsync(organization);
+        }
+    }
+}

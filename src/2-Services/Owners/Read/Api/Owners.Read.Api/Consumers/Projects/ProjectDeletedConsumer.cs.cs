3f7786fba72a3e572e519357a6bc8cf0c53cd7d2
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-04 13:58:50 +0330

    feat: add ProjectDeletedConsumer

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Consumers/Projects/ProjectDeletedConsumer.cs.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.Services.Owners.Read.Api.Infrastructure.DbContext;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace TaskoMask.Services.Owners.Read.Api.Consumers.Projects
+{
+    public class ProjectDeletedConsumer : BaseConsumer<ProjectDeleted>
+    {
+        private readonly OwnerReadDbContext _ownerReadDbContext;
+
+
+        public ProjectDeletedConsumer(IInMemoryBus inMemoryBus, OwnerReadDbContext ownerReadDbContext) : base(inMemoryBus)
+        {
+            _ownerReadDbContext = ownerReadDbContext;
+        }
+
+
+        public override async Task ConsumeMessage(ConsumeContext<ProjectDeleted> context)
+        {
+            await _ownerReadDbContext.Projects.DeleteOneAsync(p => p.Id == context.Message.Id);
+        }
+    }
+}

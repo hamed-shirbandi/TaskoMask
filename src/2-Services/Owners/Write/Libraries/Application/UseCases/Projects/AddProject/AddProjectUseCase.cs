5bb81185b768345716c358129863fab94be329c4
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-04 13:56:14 +0330

    fix: publish ProjectAdded for AddProjectUseCase

--- src/2-Services/Owners/Write/Libraries/Application/UseCases/Projects/AddProject/AddProjectUseCase.cs
+++ src/2-Services/Owners/Write/Libraries/Application/UseCases/Projects/AddProject/AddProjectUseCase.cs
@@ -1,14 +1,19 @@
 ﻿using MediatR;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using TaskoMask.BuildingBlocks.Application.Bus;
 using TaskoMask.BuildingBlocks.Application.Commands;
 using TaskoMask.BuildingBlocks.Application.Exceptions;
+using TaskoMask.BuildingBlocks.Contracts.Events;
 using TaskoMask.BuildingBlocks.Contracts.Helpers;
 using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.BuildingBlocks.Domain.Models;
 using TaskoMask.BuildingBlocks.Domain.Resources;
 using TaskoMask.Services.Owners.Write.Domain.Data;
 using TaskoMask.Services.Owners.Write.Domain.Entities;
+using TaskoMask.Services.Owners.Write.Domain.Events.Projects;
 using TaskoMask.Services.Owners.Write.Domain.Services;
 
 namespace TaskoMask.Services.Owners.Write.Application.UseCases.Projects.AddProject
@@ -51,11 +56,28 @@ namespace TaskoMask.Services.Owners.Write.Application.UseCases.Projects.AddProje
             owner.AddProject(request.OrganizationId, project);
 
             await _ownerAggregateRepository.UpdateAsync(owner);
+
             await PublishDomainEventsAsync(owner.DomainEvents);
+
+            var projectAdded = MapProjectAddedIntegrationEvent(owner.DomainEvents);
+
+            await PublishIntegrationEventAsync(projectAdded);
 
             return new CommandResult(ContractsMessages.Create_Success, project.Id);
         }
 
+
+
+        #endregion
+
+        #region Private Methods
+
+
+        private ProjectAdded MapProjectAddedIntegrationEvent(IReadOnlyCollection<DomainEvent> domainEvents)
+        {
+            var projectAddedDomainEvent = (ProjectAddedEvent)domainEvents.FirstOrDefault(e => e.EventType == nameof(ProjectAddedEvent));
+            return new ProjectAdded(projectAddedDomainEvent.Id, projectAddedDomainEvent.Name, projectAddedDomainEvent.Description, projectAddedDomainEvent.OrganizationId, projectAddedDomainEvent.OwnerId);
+        }
 
 
         #endregion

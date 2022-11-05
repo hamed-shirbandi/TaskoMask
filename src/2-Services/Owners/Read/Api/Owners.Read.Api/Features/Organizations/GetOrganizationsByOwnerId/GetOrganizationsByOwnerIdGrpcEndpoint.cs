bc570a24b41b72cf507aafb6a74895ab9f6f52fd
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-05 16:16:09 +0330

    feat: add a grpc endpoint for GetOrganizationsByOwnerId feature

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Features/Organizations/GetOrganizationsByOwnerId/GetOrganizationsByOwnerIdGrpcEndpoint.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using Grpc.Core;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Protos;
+
+namespace TaskoMask.Services.Owners.Read.Api.Features.Organizations.GetOrganizationsByOwnerId
+{
+    public class GetOrganizationsByOwnerIdGrpcEndpoint : GetOrganizationsByOwnerIdGrpcService.GetOrganizationsByOwnerIdGrpcServiceBase
+    {
+        private readonly IInMemoryBus _inMemoryBus;
+        private readonly IMapper _mapper;
+        public GetOrganizationsByOwnerIdGrpcEndpoint(IInMemoryBus inMemoryBus, IMapper mapper)
+        {
+            _inMemoryBus = inMemoryBus;
+            _mapper = mapper;
+        }
+
+        public override async Task Handle(GetOrganizationsByOwnerIdGrpcRequest request, IServerStreamWriter<GetOrganizationsByOwnerIdGrpcResponse> responseStream, ServerCallContext context)
+        {
+            var organizations = await _inMemoryBus.SendQuery(new GetOrganizationsByOwnerIdRequest(request.OwnerId));
+            foreach (var organization in organizations.Value)
+                await responseStream.WriteAsync(_mapper.Map<GetOrganizationsByOwnerIdGrpcResponse>(organization));
+        }
+
+    }
+
+}

a7d179f334f4966e092040c68b6a61ca95def485
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-06 14:45:00 +0330

    fix: add mapping profiles

---
+++ src/3-ApiGateways/UserPanel/Aggregator/Infrastructure/Mapper/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Organizations;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Projects;
+using TaskoMask.BuildingBlocks.Contracts.Protos;
+
+namespace TaskoMask.ApiGateways.UserPanel.Aggregator.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<GetOrganizationsByOwnerIdGrpcResponse, OrganizationBasicInfoDto>();
+            CreateMap<GetProjectsByOrganizationIdGrpcRequest, ProjectBasicInfoDto>();
+        }
+    }
+}

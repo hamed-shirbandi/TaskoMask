e212d2186e800490d5bde4e48212647ebf5b9671
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-06 18:09:53 +0330

    feat: add another consumer for OrganizationUpdated to update projects

---
+++ src/2-Services/Owners/Read/Api/Owners.Read.Api/Consumers/Projects/OrganizationUpdatedConsumer.cs
@@ -0,0 +1,39 @@
+﻿using MassTransit;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.Services.Owners.Read.Api.Infrastructure.DbContext;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace TaskoMask.Services.Owners.Read.Api.Consumers.Projects
+{
+    /// <summary>
+    /// Attention: OrganizationUpdated has 2 consumer
+    /// This one will update OrganizationName for all projects
+    /// And the other will update the organization itself
+    /// </summary>
+    public class OrganizationUpdatedConsumer : BaseConsumer<OrganizationUpdated>
+    {
+        private readonly OwnerReadDbContext _ownerReadDbContext;
+
+
+        public OrganizationUpdatedConsumer(IInMemoryBus inMemoryBus, OwnerReadDbContext ownerReadDbContext) : base(inMemoryBus)
+        {
+            _ownerReadDbContext = ownerReadDbContext;
+        }
+
+
+        public override async Task ConsumeMessage(ConsumeContext<OrganizationUpdated> context)
+        {
+            var projetcs = await _ownerReadDbContext.Projects.Find(e => e.OrganizationId == context.Message.Id).ToListAsync();
+
+            foreach (var projetc in projetcs)
+            {
+                projetc.OrganizationName = context.Message.Name;
+                projetc.SetAsUpdated();
+                await _ownerReadDbContext.Projects.ReplaceOneAsync(p => p.Id == projetc.Id, projetc, new ReplaceOptions() { IsUpsert = false });
+            }
+        }
+    }
+}

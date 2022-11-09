a180ab536cad1c6d68c19f1e867daf9a68802d7e
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-09 17:42:06 +0330

    test: add integration test for GetProjectsByOrganizationId

---
+++ src/2-Services/Owners/Read/Tests/Owners.Read.IntegrationTests/Features/Projects/GetProjectsByOrganizationIdTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using TaskoMask.Services.Owners.Read.Api.Features.Projects.GetProjectsByOrganizationId;
+using TaskoMask.Services.Owners.Read.IntegrationTests.Fixtures;
+using TaskoMask.Services.Owners.Read.IntegrationTests.TestData;
+using Xunit;
+
+namespace TaskoMask.Services.Owners.Read.IntegrationTests.Features.Project
+{
+    public class GetProjectsByOrganizationIdTests : IClassFixture<ProjectClassFixture>
+    {
+
+        #region Fields
+
+        private readonly ProjectClassFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public GetProjectsByOrganizationIdTests(ProjectClassFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+        [Fact]
+        public async Task Projects_Is_Fetched_By_OrganizationId()
+        {
+            //Arrange
+            var expectedProject = ProjectObjectMother.GetProject();
+            await _fixture.SeedProjectAsync(expectedProject);
+            var getProjectsByOrganizationIdHandler = new GetProjectsByOrganizationIdHandler(_fixture.DbContext, _fixture.Mapper);
+            var request = new GetProjectsByOrganizationIdRequest(expectedProject.OrganizationId);
+
+            //Act
+            var result = await getProjectsByOrganizationIdHandler.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.Should().HaveCount(1);
+            result.FirstOrDefault().Name.Should().Be(expectedProject.Name);
+        }
+
+
+        #endregion
+    }
+}

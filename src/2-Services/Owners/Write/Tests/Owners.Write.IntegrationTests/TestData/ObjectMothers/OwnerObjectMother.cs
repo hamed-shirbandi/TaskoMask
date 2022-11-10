566b2362f0617734409e2fc11a729c42611700a6
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-10 21:53:47 +0330

    test: add an integration test for UpdateOrganization

--- src/2-Services/Owners/Write/Tests/Owners.Write.IntegrationTests/TestData/ObjectMothers/OwnerObjectMother.cs
+++ src/2-Services/Owners/Write/Tests/Owners.Write.IntegrationTests/TestData/ObjectMothers/OwnerObjectMother.cs
@@ -10,13 +10,31 @@ namespace TaskoMask.Services.Owners.Write.IntegrationTests.TestData.ObjectMother
         private const string _email = "Test@TaskoMask.ir";
         private const string _displayName= "Test DisplayName";
 
-        
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+
         public static Owner GetAnOwner(IOwnerValidatorService ownerValidatorService)
         {
             return  OwnerBuilder.Init(ownerValidatorService)
                    .WithEmail(_email)
                    .WithDisplayName(_displayName)
                    .RegisterOwner();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static Owner GetAnOwnerWithAnOrganization(IOwnerValidatorService ownerValidatorService)
+        {
+            var owner = GetAnOwner(ownerValidatorService);
+            var organization = Organization.CreateOrganization("Test Name","Test Description");
+            owner.AddOrganization(organization);
+            return owner;
         }
     }
 }
---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.IntegrationTests/UseCases/Organizations/UpdateOrganizationTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.UpdateOrganization;
+using TaskoMask.Services.Owners.Write.IntegrationTests.Fixtures;
+using TaskoMask.Services.Owners.Write.IntegrationTests.TestData.ObjectMothers;
+using Xunit;
+
+namespace TaskoMask.Services.Owners.Write.IntegrationTests.UseCases.Organizations
+{
+    public class UpdateOrganizationTests : IClassFixture<OrganizationClassFixture>
+    {
+
+        #region Fields
+
+        private readonly OrganizationClassFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public UpdateOrganizationTests(OrganizationClassFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+        [Fact]
+        public async Task Organization_Is_Updated_Properly()
+        {
+            //Arrange
+            var expectedOwner = OwnerObjectMother.GetAnOwnerWithAnOrganization(_fixture.OwnerValidatorService);
+            var expectedOrganization = expectedOwner.Organizations.FirstOrDefault();
+            await _fixture.SeedOwnerAsync(expectedOwner);
+
+            var request = new UpdateOrganizationRequest(id: expectedOrganization.Id, name: "Test New Name", description: "Test New Description");
+            var updateOrganizationUseCase = new UpdateOrganizationUseCase(_fixture.OwnerAggregateRepository, _fixture.MessageBus, _fixture.InMemoryBus);
+
+            //Act
+            var result = await updateOrganizationUseCase.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.EntityId.Should().Be(expectedOrganization.Id);
+            result.Message.Should().Be(ContractsMessages.Update_Success);
+
+            var updatedOwner = await _fixture.GetOwnerAsync(expectedOwner.Id);
+            var updatedOrganization = updatedOwner.GetOrganizationById(expectedOrganization.Id);
+            updatedOrganization.Name.Should().Be(request.Name);
+        }
+
+
+        #endregion
+    }
+}

1a8bd81646dec0af05caaaf27f80fb8d1f9dd96f
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-10 21:33:45 +0330

    faet: add OrganizationClassFixture

---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.IntegrationTests/Fixtures/OrganizationClassFixture.cs
@@ -0,0 +1,17 @@
+﻿
+using TaskoMask.Services.Owners.Write.Domain.Services;
+
+namespace TaskoMask.Services.Owners.Write.IntegrationTests.Fixtures
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OrganizationClassFixture : TestsBaseFixture
+    {
+        public OrganizationClassFixture() : base(dbNameSuffix: nameof(OrganizationClassFixture))
+        {
+        }
+
+    }
+}

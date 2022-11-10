560e016caeef89f06d2afb18b5cc347703753a3b
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-10 20:42:36 +0330

    feat: add OwnerObjectMother to TestData

---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.IntegrationTests/TestData/ObjectMothers/OwnerObjectMother.cs
@@ -0,0 +1,26 @@
+﻿using TaskoMask.Services.Owners.Write.IntegrationTests.TestData.DataBuilders;
+using TaskoMask.Services.Owners.Write.Domain.Entities;
+using TaskoMask.Services.Owners.Write.Domain.Services;
+
+namespace TaskoMask.Services.Owners.Write.IntegrationTests.TestData.ObjectMothers
+{
+    internal static class OwnerObjectMother
+    {
+
+        private const string _email = "Test@TaskoMask.ir";
+        private const string _displayName= "Test DisplayName";
+
+        
+        public static Owner RegisterAnOwner(IOwnerValidatorService ownerValidatorService)
+        {
+            var owner= OwnerBuilder.Init(ownerValidatorService)
+                   .WithEmail(_email)
+                   .WithDisplayName(_displayName)
+                   .RegisterOwner();
+
+            owner.ClearDomainEvents();
+
+            return owner;
+        }
+    }
+}

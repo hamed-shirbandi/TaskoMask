5dda210a0d42b0e004b87ecdd7bb6e16e47a622d
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-12 23:59:41 +0330

    chore: prepare organization usecase test classes

---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/UseCases/Organizations/AddOrganizationTests.cs
@@ -0,0 +1,40 @@
+﻿using TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.AddOrganization;
+using TaskoMask.Services.Owners.Write.UnitTests.Fixtures;
+
+namespace TaskoMask.Services.Owners.Write.UnitTests.UseCases.Organizations
+{
+    public class AddOrganizationTests : TestsBaseFixture
+    {
+
+        #region Fields
+
+        private AddOrganizationUseCase _addOrganizationUseCase;
+
+        #endregion
+
+        #region Ctor
+
+        public AddOrganizationTests( )
+        {
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+
+
+
+        #endregion
+
+        #region Fixture
+
+        protected override void TestClassFixtureSetup()
+        {
+            _addOrganizationUseCase = new AddOrganizationUseCase(OwnerAggregateRepository, MessageBus, InMemoryBus);
+        }
+
+        #endregion
+    }
+}

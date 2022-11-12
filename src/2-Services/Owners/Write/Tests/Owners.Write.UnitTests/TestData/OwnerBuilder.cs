e9001ac05db778c439af36cc257493ef4421e8f8
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-12 23:57:25 +0330

    chore: add test data for owner unit tests

---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/TestData/OwnerBuilder.cs
@@ -0,0 +1,54 @@
+﻿using TaskoMask.Services.Owners.Write.Domain.Entities;
+using TaskoMask.Services.Owners.Write.Domain.Services;
+
+namespace TaskoMask.Services.Owners.Write.UnitTests.TestData
+{
+    internal class OwnerBuilder
+    {
+        public IOwnerValidatorService ValidatorService { get; private set; }
+        public string Email { get; private set; }
+        public string DisplayName { get; private set; }
+
+
+        private OwnerBuilder()
+        {
+        }
+
+
+        public static OwnerBuilder Init()
+        {
+            return new OwnerBuilder();
+        }
+
+
+        public OwnerBuilder WithEmail(IOwnerValidatorService validatorService)
+        {
+            ValidatorService = validatorService;
+            return this;
+        }
+
+
+
+        public OwnerBuilder WithEmail(string email)
+        {
+            Email = email;
+            return this;
+        }
+
+
+        public OwnerBuilder WithDisplayName(string displayName)
+        {
+            DisplayName = displayName;
+            return this;
+        }
+
+
+
+        public Owner RegisterOwner()
+        {
+            return Owner.RegisterOwner(DisplayName, Email, ValidatorService);
+        }
+
+
+    }
+}
---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/TestData/OwnerObjectMother.cs
@@ -0,0 +1,84 @@
+﻿using TaskoMask.Services.Owners.Write.Domain.Entities;
+using TaskoMask.Services.Owners.Write.Domain.Services;
+
+namespace TaskoMask.Services.Owners.Write.UnitTests.TestData
+{
+    internal static class OwnerObjectMother
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static Owner GetAnOwner(IOwnerValidatorService ownerValidatorService)
+        {
+            return Owner.RegisterOwner("Test DisplayName", "Test@TaskoMask.ir", ownerValidatorService);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static Organization GetAnOrganization()
+        {
+            return Organization.CreateOrganization("Test Name", "Test Description");
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static Project GetAProject()
+        {
+            return Project.Create("Test Name", "Test Description");
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static Owner GetAnOwnerWithAnOrganization(IOwnerValidatorService ownerValidatorService)
+        {
+            var owner = GetAnOwner(ownerValidatorService);
+            owner.AddOrganization(GetAnOrganization());
+            return owner;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static Owner GetAnOwnerWithAnOrganizationAndProject(IOwnerValidatorService ownerValidatorService)
+        {
+            var owner = GetAnOwnerWithAnOrganization(ownerValidatorService);
+            var organization = owner.Organizations.FirstOrDefault();
+            owner.AddProject(organization.Id,GetAProject());
+            return owner;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static List<Owner> GenerateOwnerList(IOwnerValidatorService ownerValidatorService,int number = 3)
+        {
+            var list = new List<Owner>();
+
+            for (int i = 1; i <= number; i++)
+            {
+                var owner = Owner.RegisterOwner($"DisplayName_{i}", $"Email_{i}@mail.com", ownerValidatorService);
+                owner.ClearDomainEvents();
+                list.Add(owner);
+            }
+
+            return list;
+        }
+    }
+}

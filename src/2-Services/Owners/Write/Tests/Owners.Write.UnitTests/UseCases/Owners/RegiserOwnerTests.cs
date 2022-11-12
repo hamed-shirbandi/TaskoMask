6b85dc0f698892490c005376d48e0b00a2a38398
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-12 23:59:25 +0330

    chore: prepare owner usecase test classes

---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/UseCases/Owners/RegiserOwnerTests.cs
@@ -0,0 +1,40 @@
+﻿using TaskoMask.Services.Owners.Write.Application.UseCases.Owners.RegiserOwner;
+using TaskoMask.Services.Owners.Write.UnitTests.Fixtures;
+
+namespace TaskoMask.Services.Owners.Write.UnitTests.UseCases.Owners
+{
+    public class RegiserOwnerTests : TestsBaseFixture
+    {
+
+        #region Fields
+
+        private RegiserOwnerUseCase _regiserOwnerUseCase;
+
+        #endregion
+
+        #region Ctor
+
+        public RegiserOwnerTests()
+        {
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+
+
+
+        #endregion
+
+        #region Fixture
+
+        protected override void TestClassFixtureSetup()
+        {
+            _regiserOwnerUseCase = new RegiserOwnerUseCase(OwnerAggregateRepository, OwnerValidatorService, MessageBus, InMemoryBus);
+        }
+
+        #endregion
+    }
+}
---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/UseCases/Owners/UpdateOwnerProfileTests.cs
@@ -0,0 +1,40 @@
+﻿using TaskoMask.Services.Owners.Write.Application.UseCases.Owners.UpdateOwnerProfile;
+using TaskoMask.Services.Owners.Write.UnitTests.Fixtures;
+
+namespace TaskoMask.Services.Owners.Write.UnitTests.UseCases.Owners
+{
+    public class UpdateOwnerProfileTests : TestsBaseFixture
+    {
+
+        #region Fields
+
+        private UpdateOwnerProfileUseCase _updateOwnerProfileUseCase;
+
+        #endregion
+
+        #region Ctor
+
+        public UpdateOwnerProfileTests()
+        {
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+
+
+
+        #endregion
+
+        #region Fixture
+
+        protected override void TestClassFixtureSetup()
+        {
+            _updateOwnerProfileUseCase = new UpdateOwnerProfileUseCase(OwnerAggregateRepository,OwnerValidatorService, MessageBus, InMemoryBus);
+        }
+
+        #endregion
+    }
+}

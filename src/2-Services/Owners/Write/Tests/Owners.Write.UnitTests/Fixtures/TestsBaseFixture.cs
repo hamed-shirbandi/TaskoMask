4724e849eb032ce529a47bce42d3906958780deb
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-12 23:58:34 +0330

    chore: add fixture for owner unit tests

---
+++ src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/Fixtures/TestsBaseFixture.cs
@@ -0,0 +1,78 @@
+﻿using NSubstitute;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Test;
+using TaskoMask.Services.Owners.Write.Domain.Data;
+using TaskoMask.Services.Owners.Write.Domain.Entities;
+using TaskoMask.Services.Owners.Write.Domain.Services;
+using TaskoMask.Services.Owners.Write.UnitTests.TestData;
+
+namespace TaskoMask.Services.Owners.Write.UnitTests.Fixtures
+{
+    public abstract class TestsBaseFixture : UnitTestsBase
+    {
+
+        protected IMessageBus MessageBus;
+        protected IInMemoryBus InMemoryBus;
+        protected IOwnerAggregateRepository OwnerAggregateRepository;
+        protected IOwnerValidatorService OwnerValidatorService;
+        protected List<Owner> Owners;
+
+
+
+        /// <summary>
+        /// Run before each test method
+        /// </summary>
+        public TestsBaseFixture()
+        {
+            FixtureSetup();
+        }
+
+
+
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        protected override void FixtureSetup()
+        {
+            CommonFixtureSetup();
+
+            TestClassFixtureSetup();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CommonFixtureSetup()
+        {
+            MessageBus = Substitute.For<IMessageBus>();
+            InMemoryBus = Substitute.For<IInMemoryBus>();
+            OwnerAggregateRepository = Substitute.For<IOwnerAggregateRepository>();
+            OwnerValidatorService = Substitute.For<IOwnerValidatorService>();
+
+
+            Owners = OwnerObjectMother.GenerateOwnerList(OwnerValidatorService);
+
+            OwnerAggregateRepository.GetByIdAsync(Arg.Is<string>(x => Owners.Any(u => u.Id == x))).Returns(args => Owners.First(u => u.Id == (string)args[0]));
+            OwnerAggregateRepository.CreateAsync(Arg.Any<Owner>()).Returns(async args => Owners.Add((Owner)args[0]));
+            OwnerAggregateRepository.UpdateAsync(Arg.Is<Owner>(x => Owners.Any(u => u.Id == x.Id))).Returns(async args =>
+            {
+                var existUser = Owners.FirstOrDefault(u => u.Id == ((Owner)args[0]).Id);
+                if (existUser != null)
+                {
+                    Owners.Remove(existUser);
+                    Owners.Add(((Owner)args[0]));
+                }
+            });
+        }
+
+
+
+        /// <summary>
+        /// Each test class should setup its own fixture
+        /// </summary>
+        protected abstract void TestClassFixtureSetup();
+
+    }
+}

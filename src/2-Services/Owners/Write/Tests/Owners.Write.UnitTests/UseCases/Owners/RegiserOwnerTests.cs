a07b3ba5a7aa222bb7bcc777c7b93d02a04aa038
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-16 18:59:26 +0330

    test: add a unit test for RegiserOwner
    test if registration is success

--- src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/UseCases/Owners/RegiserOwnerTests.cs
+++ src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/UseCases/Owners/RegiserOwnerTests.cs
@@ -1,5 +1,10 @@
-﻿using TaskoMask.Services.Owners.Write.Application.UseCases.Owners.RegiserOwner;
+﻿using FluentAssertions;
+using NSubstitute;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.Services.Owners.Write.Application.UseCases.Owners.RegiserOwner;
+using TaskoMask.Services.Owners.Write.Domain.Events.Owners;
 using TaskoMask.Services.Owners.Write.UnitTests.Fixtures;
+using Xunit;
 
 namespace TaskoMask.Services.Owners.Write.UnitTests.UseCases.Owners
 {
@@ -21,6 +26,23 @@ namespace TaskoMask.Services.Owners.Write.UnitTests.UseCases.Owners
         #endregion
 
         #region Test Methods
+
+
+        [Fact]
+        public async Task Owner_Is_Registered_Properly()
+        {
+            //Arrange
+            var regiserOwnerRequest = new RegiserOwnerRequest("Test_DisplayName", "Test@email.com", "Test_Password");
+
+            //Act
+            var result = await _regiserOwnerUseCase.Handle(regiserOwnerRequest, CancellationToken.None);
+
+            //Assert
+            var createdUser = Owners.FirstOrDefault(u => u.Id == result.EntityId);
+            createdUser.Email.Value.Should().Be(regiserOwnerRequest.Email);
+            await InMemoryBus.Received(1).PublishEvent(Arg.Any<OwnerRegisteredEvent>());
+            await MessageBus.Received(1).Publish(Arg.Any<OwnerRegistered>());
+        }
 
 
 

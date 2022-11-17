5bd42aca33e83cd31c3b8e1a03e8e2e96f877cc7
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-17 17:25:49 +0330

    test: add a test to verify if AddOrganization works properly

--- src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/UseCases/Organizations/AddOrganizationTests.cs
+++ src/2-Services/Owners/Write/Tests/Owners.Write.UnitTests/UseCases/Organizations/AddOrganizationTests.cs
@@ -1,5 +1,11 @@
-﻿using TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.AddOrganization;
+﻿using FluentAssertions;
+using NSubstitute;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.AddOrganization;
+using TaskoMask.Services.Owners.Write.Domain.Events.Organizations;
 using TaskoMask.Services.Owners.Write.UnitTests.Fixtures;
+using Xunit;
 
 namespace TaskoMask.Services.Owners.Write.UnitTests.UseCases.Organizations
 {
@@ -23,6 +29,23 @@ namespace TaskoMask.Services.Owners.Write.UnitTests.UseCases.Organizations
         #region Test Methods
 
 
+        [Fact]
+        public async Task Organization_Is_Added()
+        {
+            //Arrange
+            var expectedOwner = Owners.FirstOrDefault();
+            var addOrganizationRequest = new AddOrganizationRequest(expectedOwner.Id,"Test_Name", "Test_Description");
+
+            //Act
+            var result = await _addOrganizationUseCase.Handle(addOrganizationRequest, CancellationToken.None);
+
+            //Assert
+            result.Message.Should().Be(ContractsMessages.Create_Success);
+            result.EntityId.Should().NotBeNull();
+            expectedOwner.Organizations.Should().Contain(o => o.Id == result.EntityId);
+            await InMemoryBus.Received(1).PublishEvent(Arg.Any<OrganizationAddedEvent>());
+            await MessageBus.Received(1).Publish(Arg.Any<OrganizationAdded>());
+        }
 
 
 

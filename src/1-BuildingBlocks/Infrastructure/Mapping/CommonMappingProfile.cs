d3e64626954fadd0b9fdc64250847eec0de7bd3e
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-11-26 16:29:42 +0330

    refactor: add MapperExtensions to building blocks

---
+++ src/1-BuildingBlocks/Infrastructure/Mapping/CommonMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Common;
+using TaskoMask.BuildingBlocks.Domain.ValueObjects;
+
+namespace TaskoMask.BuildingBlocks.Infrastructure.Mapping
+{
+    public class CommonMappingProfile : Profile
+    {
+        public CommonMappingProfile()
+        {
+            CreateMap<CreationTime, CreationTimeDto>()
+             .ForMember(dest => dest.CreateDateTimeString, opt =>
+                  opt.MapFrom(src => src.CreateDateTime.ToLongDateString()))
+             .ForMember(dest => dest.ModifiedDateTimeString, opt =>
+                  opt.MapFrom(src => src.ModifiedDateTime.ToLongDateString()));
+        }
+    }
+}
---
+++ src/1-BuildingBlocks/Infrastructure/Mapping/MapperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace TaskoMask.BuildingBlocks.Infrastructure.Mapping
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MapperExtensions
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddMapper(this IServiceCollection services, params Type[] profileAssemblyMarkerTypes)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            //this will find all profiles in this layer
+            services.AddAutoMapper(profileAssemblyMarkerTypes);
+            services.AddAutoMapper(typeof(CommonMappingProfile));
+        }
+
+
+    }
+}

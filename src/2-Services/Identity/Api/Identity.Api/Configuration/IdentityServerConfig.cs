afd4ebb8cb538cf65938c143c669c83f68520555
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-04 14:29:52 +0330

    feat: add owner api resources to identityServer

--- src/2-Services/Identity/Api/Identity.Api/Configuration/IdentityServerConfig.cs
+++ src/2-Services/Identity/Api/Identity.Api/Configuration/IdentityServerConfig.cs
@@ -25,6 +25,8 @@ namespace TaskoMask.Services.Identity.Api.Configuration
         {
            ApiScopesConfig.Monolith_Read,
            ApiScopesConfig.Monolith_Write,
+           ApiScopesConfig.Owner_Read,
+           ApiScopesConfig.Owner_Write,
         };
 
 
@@ -35,6 +37,7 @@ namespace TaskoMask.Services.Identity.Api.Configuration
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
            ApiResourcesConfig.Monolith_Api,
+           ApiResourcesConfig.Owner_Api,
         };
 
 
@@ -53,23 +56,17 @@ namespace TaskoMask.Services.Identity.Api.Configuration
     /// <summary>
     /// 
     /// </summary>
-    static class ApiScopesConfig
-    {
-        public static ApiScope Monolith_Read => new(name: "monolith.read", displayName: "Monolith Read APIs");
-        public static ApiScope Monolith_Write => new(name: "monolith.write", displayName: "Monolith Write APIs");
-
-    }
-
-
-
-    /// <summary>
-    /// 
-    /// </summary>
     static class ApiResourcesConfig
     {
         public static ApiResource Monolith_Api => new("monolith.api", "Monolith Api")
         {
             Scopes = { ApiScopesConfig.Monolith_Read.Name, ApiScopesConfig.Monolith_Write.Name }
+        };
+
+
+        public static ApiResource Owner_Api => new("owner.api", "Owner Api")
+        {
+            Scopes = { ApiScopesConfig.Owner_Read.Name, ApiScopesConfig.Owner_Write.Name }
         };
 
     }
@@ -95,11 +92,28 @@ namespace TaskoMask.Services.Identity.Api.Configuration
                 IdentityServerConstants.StandardScopes.Profile,
                 ApiScopesConfig.Monolith_Read.Name,
                 ApiScopesConfig.Monolith_Write.Name,
+                ApiScopesConfig.Owner_Read.Name,
+                ApiScopesConfig.Owner_Write.Name,
             },
             RedirectUris = { "https://localhost:5011/authentication/login-callback/" },
             PostLogoutRedirectUris = { "https://localhost:5011/authentication/logout-callback/" },
-            ClientUri= "https://localhost:5011"
+            ClientUri = "https://localhost:5011"
         };
-
     }
+
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    static class ApiScopesConfig
+    {
+        public static ApiScope Monolith_Read => new(name: "monolith.read", displayName: "Monolith Read APIs");
+        public static ApiScope Monolith_Write => new(name: "monolith.write", displayName: "Monolith Write APIs");
+
+        public static ApiScope Owner_Read => new(name: "owner.read", displayName: "Owner Read APIs");
+        public static ApiScope Owner_Write => new(name: "owner.write", displayName: "Owner Write APIs");
+    }
+
 }

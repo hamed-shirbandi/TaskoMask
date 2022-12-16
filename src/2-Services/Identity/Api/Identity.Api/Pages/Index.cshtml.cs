d1de9560297d535597ce2f36b294d48cabf73b65
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-16 22:03:45 +0330

    chore: remove unused property and fix namespace

--- src/2-Services/Identity/Api/Identity.Api/Pages/Index.cshtml.cs
+++ src/2-Services/Identity/Api/Identity.Api/Pages/Index.cshtml.cs
@@ -1,17 +1,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Reflection;
 
-namespace TaskoMask.Services.Identity.Api.Pages.Home
+namespace TaskoMask.Services.Identity.Api.Pages
 {
     [AllowAnonymous]
     public class Index : PageModel
     {
-        public string Version;
 
         public void OnGet()
         {
-            Version = typeof(Duende.IdentityServer.Hosting.IdentityServerMiddleware).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Split('+').First();
         }
     }
 }

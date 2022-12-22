0ed5ae2f046daa6af7d908251af4a0570b1bc3e5
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-22 17:08:37 +0330

    feat: add GetCardById feature

---
+++ src/2-Services/Boards/Read/Api/Boards.Read.Api/Features/Cards/GetCardById/GetCardByIdEndpoint.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Cards;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+
+namespace TaskoMask.Services.Boards.Read.Api.Features.Cards.GetCardById
+{
+
+    [Authorize("user-read-access")]
+    [Tags("Cards")]
+    public class GetCardByIdEndpoint : BaseApiController
+    {
+        public GetCardByIdEndpoint(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+
+        /// <summary>
+        /// get card info
+        /// </summary>
+        [HttpGet]
+        [Route("cards/{id}")]
+        public async Task<Result<CardBasicInfoDto>> Get(string id)
+        {
+            return await _inMemoryBus.SendQuery(new GetCardByIdRequest(id));
+        }
+    }
+
+}
---
+++ src/2-Services/Boards/Read/Api/Boards.Read.Api/Features/Cards/GetCardById/GetCardByIdHandler.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using MediatR;
+using MongoDB.Driver;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Exceptions;
+using TaskoMask.BuildingBlocks.Application.Queries;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Cards;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+using TaskoMask.Services.Boards.Read.Api.Infrastructure.DbContext;
+
+namespace TaskoMask.Services.Boards.Read.Api.Features.Cards.GetCardById
+{
+    public class GetCardByIdHandler : BaseQueryHandler, IRequestHandler<GetCardByIdRequest, CardBasicInfoDto>
+    {
+        #region Fields
+
+        private readonly BoardReadDbContext _boardReadDbContext;
+
+        #endregion
+
+        #region Ctors
+
+        public GetCardByIdHandler(BoardReadDbContext boardReadDbContext, IMapper mapper) : base(mapper)
+        {
+            _boardReadDbContext = boardReadDbContext;
+        }
+
+        #endregion
+
+        #region Handlers
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<CardBasicInfoDto> Handle(GetCardByIdRequest request, CancellationToken cancellationToken)
+        {
+            var card = await _boardReadDbContext.Cards.Find(e => e.Id == request.Id).FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+            if (card == null)
+                throw new ApplicationException(ContractsMessages.Data_Not_exist, DomainMetadata.Card);
+
+            return _mapper.Map<CardBasicInfoDto>(card);
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+
+        #endregion
+    }
+}

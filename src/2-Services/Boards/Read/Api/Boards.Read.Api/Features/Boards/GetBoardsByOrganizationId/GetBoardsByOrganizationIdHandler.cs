6efb10b35c7d198bf905555809a03421b780e922
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-23 20:46:30 +0330

    feat: add GetBoardsByOrganizationId feature

---
+++ src/2-Services/Boards/Read/Api/Boards.Read.Api/Features/Boards/GetBoardsByOrganizationId/GetBoardsByOrganizationIdHandler.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using MediatR;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Queries;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Boards;
+using TaskoMask.Services.Boards.Read.Api.Infrastructure.DbContext;
+
+namespace TaskoMask.Services.Boards.Read.Api.Features.Boards.GetBoardsByOrganizationId
+{
+    public class GetBoardsByOrganizationIdHandler : BaseQueryHandler, IRequestHandler<GetBoardsByOrganizationIdRequest, IEnumerable<GetBoardDto>>
+    {
+        #region Fields
+
+        private readonly BoardReadDbContext _boardReadDbContext;
+
+        #endregion
+
+        #region Ctors
+
+        public GetBoardsByOrganizationIdHandler(BoardReadDbContext boardReadDbContext, IMapper mapper) : base(mapper)
+        {
+            _boardReadDbContext = boardReadDbContext;
+        }
+
+        #endregion
+
+        #region Handlers
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<IEnumerable<GetBoardDto>> Handle(GetBoardsByOrganizationIdRequest request, CancellationToken cancellationToken)
+        {
+            var boards = await _boardReadDbContext.Boards.AsQueryable().Where(e => e.OrganizationId == request.OrganizationId).ToListAsync(cancellationToken);
+
+            return _mapper.Map<IEnumerable<GetBoardDto>>(boards);
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+
+        #endregion
+    }
+}

592222ff05a27295e4072641bd50f9abe4350ea8
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-26 22:01:46 +0330

    feat: add DeleteCard use case

---
+++ src/2-Services/Boards/Write/Libraries/Application/UseCases/Cards/DeleteCard/DeleteCardRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskoMask.BuildingBlocks.Application.Commands;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+
+namespace TaskoMask.Services.Boards.Write.Application.UseCases.Cards.DeleteCard
+{
+    public class DeleteCardRequest: BaseCommand
+    {
+        public DeleteCardRequest(string id)
+        {
+            Id = id;
+        }
+
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string Id { get; }
+
+    }
+}
---
+++ src/2-Services/Boards/Write/Libraries/Application/UseCases/Cards/DeleteCard/DeleteCardValidation.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace TaskoMask.Services.Boards.Write.Application.UseCases.Cards.DeleteCard
+{
+    public abstract class DeleteCardValidation<TRequest> : AbstractValidator<TRequest> where TRequest : DeleteCardRequest
+    {
+        public DeleteCardValidation()
+        {
+        }
+
+    }
+}

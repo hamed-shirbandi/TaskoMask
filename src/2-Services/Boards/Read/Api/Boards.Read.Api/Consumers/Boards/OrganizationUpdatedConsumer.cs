58f5e41c38dc452873212cc5c2d3b98850b4a666
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-26 22:52:11 +0330

    feat: add a consumer for OrganizationUpdated integration event

---
+++ src/2-Services/Boards/Read/Api/Boards.Read.Api/Consumers/Boards/OrganizationUpdatedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using TaskoMask.Services.Boards.Read.Api.Infrastructure.DbContext;
+
+namespace TaskoMask.Services.Boards.Read.Api.Consumers.Boards
+{
+    public class OrganizationUpdatedConsumer : BaseConsumer<OrganizationUpdated>
+    {
+        private readonly BoardReadDbContext _boardReadDbContext;
+
+
+        public OrganizationUpdatedConsumer(IInMemoryBus inMemoryBus, BoardReadDbContext boardReadDbContext) : base(inMemoryBus)
+        {
+            _boardReadDbContext = boardReadDbContext;
+        }
+
+
+        public override async Task ConsumeMessage(ConsumeContext<OrganizationUpdated> context)
+        {
+            var boards = await _boardReadDbContext.Boards.Find(e => e.ProjectId == context.Message.Id).ToListAsync();
+
+            foreach (var board in boards)
+            {
+                board.OrganizationName = context.Message.Name;
+                board.SetAsUpdated();
+                await _boardReadDbContext.Boards.ReplaceOneAsync(p => p.Id == board.Id, board, new ReplaceOptions() { IsUpsert = false });
+            }
+        }
+    }
+}

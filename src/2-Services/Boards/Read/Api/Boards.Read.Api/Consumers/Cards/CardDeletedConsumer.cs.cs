844b0bed648f2ff253877e4e81dd46486af8489b
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-26 22:53:14 +0330

    feat: add a consumer for CardDeleted integration event

---
+++ src/2-Services/Boards/Read/Api/Boards.Read.Api/Consumers/Cards/CardDeletedConsumer.cs.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.Services.Boards.Read.Api.Infrastructure.DbContext;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace TaskoMask.Services.Boards.Read.Api.Consumers.Cards
+{
+    public class CardDeletedConsumer : BaseConsumer<CardDeleted>
+    {
+        private readonly BoardReadDbContext _boardReadDbContext;
+
+
+        public CardDeletedConsumer(IInMemoryBus inMemoryBus, BoardReadDbContext boardReadDbContext) : base(inMemoryBus)
+        {
+            _boardReadDbContext = boardReadDbContext;
+        }
+
+
+        public override async Task ConsumeMessage(ConsumeContext<CardDeleted> context)
+        {
+            await _boardReadDbContext.Cards.DeleteOneAsync(p => p.Id == context.Message.Id);
+        }
+    }
+}

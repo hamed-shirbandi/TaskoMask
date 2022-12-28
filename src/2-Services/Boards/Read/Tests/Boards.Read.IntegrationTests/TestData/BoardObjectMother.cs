0e9c1b788bce517baf0907032414edf515e6104c
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-28 17:15:14 +0330

    feat: add an object mother for board

---
+++ src/2-Services/Boards/Read/Tests/Boards.Read.IntegrationTests/TestData/BoardObjectMother.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using TaskoMask.Services.Boards.Read.Api.Domain;
+
+namespace TaskoMask.Services.Boards.Read.IntegrationTests.TestData
+{
+    internal static class BoardObjectMother
+    {
+        public static Board GetBoard()
+        {
+            return new Board(ObjectId.GenerateNewId().ToString())
+            {
+                Name = "Test Name",
+                Description = "Test Description",
+                OwnerId = ObjectId.GenerateNewId().ToString(),
+                OrganizationId = ObjectId.GenerateNewId().ToString(),
+                ProjectId = ObjectId.GenerateNewId().ToString(),
+                OrganizationName = "Test Organization Name",
+                ProjectName = "Test Project Name",
+            };
+        }
+
+    }
+}

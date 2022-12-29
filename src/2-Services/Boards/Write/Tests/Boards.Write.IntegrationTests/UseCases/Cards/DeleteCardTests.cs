9cac39d2055edcbac49bb355677a018ff99b48be
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-29 13:36:40 +0330

    feat: add an integration test for DeleteCard use case

---
+++ src/2-Services/Boards/Write/Tests/Boards.Write.IntegrationTests/UseCases/Cards/DeleteCardTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+using TaskoMask.Services.Boards.Write.Application.UseCases.Cards.DeleteCard;
+using TaskoMask.Services.Boards.Write.IntegrationTests.Fixtures;
+using TaskoMask.Services.Boards.Write.IntegrationTests.TestData;
+using Xunit;
+
+namespace TaskoMask.Services.Boards.Write.IntegrationTests.UseCases.Cards
+{
+    [Collection(nameof(CardCollectionFixture))]
+    public class DeleteCardTests
+    {
+
+        #region Fields
+
+        private readonly CardCollectionFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public DeleteCardTests(CardCollectionFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+        [Fact]
+        public async Task Card_Is_Deleted()
+        {
+            //Arrange
+            var expectedMessage = string.Format(ContractsMessages.Not_Found, DomainMetadata.Card);
+            var expectedBoard = BoardObjectMother.GetABoardWithACard(_fixture.BoardValidatorService);
+            await _fixture.SeedBoardAsync(expectedBoard);
+
+            var expectedCard = expectedBoard.Cards.FirstOrDefault();
+
+            var request = new DeleteCardRequest(expectedCard.Id);
+            var deleteCardUseCase = new DeleteCardUseCase(_fixture.BoardAggregateRepository, _fixture.MessageBus, _fixture.InMemoryBus);
+
+            //Act
+            var result = await deleteCardUseCase.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.EntityId.Should().Be(expectedCard.Id);
+            result.Message.Should().Be(ContractsMessages.Update_Success);
+
+            var updatedBoard = await _fixture.GetBoardAsync(expectedBoard.Id);
+            var deletedCard = updatedBoard.Cards.FirstOrDefault(c => c.Id == result.EntityId);
+            deletedCard.Should().BeNull();
+        }
+
+
+        #endregion
+    }
+}

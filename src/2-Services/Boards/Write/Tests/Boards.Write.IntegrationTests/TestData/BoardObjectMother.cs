5de34d2f03cede2dce9362e4901d797e38947265
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-29 13:35:06 +0330

    feat: add an object mother for board

---
+++ src/2-Services/Boards/Write/Tests/Boards.Write.IntegrationTests/TestData/BoardObjectMother.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Bson;
+using TaskoMask.BuildingBlocks.Contracts.Enums;
+using TaskoMask.Services.Boards.Write.Domain.Entities;
+using TaskoMask.Services.Boards.Write.Domain.Services;
+
+namespace TaskoMask.Services.Boards.Write.IntegrationTests.TestData
+{
+    internal static class BoardObjectMother
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static Board GetABoard(IBoardValidatorService boardValidatorService)
+        {
+            return Board.AddBoard(name: GetRandomString(10),description: GetRandomString(30),projectId: ObjectId.GenerateNewId().ToString(), boardValidatorService);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static Card GetACard()
+        {
+            return Card.Create(name: GetRandomString(10), type: BoardCardType.ToDo);
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static Board GetABoardWithACard(IBoardValidatorService boardValidatorService)
+        {
+            var board = GetABoard(boardValidatorService);
+            board.AddCard(GetACard());
+            return board;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static string GetRandomString(int length)
+        {
+            return Guid.NewGuid().ToString().Substring(length);
+        }
+
+    }
+}

084d2a9edc780f05c8c65c3f9f5b769d8161b1fd
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-30 15:37:46 +0330

    chore: add new dependencies

--- src/2-Services/Boards/Write/Libraries/Infrastructure/CrossCutting/DI/InfrastructureModule.cs
+++ src/2-Services/Boards/Write/Libraries/Infrastructure/CrossCutting/DI/InfrastructureModule.cs
@@ -3,7 +3,11 @@ using Microsoft.Extensions.DependencyInjection;
 using TaskoMask.BuildingBlocks.Infrastructure.Extensions;
 using TaskoMask.BuildingBlocks.Infrastructure.MongoDB;
 using TaskoMask.Services.Boards.Write.Application.Resources;
+using TaskoMask.Services.Boards.Write.Domain.Data;
+using TaskoMask.Services.Boards.Write.Domain.Services;
 using TaskoMask.Services.Boards.Write.Infrastructure.Data.DbContext;
+using TaskoMask.Services.Boards.Write.Infrastructure.Data.Repositories;
+using TaskoMask.Services.Boards.Write.Infrastructure.Data.Services;
 
 namespace TaskoMask.Services.Boards.Write.Infrastructure.CrossCutting.DI
 {
@@ -23,6 +27,10 @@ namespace TaskoMask.Services.Boards.Write.Infrastructure.CrossCutting.DI
             services.AddBuildingBlocksInfrastructure(configuration, consumerAssemblyMarkerType,handlerAssemblyMarkerType: typeof(ApplicationMessages));
 
             services.AddMongoDbContext(configuration);
+
+            services.AddDomainServices();
+
+            services.AddRepositories();
         }
 
 
@@ -36,5 +44,24 @@ namespace TaskoMask.Services.Boards.Write.Infrastructure.CrossCutting.DI
             services.AddScoped<BoardWriteDbContext>().AddOptions<MongoDbOptions>().Bind(options);
         }
 
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void AddDomainServices(this IServiceCollection services)
+        {
+            services.AddScoped<IBoardValidatorService, BoardValidatorService>();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IBoardAggregateRepository, BoardAggregateRepository>();
+        }
     }
 }

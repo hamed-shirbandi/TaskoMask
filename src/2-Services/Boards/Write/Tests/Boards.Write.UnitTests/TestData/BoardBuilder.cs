6286418f08657e36a2e38b75aa4d46c08d8b5b96
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-30 15:38:35 +0330

    feat: add test data builders for board

---
+++ src/2-Services/Boards/Write/Tests/Boards.Write.UnitTests/TestData/BoardBuilder.cs
@@ -0,0 +1,69 @@
+﻿using TaskoMask.Services.Boards.Write.Domain.Entities;
+using TaskoMask.Services.Boards.Write.Domain.Services;
+
+namespace TaskoMask.Services.Boards.Write.UnitTests.TestData
+{
+    internal class BoardBuilder
+    {
+        public IBoardValidatorService ValidatorService { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string ProjectId { get; private set; }
+
+
+
+        private BoardBuilder()
+        {
+        }
+
+
+
+        public static BoardBuilder Init()
+        {
+            return new BoardBuilder();
+        }
+
+
+
+        public BoardBuilder WithValidatorService(IBoardValidatorService validatorService)
+        {
+            ValidatorService = validatorService;
+            return this;
+        }
+
+
+
+        public BoardBuilder WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+
+
+        public BoardBuilder WithDisplayName(string description)
+        {
+            Description = description;
+            return this;
+        }
+
+
+
+        public BoardBuilder WithProjectIde(string projectId)
+        {
+            ProjectId = projectId;
+            return this;
+        }
+
+
+
+        public Board AddBoard()
+        {
+            var board = Board.AddBoard(Name, Description, ProjectId, ValidatorService);
+            board.ClearDomainEvents();
+            return board;
+        }
+
+
+    }
+}
---
+++ src/2-Services/Boards/Write/Tests/Boards.Write.UnitTests/TestData/BoardObjectMother.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson;
+using TaskoMask.BuildingBlocks.Contracts.Enums;
+using TaskoMask.BuildingBlocks.Test.TestData;
+using TaskoMask.Services.Boards.Write.Domain.Entities;
+using TaskoMask.Services.Boards.Write.Domain.Services;
+
+namespace TaskoMask.Services.Boards.Write.UnitTests.TestData
+{
+    internal static class BoardObjectMother
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static Board GetABoard(IBoardValidatorService boardValidatorService)
+        {
+            var board= Board.AddBoard(name: TestDataGenerator.GetRandomName(10), description: TestDataGenerator.GetRandomString(20), projectId: ObjectId.GenerateNewId().ToString(), boardValidatorService);
+            board.ClearDomainEvents();
+            return board;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static Card GetACard()
+        {
+            return Card.Create(name: TestDataGenerator.GetRandomName(10), type: BoardCardType.ToDo);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static List<Board> GenerateBoardsList(IBoardValidatorService boardValidatorService,int number = 3)
+        {
+            var list = new List<Board>();
+            var projectId = ObjectId.GenerateNewId().ToString();
+            for (int i = 1; i <= number; i++)
+            {
+                var board = Board.AddBoard($"Test Name {i}", $"Test Description {i}", projectId, boardValidatorService);
+                board.ClearDomainEvents();
+                list.Add(board);
+            }
+
+            return list;
+        }
+    }
+}

f8059dc2bad63da760918f8fcba96bc32e6e5b0c
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-12-30 15:37:17 +0330

    feat: implement BoardValidatorService

---
+++ src/2-Services/Boards/Write/Libraries/Infrastructure/Data/Services/BoardValidatorService.cs
@@ -0,0 +1,29 @@
+﻿
+using TaskoMask.Services.Boards.Write.Domain.Data;
+using TaskoMask.Services.Boards.Write.Domain.Services;
+
+namespace TaskoMask.Services.Boards.Write.Infrastructure.Data.Services
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BoardValidatorService : IBoardValidatorService
+    {
+        private readonly IBoardAggregateRepository _boardRepository;
+
+        public BoardValidatorService(IBoardAggregateRepository boardRepository)
+        {
+            _boardRepository = boardRepository;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool BoardHasUniqueName(string boardId, string projectId, string boardName)
+        {
+            return !_boardRepository.ExistBoard(boardId, projectId, boardName);
+        }
+    }
+}

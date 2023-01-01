301f37547801561d9ff7d2ab43bffcfdd9237cda
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-01 16:19:59 +0330

    test: add test  for updating a borad

--- src/2-Services/Boards/Write/Tests/Boards.Write.UnitTests/UseCases/Boards/UpdateBoardTests.cs
+++ src/2-Services/Boards/Write/Tests/Boards.Write.UnitTests/UseCases/Boards/UpdateBoardTests.cs
@@ -1,5 +1,13 @@
-﻿using TaskoMask.Services.Boards.Write.Application.UseCases.Boards.UpdateBoard;
+﻿using FluentAssertions;
+using MongoDB.Bson;
+using NSubstitute;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+using TaskoMask.Services.Boards.Write.Application.UseCases.Boards.UpdateBoard;
+using TaskoMask.Services.Boards.Write.Domain.Events.Boards;
 using TaskoMask.Services.Boards.Write.UnitTests.Fixtures;
+using Xunit;
 
 namespace TaskoMask.Services.Boards.Write.UnitTests.UseCases.Boards
 {
@@ -22,6 +30,26 @@ namespace TaskoMask.Services.Boards.Write.UnitTests.UseCases.Boards
 
         #region Test Methods
 
+
+        [Fact]
+        public async Task Board_Is_Updated()
+        {
+            //Arrange
+            var expectedBoard = Boards.FirstOrDefault();
+            var updateBoardRequest = new UpdateBoardRequest(expectedBoard.Id, "Test_New_Name", "Test_New_Description");
+
+            //Act
+            var result = await _updateBoardUseCase.Handle(updateBoardRequest, CancellationToken.None);
+
+            //Assert
+            result.Message.Should().Be(ContractsMessages.Update_Success);
+            result.EntityId.Should().Be(expectedBoard.Id);
+
+            expectedBoard.Name.Value.Should().Be(updateBoardRequest.Name);
+
+            await InMemoryBus.Received(1).PublishEvent(Arg.Any<BoardUpdatedEvent>());
+            await MessageBus.Received(1).Publish(Arg.Any<BoardUpdated>());
+        }
 
 
 

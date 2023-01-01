764264d7479b70375681c03819a96352a978e120
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-01 16:17:46 +0330

    test: add test for deleting a board

--- src/2-Services/Boards/Write/Tests/Boards.Write.UnitTests/UseCases/Boards/DeleteBoardTests.cs
+++ src/2-Services/Boards/Write/Tests/Boards.Write.UnitTests/UseCases/Boards/DeleteBoardTests.cs
@@ -1,5 +1,13 @@
-﻿using TaskoMask.Services.Boards.Write.Application.UseCases.Boards.DeleteBoard;
+﻿using FluentAssertions;
+using MongoDB.Bson;
+using NSubstitute;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+using TaskoMask.Services.Boards.Write.Application.UseCases.Boards.DeleteBoard;
+using TaskoMask.Services.Boards.Write.Domain.Events.Boards;
 using TaskoMask.Services.Boards.Write.UnitTests.Fixtures;
+using Xunit;
 
 namespace TaskoMask.Services.Boards.Write.UnitTests.UseCases.Boards
 {
@@ -23,6 +31,26 @@ namespace TaskoMask.Services.Boards.Write.UnitTests.UseCases.Boards
         #region Test Methods
 
 
+        [Fact]
+        public async Task Board_Is_Deleted()
+        {
+            //Arrange
+            var expectedBoard = Boards.FirstOrDefault();
+            var deleteBoardRequest = new DeleteBoardRequest(expectedBoard.Id);
+
+            //Act
+            var result = await _deleteBoardUseCase.Handle(deleteBoardRequest, CancellationToken.None);
+
+            //Assert
+            result.EntityId.Should().NotBeNull();
+            result.Message.Should().Be(ContractsMessages.Update_Success);
+
+            var deleteedBoard = Boards.FirstOrDefault(u => u.Id == result.EntityId);
+            deleteedBoard.Should().BeNull();
+
+            await InMemoryBus.Received(1).PublishEvent(Arg.Any<BoardDeletedEvent>());
+            await MessageBus.Received(1).Publish(Arg.Any<BoardDeleted>());
+        }
 
 
 

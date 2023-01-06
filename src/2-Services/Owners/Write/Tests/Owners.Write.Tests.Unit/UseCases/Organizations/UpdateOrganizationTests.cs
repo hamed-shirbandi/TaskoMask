bfa4209e704a9e7fcef3dbde5ec85de06e55815c
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-06 16:43:40 +0330

    test: add a unit test for update organization use case

--- src/2-Services/Owners/Write/Tests/Owners.Write.Tests.Unit/UseCases/Organizations/UpdateOrganizationTests.cs
+++ src/2-Services/Owners/Write/Tests/Owners.Write.Tests.Unit/UseCases/Organizations/UpdateOrganizationTests.cs
@@ -1,5 +1,15 @@
-﻿using TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.UpdateOrganization;
+﻿using NSubstitute;
+using System.Threading;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.Services.Owners.Write.Application.UseCases.Organizations.UpdateOrganization;
 using TaskoMask.Services.Owners.Write.Tests.Unit.Fixtures;
+using Xunit;
+using System.Threading.Tasks;
+using System.Linq;
+using TaskoMask.Services.Owners.Write.Tests.Base.TestData;
+using FluentAssertions;
+using TaskoMask.Services.Owners.Write.Domain.Events.Organizations;
 
 namespace TaskoMask.Services.Owners.Write.Tests.Unit.UseCases.Organizations
 {
@@ -24,7 +34,29 @@ namespace TaskoMask.Services.Owners.Write.Tests.Unit.UseCases.Organizations
 
 
 
+        [Fact]
+        public async Task Organization_is_updated()
+        {
+            //Arrange
+            var expectedOwner = Owners.FirstOrDefault();
+            var expectedOrganization = OwnerObjectMother.CreateOrganization();
+            expectedOwner.AddOrganization(expectedOrganization);
+            var updateOrganizationRequest = new UpdateOrganizationRequest(expectedOrganization.Id, "Test New Name", "Test New Description");
 
+            //Act
+            var result = await _updateOrganizationUseCase.Handle(updateOrganizationRequest, CancellationToken.None);
+
+            //Assert
+            result.Message.Should().Be(ContractsMessages.Update_Success);
+            result.EntityId.Should().Be(expectedOrganization.Id);
+
+            var updatedOrganization = expectedOwner.GetOrganizationById(expectedOrganization.Id);
+            updatedOrganization.Name.Value.Should().Be(updateOrganizationRequest.Name);
+            updatedOrganization.Description.Value.Should().Be(updateOrganizationRequest.Description);
+
+            await InMemoryBus.Received(1).PublishEvent(Arg.Any<OrganizationUpdatedEvent>());
+            await MessageBus.Received(1).Publish(Arg.Any<OrganizationUpdated>());
+        }
 
         #endregion
 

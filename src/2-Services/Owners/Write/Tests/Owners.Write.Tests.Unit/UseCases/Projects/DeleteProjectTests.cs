8fa5d38702809817af90a7af61d6447048cbfca7
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-09 22:14:09 +0330

    test: add a test for DeleteProject use case

--- src/2-Services/Owners/Write/Tests/Owners.Write.Tests.Unit/UseCases/Projects/DeleteProjectTests.cs
+++ src/2-Services/Owners/Write/Tests/Owners.Write.Tests.Unit/UseCases/Projects/DeleteProjectTests.cs
@@ -1,5 +1,20 @@
-﻿using TaskoMask.Services.Owners.Write.Application.UseCases.Projects.DeleteProject;
+﻿using MongoDB.Bson;
+using NSubstitute;
+using System.Linq;
+using System.Threading;
+using System;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+using TaskoMask.BuildingBlocks.Domain.Exceptions;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+using TaskoMask.Services.Owners.Write.Application.UseCases.Projects.DeleteProject;
+using TaskoMask.Services.Owners.Write.Application.UseCases.Projects.DeleteProject;
+using TaskoMask.Services.Owners.Write.Domain.Events.Projects;
+using TaskoMask.Services.Owners.Write.Tests.Base.TestData;
 using TaskoMask.Services.Owners.Write.Tests.Unit.Fixtures;
+using Xunit;
+using FluentAssertions;
+using System.Threading.Tasks;
 
 namespace TaskoMask.Services.Owners.Write.Tests.Unit.UseCases.Projects
 {
@@ -23,6 +38,35 @@ namespace TaskoMask.Services.Owners.Write.Tests.Unit.UseCases.Projects
         #region Test Methods
 
 
+        [Fact]
+        public async Task Project_is_deleted()
+        {
+            //Arrange
+            var expectedOwner = Owners.FirstOrDefault();
+            var expectedOrganization = OwnerObjectMother.CreateOrganization();
+            var expectedProject = OwnerObjectMother.CreateProject();
+            expectedOwner.AddOrganization(expectedOrganization);
+            expectedOwner.AddProject(expectedOrganization.Id, expectedProject);
+
+            var deleteProjectRequest = new DeleteProjectRequest(expectedProject.Id);
+            var expectedMessage = string.Format(ContractsMessages.Not_Found, DomainMetadata.Project);
+
+            //Act
+            var result = await _deleteProjectUseCase.Handle(deleteProjectRequest, CancellationToken.None);
+
+            //Assert
+            result.Message.Should().Be(ContractsMessages.Update_Success);
+            result.EntityId.Should().Be(expectedProject.Id);
+
+            //Act
+            Action act = () => expectedOwner.GetProjectById(expectedProject.Id);
+
+            //Assert
+            act.Should().Throw<DomainException>().Where(e => e.Message.Equals(expectedMessage));
+
+            await InMemoryBus.Received(1).PublishEvent(Arg.Any<ProjectDeletedEvent>());
+            await MessageBus.Received(1).Publish(Arg.Any<ProjectDeleted>());
+        }
 
 
 

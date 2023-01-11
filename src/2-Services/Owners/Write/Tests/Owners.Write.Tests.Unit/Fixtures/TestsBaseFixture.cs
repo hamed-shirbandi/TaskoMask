7ddbc8c0381f00616086b34fa21b4ffef0ee284c
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-11 15:13:10 +0330

    fix: mock other get methods for OwnerAggregateRepository

--- src/2-Services/Owners/Write/Tests/Owners.Write.Tests.Unit/Fixtures/TestsBaseFixture.cs
+++ src/2-Services/Owners/Write/Tests/Owners.Write.Tests.Unit/Fixtures/TestsBaseFixture.cs
@@ -66,6 +66,14 @@ namespace TaskoMask.Services.Owners.Write.Tests.Unit.Fixtures
 
                 return Task.CompletedTask;
             });
+            OwnerAggregateRepository.GetByOrganizationIdAsync(Arg.Is<string>(x => x.Any())).Returns(args =>
+            {
+                return Owners.FirstOrDefault(u => u.Organizations.Any(c => c.Id == (string)args[0]));
+            });
+            OwnerAggregateRepository.GetByProjectIdAsync(Arg.Is<string>(x => x.Any())).Returns(args =>
+            {
+                return Owners.FirstOrDefault(u => u.Organizations.Any(c => c.Projects.Any(p => p.Id == (string)args[0])));
+            });
 
         }
 

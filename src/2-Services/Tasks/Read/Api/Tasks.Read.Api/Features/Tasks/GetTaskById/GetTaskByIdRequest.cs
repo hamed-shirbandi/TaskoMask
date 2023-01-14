8f38fa02f61b0e8f5bf038a060aad7b92474391d
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-14 18:45:08 +0330

    feat: add GetTaskById feature

---
+++ src/2-Services/Tasks/Read/Api/Tasks.Read.Api/Features/Tasks/GetTaskById/GetTaskByIdRequest.cs
@@ -0,0 +1,18 @@
+﻿using TaskoMask.BuildingBlocks.Application.Queries;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Tasks;
+
+namespace TaskoMask.Services.Tasks.Read.Api.Features.Tasks.GetTaskById
+{
+    public class GetTaskByIdRequest : BaseQuery<GetTaskDto>
+    {
+        public GetTaskByIdRequest(string id)
+        {
+            Id = id;
+        }
+
+
+        public string Id { get; }
+
+
+    }
+}

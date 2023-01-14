31e16ffc21df2cd189af77ca84d0ef1e24548e32
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-14 19:01:56 +0330

    feat: add GetCommentsByTaskId feature

---
+++ src/2-Services/Tasks/Read/Api/Tasks.Read.Api/Features/Comments/GetCommentsByTaskId/GetCommentsByTaskIdHandler.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using MediatR;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Queries;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Comments;
+using TaskoMask.Services.Tasks.Read.Api.Infrastructure.DbContext;
+
+namespace TaskoMask.Services.Tasks.Read.Api.Features.Comments.GetCommentsByTaskId
+{
+    public class GetCommentsByTaskIdHandler : BaseQueryHandler, IRequestHandler<GetCommentsByTaskIdRequest, IEnumerable<GetCommentDto>>
+    {
+        #region Fields
+
+        private readonly TaskReadDbContext _taskReadDbContext;
+
+        #endregion
+
+        #region Ctors
+
+        public GetCommentsByTaskIdHandler(TaskReadDbContext taskReadDbContext, IMapper mapper) : base(mapper)
+        {
+            _taskReadDbContext = taskReadDbContext;
+        }
+
+        #endregion
+
+        #region Handlers
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<IEnumerable<GetCommentDto>> Handle(GetCommentsByTaskIdRequest request, CancellationToken cancellationToken)
+        {
+            var comments = await _taskReadDbContext.Comments.AsQueryable().Where(e => e.TaskId == request.TaskId).ToListAsync(cancellationToken);
+
+            return _mapper.Map<IEnumerable<GetCommentDto>>(comments);
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+
+        #endregion
+    }
+}
---
+++ src/2-Services/Tasks/Read/Api/Tasks.Read.Api/Features/Comments/GetCommentsByTaskId/GetCommentsByTaskIdRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TaskoMask.BuildingBlocks.Application.Queries;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Comments;
+
+namespace TaskoMask.Services.Tasks.Read.Api.Features.Comments.GetCommentsByTaskId
+{
+    public class GetCommentsByTaskIdRequest : BaseQuery<IEnumerable<GetCommentDto>>
+    {
+        public GetCommentsByTaskIdRequest(string taskId)
+        {
+            TaskId = taskId;
+        }
+
+        public string TaskId { get; }
+    }
+}

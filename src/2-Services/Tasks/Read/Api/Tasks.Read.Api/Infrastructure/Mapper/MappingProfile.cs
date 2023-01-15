fa3cd038501c70ef611ee23fe341fc8654762fb9
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-15 15:57:29 +0330

    feat: add grpc endpoint for GetCommentsByTaskId feature

--- src/2-Services/Tasks/Read/Api/Tasks.Read.Api/Infrastructure/Mapper/MappingProfile.cs
+++ src/2-Services/Tasks/Read/Api/Tasks.Read.Api/Infrastructure/Mapper/MappingProfile.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
 using TaskoMask.BuildingBlocks.Contracts.Dtos.Activities;
 using TaskoMask.BuildingBlocks.Contracts.Dtos.Comments;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Common;
 using TaskoMask.BuildingBlocks.Contracts.Dtos.Tasks;
+using TaskoMask.BuildingBlocks.Contracts.Protos;
 using TaskoMask.Services.Tasks.Read.Api.Domain;
 
 namespace TaskoMask.Services.Tasks.Read.Api.Infrastructure.Mapper
@@ -10,10 +13,21 @@ namespace TaskoMask.Services.Tasks.Read.Api.Infrastructure.Mapper
     {
         public MappingProfile()
         {
+            CreateMap<CreationTimeDto, CreationTimeGrpcResponse>()
+                .ForMember(dest => dest.CreateDateTime, opt =>
+                    opt.MapFrom(src => src.CreateDateTime.ToTimestamp()))
+                .ForMember(dest => dest.ModifiedDateTime, opt =>
+                    opt.MapFrom(src => src.ModifiedDateTime.ToTimestamp()));
 
             CreateMap<Task, GetTaskDto>();
 
+            CreateMap<GetTaskDto, GetTaskGrpcResponse>()
+            .ForMember(dest => dest.Description, opt =>
+                  opt.MapFrom(src => src.Description ?? string.Empty));
+
             CreateMap<Comment, GetCommentDto>();
+
+            CreateMap<GetCommentDto, GetCommentGrpcResponse>();
 
             CreateMap<Activity, GetActivityDto>();
             

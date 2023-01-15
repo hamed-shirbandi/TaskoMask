50c42f32da70393c32d4310a35d27d4e2cbf4095
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-15 17:54:28 +0330

    feat: add GetTaskById feature to aggregator

---
+++ src/3-ApiGateways/UserPanel/Aggregator/Features/GetTaskById/GetTaskByIdRequest.cs
@@ -0,0 +1,16 @@
+﻿using TaskoMask.BuildingBlocks.Application.Queries;
+using TaskoMask.BuildingBlocks.Contracts.ViewModels;
+
+namespace TaskoMask.ApiGateways.UserPanel.Aggregator.Features.GetTaskById
+{
+    public class GetTaskByIdRequest : BaseQuery<TaskDetailsViewModel>
+    {
+        public GetTaskByIdRequest(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; }
+
+    }
+}

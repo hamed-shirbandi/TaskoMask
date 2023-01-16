22d111d847604bfaf8bbd168111391a489149336
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-16 16:32:32 +0330

    feat: add DeleteTask use case to tasks write service

---
+++ src/2-Services/Tasks/Write/Libraries/Application/UseCases/Tasks/DeleteTask/DeleteTaskRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskoMask.BuildingBlocks.Application.Commands;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+
+namespace TaskoMask.Services.Tasks.Write.Application.UseCases.Tasks.DeleteTask
+{
+    public class DeleteTaskRequest: BaseCommand
+    {
+        public DeleteTaskRequest(string id)
+        {
+            Id = id;
+        }
+
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string Id { get; }
+
+    }
+}

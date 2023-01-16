57e97e79774e98aaeb3f30e256af2711be39c50f
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-16 16:33:31 +0330

    feat: add UpdateComment use case to tasks write service

---
+++ src/2-Services/Tasks/Write/Libraries/Application/UseCases/Comments/UpdateComment/UpdateCommentRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskoMask.BuildingBlocks.Application.Commands;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+
+namespace TaskoMask.Services.Tasks.Write.Application.UseCases.Comments.UpdateComment
+{
+    public class UpdateCommentRequest : BaseCommand
+    {
+        public UpdateCommentRequest(string id, string content)
+        {
+            Id = id;
+            Content = content;
+        }
+
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string Id { get; }
+
+
+        [MaxLength(DomainConstValues.Comment_Content_Max_Length, ErrorMessageResourceName = nameof(ContractsMetadata.Max_Length_Error), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string Content { get; }
+
+    }
+}

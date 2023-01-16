9113af3ee106d7f944ae5d0f7d7d3cde14e6c6bc
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-16 16:33:19 +0330

    feat: add AddComment use case to tasks write service

---
+++ src/2-Services/Tasks/Write/Libraries/Application/UseCases/Comments/AddComment/AddCommentRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskoMask.BuildingBlocks.Application.Commands;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+
+namespace TaskoMask.Services.Tasks.Write.Application.UseCases.Comments.AddComment
+{
+    public class AddCommentRequest: BaseCommand
+    {
+        public AddCommentRequest(string taskId, string content)
+        {
+            TaskId = taskId;
+            Content = content;
+        }
+
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string TaskId { get; }
+
+
+        [MaxLength(DomainConstValues.Comment_Content_Max_Length, ErrorMessageResourceName = nameof(ContractsMetadata.Max_Length_Error), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string Content { get; }
+
+
+    }
+}

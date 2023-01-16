e5986ad9cdbbd263e32761b13e768fd134206343
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-16 16:32:18 +0330

    feat: add UpdateTask use case to tasks write service

---
+++ src/2-Services/Tasks/Write/Libraries/Application/UseCases/Tasks/UpdateTask/UpdateTaskValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TaskoMask.BuildingBlocks.Domain.Resources;
+
+namespace TaskoMask.Services.Tasks.Write.Application.UseCases.Tasks.UpdateTask
+{
+    public abstract class UpdateTaskValidation<TRequest> : AbstractValidator<TRequest> where TRequest : UpdateTaskRequest
+    {
+        public UpdateTaskValidation()
+        {
+            ValidateDescription();
+        }
+
+
+        private void ValidateDescription()
+        {
+            RuleFor(o => o.Description).NotEqual(o => o.Title).WithMessage(DomainMessages.Equal_Name_And_Description_Error);
+        }
+    }
+}

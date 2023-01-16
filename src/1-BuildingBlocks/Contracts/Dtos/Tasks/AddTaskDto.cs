0b700061bb6e11bcf3217153f39736236a9de07b
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-16 16:30:53 +0330

    feat: add AddTask use case to tasks write service

--- src/1-BuildingBlocks/Contracts/Dtos/Tasks/AddTaskDto.cs
+++ src/1-BuildingBlocks/Contracts/Dtos/Tasks/AddTaskDto.cs
@@ -1,6 +1,11 @@
-﻿namespace TaskoMask.BuildingBlocks.Contracts.Dtos.Tasks
+﻿using System.ComponentModel.DataAnnotations;
+using TaskoMask.BuildingBlocks.Contracts.Resources;
+
+namespace TaskoMask.BuildingBlocks.Contracts.Dtos.Tasks
 {
     public class AddTaskDto : TaskBaseDto
     {
+        [Required(ErrorMessageResourceName = nameof(ContractsMetadata.Required), ErrorMessageResourceType = typeof(ContractsMetadata))]
+        public string BoardId { get; set; }
     }
 }

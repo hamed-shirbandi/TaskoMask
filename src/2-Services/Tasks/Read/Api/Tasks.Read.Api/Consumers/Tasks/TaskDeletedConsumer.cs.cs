d85983e1fac95af5ab4ef409c90b89fd124b92ee
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-21 22:20:13 +0330

    feat: add TaskDeletedConsumer to tasks

---
+++ src/2-Services/Tasks/Read/Api/Tasks.Read.Api/Consumers/Tasks/TaskDeletedConsumer.cs.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Web.MVC.Consumers;
+using TaskoMask.BuildingBlocks.Contracts.Events;
+using TaskoMask.Services.Tasks.Read.Api.Infrastructure.DbContext;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace TaskoMask.Services.Tasks.Read.Api.Consumers.Tasks
+{
+    public class TaskDeletedConsumer : BaseConsumer<TaskDeleted>
+    {
+        private readonly TaskReadDbContext _taskReadDbContext;
+
+
+        public TaskDeletedConsumer(IInMemoryBus inMemoryBus, TaskReadDbContext taskReadDbContext) : base(inMemoryBus)
+        {
+            _taskReadDbContext = taskReadDbContext;
+        }
+
+
+        public override async Task ConsumeMessage(ConsumeContext<TaskDeleted> context)
+        {
+            await _taskReadDbContext.Tasks.DeleteOneAsync(p => p.Id == context.Message.Id);
+        }
+    }
+}

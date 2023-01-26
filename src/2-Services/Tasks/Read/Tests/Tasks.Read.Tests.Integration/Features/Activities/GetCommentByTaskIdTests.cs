3497181e564be3ccea782f2ca3ac36f03c16d960
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-26 17:20:56 +0330

    test: add an integration test for GetCommentsByTaskId feature

---
+++ src/2-Services/Tasks/Read/Tests/Tasks.Read.Tests.Integration/Features/Activities/GetCommentByTaskIdTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskoMask.Services.Tasks.Read.Api.Features.Activities.GetActivitiesByTaskId;
+using TaskoMask.Services.Tasks.Read.Tests.Integration.Fixtures;
+using TaskoMask.Services.Tasks.Read.Tests.Integration.TestData;
+using Xunit;
+
+namespace TaskoMask.Services.Tasks.Read.Tests.Integration.Features.Activities
+{
+    [Collection(nameof(ActivityCollectionFixture))]
+    public class GetActivitiesByTaskIdTests
+    {
+        #region Fields
+
+        private readonly ActivityCollectionFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public GetActivitiesByTaskIdTests(ActivityCollectionFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+        [Fact]
+        public async Task Activities_are_fetched_by_task_Id()
+        {
+            //Arrange
+            var expectedActivity = ActivityObjectMother.CreateActivity();
+            await _fixture.SeedActivityAsync(expectedActivity);
+            var getActivitiesByTaskIdHandler = new GetActivitiesByTaskIdHandler(_fixture.DbContext, _fixture.Mapper);
+            var request = new GetActivitiesByTaskIdRequest(expectedActivity.TaskId);
+
+            //Act
+            var result = await getActivitiesByTaskIdHandler.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.Should().HaveCountGreaterThanOrEqualTo(1);
+            result.Should().Contain(c => c.Id == expectedActivity.Id);
+        }
+
+
+        #endregion
+    }
+}
---
+++ src/2-Services/Tasks/Read/Tests/Tasks.Read.Tests.Integration/Features/Comments/GetCommentsByTaskIdTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskoMask.Services.Tasks.Read.Api.Features.Comments.GetCommentsByTaskId;
+using TaskoMask.Services.Tasks.Read.Tests.Integration.Fixtures;
+using TaskoMask.Services.Tasks.Read.Tests.Integration.TestData;
+using Xunit;
+
+namespace TaskoMask.Services.Tasks.Read.Tests.Integration.Features.Comments
+{
+    [Collection(nameof(CommentCollectionFixture))]
+    public class GetCommentsByTaskIdTests
+    {
+        #region Fields
+
+        private readonly CommentCollectionFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public GetCommentsByTaskIdTests(CommentCollectionFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+        [Fact]
+        public async Task Comments_are_fetched_by_task_Id()
+        {
+            //Arrange
+            var expectedComment = CommentObjectMother.CreateComment();
+            await _fixture.SeedCommentAsync(expectedComment);
+            var getCommentsByTaskIdHandler = new GetCommentsByTaskIdHandler(_fixture.DbContext, _fixture.Mapper);
+            var request = new GetCommentsByTaskIdRequest(expectedComment.TaskId);
+
+            //Act
+            var result = await getCommentsByTaskIdHandler.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.Should().HaveCountGreaterThanOrEqualTo(1);
+            result.Should().Contain(c => c.Id == expectedComment.Id);
+        }
+
+
+        #endregion
+    }
+}

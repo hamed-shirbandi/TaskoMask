85dc501c6c38bd7b69f6f36f6ff0d708d5edd5eb
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-26 16:08:28 +0330

    feat: add test data for task features

---
+++ src/2-Services/Tasks/Read/Tests/Tasks.Read.Tests.Integration/TestData/CommentObjectMother.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using TaskoMask.BuildingBlocks.Test.TestData;
+using TaskoMask.Services.Tasks.Read.Api.Domain;
+
+namespace TaskoMask.Services.Tasks.Read.Tests.Integration.TestData
+{
+    internal static class CommentObjectMother
+    {
+        public static Comment CreateComment()
+        {
+            return new Comment(ObjectId.GenerateNewId().ToString())
+            {
+                Content = TestDataGenerator.GetRandomString(20),
+                TaskId = ObjectId.GenerateNewId().ToString(),
+            };
+        }
+
+    }
+}
---
+++ src/2-Services/Tasks/Read/Tests/Tasks.Read.Tests.Integration/TestData/TaskObjectMother.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using TaskoMask.BuildingBlocks.Contracts.Enums;
+using TaskoMask.BuildingBlocks.Test.TestData;
+using TaskoMask.Services.Tasks.Read.Api.Domain;
+
+namespace TaskoMask.Services.Tasks.Read.Tests.Integration.TestData
+{
+    internal static class TaskObjectMother
+    {
+        public static Task CreateTask()
+        {
+            return new Task(ObjectId.GenerateNewId().ToString())
+            {
+                Title = TestDataGenerator.GetRandomName(10),
+                Description = TestDataGenerator.GetRandomString(20),
+                OwnerId = ObjectId.GenerateNewId().ToString(),
+                OrganizationId = ObjectId.GenerateNewId().ToString(),
+                ProjectId = ObjectId.GenerateNewId().ToString(),
+                BoardId = ObjectId.GenerateNewId().ToString(),
+                CardId = ObjectId.GenerateNewId().ToString(),
+                CardName = TestDataGenerator.GetRandomName(10),
+                CardType =BoardCardType.Backlog,
+            };
+        }
+
+    }
+}

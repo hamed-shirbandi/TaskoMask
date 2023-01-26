c3b355da9a279c1e50caad12b7af01d13c568578
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-26 16:15:57 +0330

    feat: add a fixture for activities features integration tests

---
+++ src/2-Services/Tasks/Read/Tests/Tasks.Read.Tests.Integration/Fixtures/ActivityCollectionFixture.cs
@@ -0,0 +1,44 @@
+﻿using TaskoMask.Services.Tasks.Read.Api.Domain;
+using Xunit;
+
+namespace TaskoMask.Services.Tasks.Read.Tests.Integration.Fixtures
+{
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [CollectionDefinition(nameof(ActivityCollectionFixture))]
+    public class ActivityCollectionFixtureDefinition : ICollectionFixture<ActivityCollectionFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ActivityCollectionFixture : TestsBaseFixture
+    {
+
+        public ActivityCollectionFixture() : base(dbNameSuffix: nameof(ActivityCollectionFixture))
+        {
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async System.Threading.Tasks.Task SeedActivityAsync(Activity activity)
+        {
+            await DbContext.Activities.InsertOneAsync(activity);
+        }
+
+    }
+}

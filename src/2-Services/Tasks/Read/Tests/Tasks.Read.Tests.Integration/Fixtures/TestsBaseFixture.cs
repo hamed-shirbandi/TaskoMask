ea42b47fca945270274c636f11e791eda38109e8
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-26 16:14:53 +0330

    feat: add a base fixture for tasks integration tests

---
+++ src/2-Services/Tasks/Read/Tests/Tasks.Read.Tests.Integration/Fixtures/TestsBaseFixture.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using TaskoMask.BuildingBlocks.Test.TestBase;
+using TaskoMask.Services.Tasks.Read.Api.Infrastructure.DbContext;
+using TaskoMask.Services.Tasks.Read.Api.Infrastructure.DI;
+
+namespace TaskoMask.Services.Tasks.Read.Tests.Integration.Fixtures
+{
+    public abstract class TestsBaseFixture : IntegrationTestsBase
+    {
+        public readonly IMapper Mapper;
+        public readonly TaskReadDbContext DbContext;
+
+
+        protected TestsBaseFixture(string dbNameSuffix) : base(dbNameSuffix)
+        {
+            Mapper = GetRequiredService<IMapper>();
+            DbContext = GetRequiredService<TaskReadDbContext>();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void InitialDatabase()
+        {
+            _serviceProvider.InitialDatabase();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void DropDatabase()
+        {
+            _serviceProvider.DropDatabase();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IServiceProvider GetServiceProvider(string dbNameSuffix)
+        {
+            var services = new ServiceCollection();
+
+            var configuration = new ConfigurationBuilder()
+                                //Copy from Tasks.Read.Api card during the build event
+                                .AddJsonFile("appsettings.json", reloadOnChange: true, optional: false)
+                                .AddJsonFile("appsettings.Staging.json", optional: true)
+                                .AddJsonFile("appsettings.Development.json", optional: true)
+                                .AddInMemoryCollection(new[]
+                                {
+                                   new KeyValuePair<string,string>("MongoDB:DatabaseName", $"Tasks_Read_DB_{dbNameSuffix}")
+                                })
+                                .Build();
+
+            services.AddSingleton<IConfiguration>(provider => { return configuration; });
+
+            services.AddModules(configuration);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+}

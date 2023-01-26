e43e707393a7ff61c45d88379e6a2fad2d7d6f73
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-26 17:20:38 +0330

    test: add an integration test for GetCommentById feature

---
+++ src/2-Services/Tasks/Read/Tests/Tasks.Read.Tests.Integration/Features/Comments/GetCommentByIdTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskoMask.Services.Tasks.Read.Api.Features.Comments.GetCommentById;
+using TaskoMask.Services.Tasks.Read.Tests.Integration.Fixtures;
+using TaskoMask.Services.Tasks.Read.Tests.Integration.TestData;
+using Xunit;
+
+namespace TaskoMask.Services.Tasks.Read.Tests.Integration.Features.Comments
+{
+    [Collection(nameof(CommentCollectionFixture))]
+    public class GetCommentByIdTests
+    {
+        #region Fields
+
+        private readonly CommentCollectionFixture _fixture;
+
+        #endregion
+
+        #region Ctor
+
+        public GetCommentByIdTests(CommentCollectionFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #endregion
+
+        #region Test Methods
+
+
+        [Fact]
+        public async Task Comment_is_fetched_by_Id()
+        {
+            //Arrange
+            var expectedComment = CommentObjectMother.CreateComment();
+            await _fixture.SeedCommentAsync(expectedComment);
+            var getCommentByIdHandler = new GetCommentByIdHandler(_fixture.DbContext, _fixture.Mapper);
+            var request = new GetCommentByIdRequest(expectedComment.Id);
+
+            //Act
+            var result = await getCommentByIdHandler.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.Id.Should().Be(expectedComment.Id);
+            result.Content.Should().Be(expectedComment.Content);
+        }
+
+
+        #endregion
+    }
+}

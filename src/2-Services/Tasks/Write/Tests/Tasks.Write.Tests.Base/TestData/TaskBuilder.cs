94aa66f2a4aa5e6e442af343ff70ca6ae426a5c1
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-28 16:28:44 +0330

    feat: add TaskBuilder

---
+++ src/2-Services/Tasks/Write/Tests/Tasks.Write.Tests.Base/TestData/TaskBuilder.cs
@@ -0,0 +1,78 @@
+﻿using TaskoMask.Services.Tasks.Write.Domain.Entities;
+using TaskoMask.Services.Tasks.Write.Domain.Services;
+
+namespace TaskoMask.Services.Tasks.Write.Tests.Base.TestData
+{
+    public class TaskBuilder
+    {
+        public ITaskValidatorService ValidatorService { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string CardId { get; private set; }
+        public string BoardId { get; private set; }
+
+
+
+        private TaskBuilder()
+        {
+        }
+
+
+
+        public static TaskBuilder Init()
+        {
+            return new TaskBuilder();
+        }
+
+
+
+        public TaskBuilder WithValidatorService(ITaskValidatorService validatorService)
+        {
+            ValidatorService = validatorService;
+            return this;
+        }
+
+
+
+        public TaskBuilder WithTitle(string title)
+        {
+            Title = title;
+            return this;
+        }
+
+
+
+        public TaskBuilder WithDescription(string description)
+        {
+            Description = description;
+            return this;
+        }
+
+
+
+        public TaskBuilder WithCardId(string cardId)
+        {
+            CardId = cardId;
+            return this;
+        }
+
+
+
+        public TaskBuilder WithBoardId(string boardId)
+        {
+            BoardId = boardId;
+            return this;
+        }
+
+
+
+        public Task Build()
+        {
+            var task = Task.AddTask(Title, Description, CardId, BoardId,ValidatorService);
+            task.ClearDomainEvents();
+            return task;
+        }
+
+
+    }
+}

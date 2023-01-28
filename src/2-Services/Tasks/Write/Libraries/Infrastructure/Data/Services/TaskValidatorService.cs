87658015b12496f3d52b57d76e0d8f71a93b1d53
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-28 16:54:16 +0330

    feat: add TaskValidatorService

---
+++ src/2-Services/Tasks/Write/Libraries/Infrastructure/Data/Services/TaskValidatorService.cs
@@ -0,0 +1,40 @@
+﻿using TaskoMask.Services.Tasks.Write.Domain.Data;
+using TaskoMask.Services.Tasks.Write.Domain.Services;
+
+namespace TaskoMask.Services.Tasks.Write.Infrastructure.Data.Services
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TaskValidatorService : ITaskValidatorService
+    {
+        private readonly ITaskAggregateRepository _taskRepository;
+
+        public TaskValidatorService(ITaskAggregateRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool TaskHasUniqueName(string taskId, string boardId, string taskTitle)
+        {
+            return !_taskRepository.ExistTask(taskId, boardId, taskTitle);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool CanAddNewTaskToBoard(string boardId, int maxTasksCount)
+        {
+            var tasksCount = _taskRepository.CountByBoardId(boardId);
+            return tasksCount < maxTasksCount;
+        }
+    }
+}

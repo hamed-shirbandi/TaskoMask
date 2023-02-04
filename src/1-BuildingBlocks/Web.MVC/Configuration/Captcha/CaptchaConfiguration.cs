1bb5105e81a5f4ea289f99a9ddb49c943b39c9bc
mrAhmadi <mrAhmadi@DESKTOP-N355MJ7>
2023-02-04 17:00:34 +0330

    refactor: move captpcha config into class
    this add class captcha configuration for clean code

---
+++ src/1-BuildingBlocks/Web.MVC/Configuration/Captcha/CaptchaConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using DNTCaptcha.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskoMask.BuildingBlocks.Web.MVC.Configuration.Captcha
+{
+    public static class CaptchaConfiguration
+    {
+        public static void AddCaptcha(this IServiceCollection services)
+        {
+            services.AddDNTCaptcha(options=>options.UseCookieStorageProvider(SameSiteMode.Strict)
+                .AbsoluteExpiration(minutes: 7)
+                    .ShowThousandsSeparators(false)
+                    .WithNoise(pixelsDensity: 25, linesCount: 3)
+                    .WithEncryptionKey("CaptchaEncryptionKey")
+                    .InputNames(
+                        new DNTCaptchaComponent
+                        {
+                            CaptchaHiddenInputName = "DNTCaptchaText",
+                            CaptchaHiddenTokenName = "DNTCaptchaToken",
+                            CaptchaInputName = "DNTCaptchaInputText"
+                        })
+                    .Identifier("dntCaptcha")
+            );
+            
+            services.AddControllers();
+        }
+
+        public static void UseCaptchaPreConfigured(this IApplicationBuilder app)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}

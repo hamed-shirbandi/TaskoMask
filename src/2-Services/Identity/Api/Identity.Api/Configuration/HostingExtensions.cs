c3a33f224deac8391e7d12e656d1514825cddd71
mrAhmadi <mrAhmadi@DESKTOP-N355MJ7>
2023-02-06 10:20:26 +0330

    Revert "fix: remove map razor page into use end point"
    This reverts commit 02e6f3024f4f2b5265ca709fb9bda09b67927ee2.

--- src/2-Services/Identity/Api/Identity.Api/Configuration/HostingExtensions.cs
+++ src/2-Services/Identity/Api/Identity.Api/Configuration/HostingExtensions.cs
@@ -49,9 +49,10 @@ namespace TaskoMask.Services.Identity.Api.Configuration
             app.Services.InitialDatabasesAndSeedEssentialData();
 
             app.MapRazorPages().RequireAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");

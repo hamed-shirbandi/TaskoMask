fb546545f6cf678ffe4f6d1aad66e8a3ac6265b6
mrAhmadi <mrAhmadi@DESKTOP-N355MJ7>
2023-02-06 10:29:07 +0330

    fix: remove use end point
    this is a clean code into host extentions

--- src/2-Services/Identity/Api/Identity.Api/Configuration/HostingExtensions.cs
+++ src/2-Services/Identity/Api/Identity.Api/Configuration/HostingExtensions.cs
@@ -52,7 +52,6 @@ namespace TaskoMask.Services.Identity.Api.Configuration
             
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");

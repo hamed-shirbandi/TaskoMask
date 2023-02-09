a7e2e54d0da958db727ae02c99278cde268a9295
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-02-09 13:45:10 +0330

    refactor: refactor tasks service architecture

---
+++ src/2-Services/Tasks/Api/Tasks.Write.Api/Domain/Specifications/TaskTitleAndDescriptionCannotSameSpecification.cs
@@ -0,0 +1,17 @@
+﻿using TaskoMask.BuildingBlocks.Domain.Specifications;
+using TaskoMask.Services.Tasks.Write.Api.Domain.Entities;
+
+namespace TaskoMask.Services.Tasks.Write.Api.Domain.Specifications
+{
+    internal class TaskTitleAndDescriptionCannotSameSpecification : ISpecification<Task>
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsSatisfiedBy(Task task)
+        {
+            return task.Title.Value.ToLower() != task.Description.Value?.ToLower();
+        }
+    }
+}
---
+++ src/2-Services/Tasks/Api/Tasks.Write.Api/Infrastructure/CrossCutting/DI/InfrastructureModule.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using TaskoMask.BuildingBlocks.Infrastructure.Extensions;
+using TaskoMask.BuildingBlocks.Infrastructure.MongoDB;
+using TaskoMask.Services.Tasks.Write.Api.Resources;
+using TaskoMask.Services.Tasks.Write.Api.Domain.Data;
+using TaskoMask.Services.Tasks.Write.Api.Domain.Services;
+using TaskoMask.Services.Tasks.Write.Api.Infrastructure.Data.DbContext;
+using TaskoMask.Services.Tasks.Write.Api.Infrastructure.Data.Repositories;
+using TaskoMask.Services.Tasks.Write.Api.Infrastructure.Data.Services;
+
+namespace TaskoMask.Services.Tasks.Write.Api.Infrastructure.CrossCutting.DI
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class InfrastructureModule
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddInfrastructureModule(this IServiceCollection services, IConfiguration configuration,Type consumerAssemblyMarkerType)
+        {
+            services.AddBuildingBlocksInfrastructure(configuration, consumerAssemblyMarkerType, handlerAssemblyMarkerType: typeof(ApplicationMessages));
+
+            services.AddMongoDbContext(configuration);
+
+            services.AddDomainServices();
+
+            services.AddRepositories();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void AddMongoDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var options = configuration.GetSection("MongoDB");
+            services.AddScoped<TaskWriteDbContext>().AddOptions<MongoDbOptions>().Bind(options);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void AddDomainServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITaskValidatorService, TaskValidatorService>();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ITaskAggregateRepository, TaskAggregateRepository>();
+        }
+    }
+}

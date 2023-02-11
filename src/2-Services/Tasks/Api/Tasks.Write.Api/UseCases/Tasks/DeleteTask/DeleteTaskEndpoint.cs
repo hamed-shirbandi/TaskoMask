0d5cdcf58541934fb5c46366eca616c10c6c21a2
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-02-11 17:10:01 +0330

    refactor: move tasks endpoits to usecases

---
+++ src/2-Services/Tasks/Api/Tasks.Write.Api/UseCases/Tasks/DeleteTask/DeleteTaskEndpoint.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+
+namespace TaskoMask.Services.Tasks.Write.Api.UseCases.Tasks.DeleteTask
+{
+
+    [Authorize("user-write-access")]
+    [Tags("Tasks")]
+    public class DeleteTaskEndpoint : BaseApiController
+    {
+        public DeleteTaskEndpoint(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+
+        /// <summary>
+        /// Delete an existing task
+        /// </summary>
+        [HttpDelete]
+        [Route("tasks/{id}")]
+        public async Task<Result<CommandResult>> Delete(string id)
+        {
+            return await _inMemoryBus.SendCommand<DeleteTaskRequest>(new(id));
+        }
+    }
+
+}
---
+++ src/2-Services/Tasks/Api/Tasks.Write.Api/UseCases/Tasks/MoveTaskToAnotherCard/MoveTaskToAnotherCardEndpoint.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+
+namespace TaskoMask.Services.Tasks.Write.Api.UseCases.Tasks.MoveTaskToAnotherCard
+{
+
+    [Authorize("user-write-access")]
+    [Tags("Tasks")]
+    public class MoveTaskToAnotherCardEndpoint : BaseApiController
+    {
+        public MoveTaskToAnotherCardEndpoint(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+
+        /// <summary>
+        /// Move a task to another card
+        /// </summary>
+        [HttpPut]
+        [Route("tasks/{taskId}/moveto/{cardId}")]
+        public async Task<Result<CommandResult>> Put(string taskId,string cardId)
+        {
+            return await _inMemoryBus.SendCommand<MoveTaskToAnotherCardRequest>(new(taskId, cardId));
+        }
+    }
+
+}

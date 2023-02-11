316ecae46601bd26a12ef7f45d33de30901c29d6
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-02-11 17:09:20 +0330

    refactor: move projects endpoits to usecases

---
+++ src/2-Services/Owners/Api/Owners.Write.Api/UseCases/Projects/AddProject/AddProjectEndpoint.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Projects;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+
+namespace TaskoMask.Services.Owners.Write.Api.UseCases.Projects.AddProject
+{
+
+    [Authorize("user-write-access")]
+    [Tags("Projects")]
+    public class AddProjectEndpoint : BaseApiController
+    {
+        public AddProjectEndpoint(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+
+        /// <summary>
+        /// Add new project
+        /// </summary>
+        [HttpPost]
+        [Route("projects")]
+        public async Task<Result<CommandResult>> Add([FromBody] AddProjectDto input)
+        {
+            return await _inMemoryBus.SendCommand<AddProjectRequest>(new(organizationId: input.OrganizationId, name: input.Name, description: input.Description));
+        }
+
+    }
+
+}
---
+++ src/2-Services/Owners/Api/Owners.Write.Api/UseCases/Projects/UpdateProject/UpdateProjectEndpoint.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Projects;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+
+namespace TaskoMask.Services.Owners.Write.Api.UseCases.Projects.UpdateProject
+{
+
+    [Authorize("user-write-access")]
+    [Tags("Projects")]
+    public class UpdateProjectEndpoint : BaseApiController
+    {
+        public UpdateProjectEndpoint(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+
+        /// <summary>
+        /// Update an existing project
+        /// </summary>
+        [HttpPut]
+        [Route("projects/{id}")]
+        public async Task<Result<CommandResult>> Put(string id, [FromBody] UpdateProjectDto input)
+        {
+            return await _inMemoryBus.SendCommand<UpdateProjectRequest>(new(id: input.Id, name: input.Name, description: input.Description));
+        }
+
+    }
+
+}

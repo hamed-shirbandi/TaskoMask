06650f6e65a92e893ab81f10218104e6e9d5506f
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-02-11 17:09:01 +0330

    refactor: move organization endpoits to usecases

---
+++ src/2-Services/Owners/Api/Owners.Write.Api/UseCases/Organizations/AddOrganization/AddOrganizationEndpoint.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Organizations;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+
+namespace TaskoMask.Services.Owners.Write.Api.UseCases.Organizations.AddOrganization
+{
+
+    [Authorize("user-write-access")]
+    [Tags("Organizations")]
+    public class AddOrganizationEndpoint : BaseApiController
+    {
+        public AddOrganizationEndpoint(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+
+        /// <summary>
+        /// Add new organization for current owner
+        /// </summary>
+        [HttpPost]
+        [Route("organizations")]
+        public async Task<Result<CommandResult>> Post([FromBody] AddOrganizationDto input)
+        {
+            input.OwnerId = GetCurrentUserId();
+            return await _inMemoryBus.SendCommand<AddOrganizationRequest>(new(ownerId: input.OwnerId, name: input.Name, description: input.Description));
+        }
+    }
+
+}
---
+++ src/2-Services/Owners/Api/Owners.Write.Api/UseCases/Organizations/DeleteOrganization/DeleteOrganizationEndpoint.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskoMask.BuildingBlocks.Application.Bus;
+using TaskoMask.BuildingBlocks.Contracts.Dtos.Organizations;
+using TaskoMask.BuildingBlocks.Contracts.Helpers;
+using TaskoMask.BuildingBlocks.Contracts.Services;
+using TaskoMask.BuildingBlocks.Web.MVC.Controllers;
+
+namespace TaskoMask.Services.Owners.Write.Api.UseCases.Organizations.DeleteOrganization
+{
+
+    [Authorize("user-write-access")]
+    [Tags("Organizations")]
+    public class DeleteOrganizationEndpoint : BaseApiController
+    {
+        public DeleteOrganizationEndpoint(IAuthenticatedUserService authenticatedUserService, IInMemoryBus inMemoryBus) : base(authenticatedUserService, inMemoryBus)
+        {
+        }
+
+
+
+        /// <summary>
+        /// Delete an organization
+        /// </summary>
+        [HttpDelete]
+        [Route("organizations/{id}")]
+        public async Task<Result<CommandResult>> Delete(string id)
+        {
+            return await _inMemoryBus.SendCommand<DeleteOrganizationRequest>(new(id));
+        }
+    }
+
+}
